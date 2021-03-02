b854badbd23248d949449399207130eafa326a4a
sunhpark42 <sunhpark42@gmail.com>
2021-03-02 17:13:05 +0900

    Test: 최근 검색어가 제대로 추가되는지 확인하는 테스트코드 작성

--- cypress/integration/youtube_classroom_search_modal.spec.js
+++ cypress/integration/youtube_classroom_search_modal.spec.js
@@ -25,4 +25,26 @@ describe('유튜브 강의실 영상 검색 모달', () => {
     cy.get('.clip').its('length').should('be.gt', 0);
     cy.get('.clip').its('length').should('be.lte', 10);
   })
+
+  it('사용자가 검색어를 입력하고 검색 버튼을 눌렀을 때, 최근 검색어에 추가 된다.', () => {
+    const searchTerm = '서니';
+    const searchTerm1 = '도비';
+    const searchTerm2 = '하루';
+
+    cy.get('#search-button').click();
+    cy.get('#youtube-search-input').type(searchTerm);
+    cy.get('#youtube-search-button').click();
+
+    cy.get('.chip').first().should('have.text', searchTerm);
+
+    cy.get('#youtube-search-input').type(searchTerm1);
+    cy.get('#youtube-search-button').click();
+
+    cy.get('.chip').first().should('have.text', searchTerm1);
+
+    cy.get('#youtube-search-input').type(searchTerm2);
+    cy.get('#youtube-search-button').click();
+
+    cy.get('.chip').first().should('have.text', searchTerm2);
+  })
 });
