7acd1d9c6158bec72f54e9ec5310268ec27b07be
ddongule <mnk918p@gmail.com>
2021-03-02 18:04:26 +0900

    feat: getVideoBySearch 구현
    - youtube 비디오 가져오기

--- src/js/model/youtube.js
+++ src/js/model/youtube.js
@@ -1,1 +1,20 @@
-class YoutubeModel {}
+import { YOUTUBE_API_KEY } from '../../../env.js';
+
+class YoutubeModel {
+  getVideoBySearch = ({ query, max = 10 }) => {
+    const videos = fetch(
+      `https://www.googleapis.com/youtube/v3/search?key=${YOUTUBE_API_KEY}&q=${query}&max_results=${max}&type=video&videoEmbeddable=true`,
+      {
+        method: 'GET',
+      }
+    )
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+      })
+      .then(myjson => console.log(myjson));
+  };
+}
+
+export default YoutubeModel;
