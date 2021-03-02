140b537f6373d0038dc69ea1137c1097940a1cec
sunhpark42 <sunhpark42@gmail.com>
2021-03-02 17:04:58 +0900

    Test: 사용자가 요청한 결과가 화면에 잘 뜨는지 확인하는 테스트 코드 작성

--- cypress/integration/youtube_classroom_search_modal.spec.js
+++ cypress/integration/youtube_classroom_search_modal.spec.js
@@ -6,9 +6,23 @@ describe('유튜브 강의실 영상 검색 모달', () => {
   });
 
   it('검색어에 맞게 데이터를 잘 가져오는지 확인한다.', () => {
-    const searchTerm = '서니'
+    const searchTerm = '서니';
     getVideos(searchTerm).then(res => {
       expect(res.status).to.equal(200)
     });
+  })
+
+  it('사용자가 검색 버튼을 누르고, 검색어를 입력하고, 검색 버튼을 눌렀을 때, 검색 결과가 화면에 나타난다.', () => {
+    const searchTerm = '서니';
+
+    cy.get('#search-button').click();
+    cy.get('#youtube-search-input').type(searchTerm);
+    cy.get('#youtube-search-button').click();
+
+    // TODO : cypress 로 데이터 요청 이후 테스트 하는 방법으로 교체하기
+    cy.wait(5000);
+
+    cy.get('.clip').its('length').should('be.gt', 0);
+    cy.get('.clip').its('length').should('be.lte', 10);
   })
 });
