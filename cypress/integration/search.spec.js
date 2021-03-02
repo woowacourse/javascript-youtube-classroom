d9a43279cfc5c8f796f16190c683d7e2909d03a9
yujo <yujo@student.42seoul.kr>
2021-03-02 21:48:38 +0900

    test: 검색 후 웹 스토리지에 데이터가 저장되는지 확인하는 테스트 코드 작성

--- cypress/integration/search.spec.js
+++ cypress/integration/search.spec.js
@@ -30,3 +30,39 @@ describe('유튜브 검색 테스트', () => {
     cy.get('[data-js="youtube-search-modal__not-found"]').should('be.visible');
   });
 
+  it('검색한 영상들의 json데이터가 Web Storage에 저장되는지 테스트 한다.', () => {
+    typeSearchKeywordAndClickToSubmitButton('우테코');
+    cy.get('[data-js="youtube-search-modal__save"]').click();
+
+    // 웹 스토리지에 저장되어있는지 확인
+  });
+
+  it('웹 스토리지에 100개의 동영상이 있을 때 동영상이 추가로 저장되지 않는지 테스트한다.', () => {
+    // 웹 스토리지에 더미 데이터 100개를 넣고
+
+    typeSearchKeywordAndClickToSubmitButton('우테코');
+    cy.get('[data-js="youtube-search-modal__save"]').click();
+
+    // 웹스토리지 확인
+  });
+
+  it('검색창을 다시 열었을 때 마지막 검색결과가 나오는지 테스트한다.', () => {
+    typeSearchKeywordAndClickToSubmitButton('우테코');
+    cy.get('[data-js="youtube-serach-modal__close"]').click();
+    cy.get('#search-button').click();
+    cy.get('[data-js="youtube-search-modal__clip"]').should('be.visible');
+  });
+
+  it('검색창의 하단에 최근 검색 키워드 3개가 표시되는지 테스트한다.', () => {
+    const keywords = ['우', '테', '코'];
+    keywords.forEach((keyword) => {
+      typeSearchKeywordAndClickToSubmitButton(keyword);
+    });
+
+    keywords.reverse().forEach((keyword, index) => {
+      cy.get('[data-js="youtube-search-modal__chip"]')
+        .eq(index)
+        .should('have.text', keyword);
+    });
+  });
+});
