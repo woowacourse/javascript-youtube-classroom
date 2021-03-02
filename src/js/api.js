3f5a59dfe92a2d2a1cd5ab8a81b7e3c0e8e24a47
Shim MunSeong <Puterism.K@gmail.com>
2021-03-02 18:25:23 +0900

    feat: 유튜브 검색 API를 통한 동영상 검색 기능 구현 - 검색 결과 동영상의 레이아웃이 통일되도록 스타일 및 마크업 수정

--- src/js/api.js
+++ src/js/api.js
@@ -2,7 +2,7 @@ import { YOUTUBE_API_KEY } from './config.js';
 
 export const searchYoutube = async (keyword) => {
   const response = await fetch(
-    `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${keyword}&maxResults=10&key=${YOUTUBE_API_KEY}`
+    `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&q=${keyword}&maxResults=10&key=${YOUTUBE_API_KEY}`
   );
 
   return response.json();
--- src/js/index.js
+++ src/js/index.js
@@ -16,6 +16,56 @@ const onModalClose = () => {
 $searchButton.addEventListener('click', onModalShow);
 $modalClose.addEventListener('click', onModalClose);
 
-$('#youtube-search-form').addEventListener('submit', (event) => {
+const renderSearchResult = (result) => {
+  const resultTemplate = result
+    .map((item) => {
+      const { channelId, title, channelTitle, publishedAt } = item.snippet;
+      const { videoId } = item.id;
+
+      return `
+        <article class="clip d-flex flex-col">
+          <div class="preview-container">
+            <iframe
+              width="100%"
+              height="118"
+              src="https://www.youtube.com/embed/${videoId}"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+          </div>
+          <div class="content-container pt-2 px-1 d-flex flex-col justify-between flex-1">
+            <div>
+              <h3>${title}</h3>
+              <a
+                href="https://www.youtube.com/channel/${channelId}"
+                target="_blank"
+                class="channel-name mt-1"
+              >
+                ${channelTitle}
+              </a>
+              <div class="meta">
+                <p>${publishedAt}</p>
+              </div>
+            </div>
+            <div class="d-flex justify-end">
+              <button class="btn btn-save">⬇️ 저장</button>
+            </div>
+          </div>
+        </article>
+      `;
+    })
+    .join('');
+
+  $('.youtube-search-result').innerHTML = resultTemplate;
+};
+
+$('#youtube-search-form').addEventListener('submit', async (event) => {
   event.preventDefault();
+
+  const keyword = event.target.elements.keyword.value;
+  const response = await searchYoutube(keyword);
+  const searchResult = response.items;
+
+  renderSearchResult(searchResult);
 });
