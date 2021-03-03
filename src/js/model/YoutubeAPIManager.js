641b65970be0f3db9729aeb2a116b3afbfd741b6
sunhpark42 <sunhpark42@gmail.com>
2021-03-03 17:27:23 +0900

    Feat: youtube request를 위한 모듈 추가

---
+++ src/js/model/YoutubeAPIManager.js
@@ -0,0 +1,38 @@
+import { MY_KEY } from '../key.js';
+import { MAX_RESULT } from '../constants/constants.js';
+
+export default class YoutubeAPIManager {
+  constructor() {
+    this.searchTerm = '';
+    this.pageToken = '';
+  }
+
+  setSearchTerm(searchTerm) {
+    this.searchTerm = searchTerm;
+    this.pageToken = '';
+  }
+
+  createRequestUrl() {
+    const pageTokenQuery = this.pageToken ? `pageToken=${this.pageToken}` : '';
+
+    return (
+      `https://www.googleapis.com/youtube/v3/search?` +
+      `part=snippet&key=${MY_KEY}&q=${this.searchTerm}&maxResults=${MAX_RESULT}&type=video&${pageTokenQuery}`
+    );
+  }
+
+  async requestVideos() {
+    const url = this.createRequestUrl(this.searchTerm, this.pageToken);
+
+    const res = await fetch(url, { method: 'GET' }).then((data) => {
+      if (!data.ok) {
+        throw new Error(data.status);
+      }
+      return data.json();
+    });
+
+    this.pageToken = res.nextPageToken;
+
+    return res.items;
+  }
+}
