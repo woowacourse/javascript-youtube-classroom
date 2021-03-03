9101b7fbe64c7af8ab8f93c4debe25310a95a74f
Chayan <chayans999@gmail.com>
2021-03-03 16:31:03 +0900

    feat: 비디오 템플릿, 뷰 구현

--- src/js/view/my-youtube.js
+++ src/js/view/my-youtube.js
@@ -1,1 +1,10 @@
-class MyYoutubeView {}
+import { searchVideoTemplate } from './template.js';
+
+class MyYoutubeView {
+  renderVideoArticles = infos => {
+    const $searchVideoWrapper = $('#search-video-wrapper');
+    infos.forEach(info => {
+      $searchVideoWrapper.appendChild(searchVideoTemplate(info));
+    });
+  };
+}
