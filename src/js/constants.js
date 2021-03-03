8c9a350f5895ef48250ac7f5184d8c7f3a15cd93
bigsaigon333 <likepeppermint@gmail.com>
2021-03-03 21:50:17 +0900

    feat: 유튜브 검색 API를 이용하여 검색된 영상을 표시한다

--- src/js/constants.js
+++ src/js/constants.js
@@ -4,8 +4,11 @@ const CLASSNAME = {
   VIDEO_SEARCH_TAB: "js-video-search-tab",
   YOUTUBE_SEARCH_FORM: "js-youtube-search-form",
   YOUTUBE_SEARCH_FORM_INPUT: "js-youtube-search-form__input",
-
+  YOUTUBE_SEARCH_FORM_BUTTON: "js-youtube-search-form__button",
+  MODAL_VIDEO_WRAPPER: "js-modal-video-wrapper",
+  NOT_FOUND_IMAGE: "js-not-found-img",
   OPEN: "open",
+  HIDDEN: "--hidden",
 };
 
 export default CLASSNAME;
--- src/js/index.js
+++ src/js/index.js
@@ -2,10 +2,48 @@ import CLASSNAME from "./constants.js";
 import API_KEY from "./key.js";
 import { $ } from "./querySelector.js";
 
+const TEMPLATE = ({ videoId, title, channelId, channelTitle, publishedAt }) => `
+<article class="clip">
+<div class="preview-container">
+  <iframe
+    width="100%"
+    height="118"
+    src="https://www.youtube.com/embed/${videoId}"
+    frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen
+  ></iframe>
+</div>
+<div class="content-container pt-2 px-1">
+  <h3>${title}</h3>
+  <div>
+    <a
+      href="https://www.youtube.com/channel/${channelId}"
+      target="_blank"
+      class="channel-name mt-1"
+    >
+      ${channelTitle}
+    </a>
+    <div class="meta">
+      <p>${new Date(publishedAt).toLocaleDateString("ko-KR", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      })}</p>
+    </div>
+    <div class="d-flex justify-end">
+      <button class="btn">⬇️ 저장</button>
+    </div>
+  </div>
+</div>
+</article>`;
+
 const $modal = $(CLASSNAME.MODAL);
 const $modalClose = $(CLASSNAME.MODAL_CLOSE);
 const $videoSearchTab = $(CLASSNAME.VIDEO_SEARCH_TAB);
 const $youtubeSearchForm = $(CLASSNAME.YOUTUBE_SEARCH_FORM);
+const $modalVideoWrapper = $(CLASSNAME.MODAL_VIDEO_WRAPPER);
+const $notFoundImg = $(CLASSNAME.NOT_FOUND_IMAGE);
 
 const onModalShow = () => {
   $modal.classList.add(CLASSNAME.OPEN);
@@ -18,17 +56,49 @@ const onModalClose = () => {
 const handleFormSubmit = async (event) => {
   event.preventDefault();
 
-  const query =
-    event.target.elements[CLASSNAME.YOUTUBE_SEARCH_FORM_INPUT].value;
-  const response = await fetch(
-    `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=2&regionCode=kr&safeSearch=strict&q=${query}}&key=${API_KEY}`
-  );
+  const {
+    target: {
+      elements: {
+        [CLASSNAME.YOUTUBE_SEARCH_FORM_INPUT]: { value: query },
+      },
+    },
+  } = event;
 
-  if (response.ok) {
-    // const json = await response.json();
-    // console.log(json);
-  } else {
-    // alert(`HTTP-Error: ${response.status}`);
+  try {
+    const response = await fetch(
+      `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&regionCode=kr&safeSearch=strict&q=${query}}&key=${API_KEY}`
+    );
+
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    const { items } = await response.json();
+    const clipInfos = items.map(
+      ({
+        id: { videoId },
+        snippet: { title, channelId, channelTitle, publishedAt },
+      }) => ({
+        videoId,
+        title,
+        channelId,
+        channelTitle,
+        publishedAt,
+      })
+    );
+
+    if (clipInfos.length === 0) {
+      $notFoundImg.classList.remove(CLASSNAME.HIDDEN);
+      $modalVideoWrapper.innerHTML = "";
+      return;
+    }
+
+    $modalVideoWrapper.innerHTML = clipInfos
+      .map((clipInfo) => TEMPLATE(clipInfo))
+      .join("");
+    $notFoundImg.classList.add(CLASSNAME.HIDDEN);
+  } catch (error) {
+    // console.error(error);
   }
 };
 
