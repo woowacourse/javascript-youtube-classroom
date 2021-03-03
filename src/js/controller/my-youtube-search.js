96d371fc64d0e8d7f274127f706b20de26008fef
Chayan <chayans999@gmail.com>
2021-03-03 17:35:41 +0900

    feat: 검색결과 렌더링 구현

--- src/js/controller/my-youtube-search.js
+++ src/js/controller/my-youtube-search.js
@@ -1,12 +1,28 @@
+import { $ } from '../utils/util.js';
 class MyYoutubeSearchController {
   constructor(model, view) {
     this.model = model;
     this.view = view;
   }
 
+  init() {
+    this.handleSearch();
+  }
+
+  getSearchInput = () => {
+    return $('#search-youtube-input').value;
+  };
+
   getVideosBySearch = async () => {
-    await this.model.getVideoInfosBySearch({ query: '메이커준' });
+    const query = this.getSearchInput();
+    await this.model.getVideoInfosBySearch({ query });
     this.view.renderVideoArticles(this.model.videoInfos);
+  };
+
+  handleSearch = () => {
+    $('#search-youtube-button').addEventListener('click', () => {
+      this.getVideosBySearch();
+    });
   };
 }
 
