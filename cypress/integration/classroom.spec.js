1d7954dad4235208d29d90c5e0c5379bd7bb38b4
Kyle <mkitigy@gmail.com>
2021-03-03 14:11:35 +0900

    test: 유튜브 검색 모달에서, 검색을 하면 동영상 리스트를 불러온다.

---
+++ cypress/integration/classroom.spec.js
@@ -0,0 +1,12 @@
+describe('Youtube classroom test', () => {
+  before(() => {
+    cy.visit('http://127.0.0.1:5500');
+  });
+
+  it('유튜브 검색 모달에서, 검색을 하면 동영상 리스트를 불러온다.', () => {
+    cy.get('#search-button').click();
+    cy.get('#video-search-input').type('로이드');
+    cy.get('#video-search-submit').click();
+    cy.get('#video-search-result video').should('have.length', 10);
+  });
+});
