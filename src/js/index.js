14168e45bbd4a3ac2ebc69dd67c1ce924056e063
Chayan <chayans999@gmail.com>
2021-03-03 17:36:37 +0900

    feat: 더미 index 구현

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,22 @@
-const $searchButton = document.querySelector("#search-button");
-const $modalClose = document.querySelector(".modal-close");
-const $modal = document.querySelector(".modal");
+import MyYoutubeSearchController from './controller/my-youtube-search.js';
+import YoutubeModel from './model/youtube.js';
+import MyYoutubeView from './view/my-youtube.js';
+
+const model = new YoutubeModel();
+const view = new MyYoutubeView();
+const controller = new MyYoutubeSearchController(model, view);
+controller.init();
+const $searchButton = document.querySelector('#search-button');
+const $modalClose = document.querySelector('.modal-close');
+const $modal = document.querySelector('.modal');
 
 const onModalShow = () => {
-  $modal.classList.add("open");
+  $modal.classList.add('open');
 };
 
 const onModalClose = () => {
-  $modal.classList.remove("open");
+  $modal.classList.remove('open');
 };
 
-$searchButton.addEventListener("click", onModalShow);
-$modalClose.addEventListener("click", onModalClose);
+$searchButton.addEventListener('click', onModalShow);
+$modalClose.addEventListener('click', onModalClose);
