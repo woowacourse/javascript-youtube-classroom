772f3fa713aa04673f162382e9b3b08403df1672
ddongule <mnk918p@gmail.com>
2021-03-03 17:10:18 +0900

    feat: getVideosBySearch 구현 및 적용

--- src/js/view/my-youtube.js
+++ src/js/view/my-youtube.js
@@ -1,10 +1,15 @@
+import { $ } from '../utils/util.js';
 import { searchVideoTemplate } from './template.js';
-
 class MyYoutubeView {
   renderVideoArticles = infos => {
     const $searchVideoWrapper = $('#search-video-wrapper');
+    console.log(infos);
+    console.log($searchVideoWrapper);
     infos.forEach(info => {
-      $searchVideoWrapper.appendChild(searchVideoTemplate(info));
+      $searchVideoWrapper.innerHTML += searchVideoTemplate(info);
+      // $searchVideoWrapper.appendChild();
     });
   };
 }
+
+export default MyYoutubeView;
