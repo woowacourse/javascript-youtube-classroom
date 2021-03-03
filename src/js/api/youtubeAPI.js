43592bc5347cc1eb9c37644fa003925d4613d4da
ddongule <mnk918p@gmail.com>
2021-03-03 16:18:59 +0900

    feat: api를 통해 필요한 정보를 가져오는 메서드 구현

--- src/js/api/youtubeAPI.js
+++ src/js/api/youtubeAPI.js
@@ -3,12 +3,14 @@ import { YOUTUBE_API_KEY } from '../../../env.js';
 export const api = {
   fetchVideoItems: ({ query, max = 10 }) => {
     return fetch(
-      `https://www.googleapis.com/youtube/v3/search?key=${YOUTUBE_API_KEY}&q=${query}&max_results=${max}&type=video&videoEmbeddable=true`,
+      `https://www.googleapis.com/youtube/v3/search?key=${YOUTUBE_API_KEY}&q=${query}&max_results=${max}&type=video&videoEmbeddable=true&part=snippet`,
       {
         method: 'GET',
       }
-    ).then(res => {
-      return res.ok && res.json();
-    });
+    )
+      .then(response => response.ok && response.json())
+      .catch(error => {
+        console.log(error);
+      });
   },
 };
