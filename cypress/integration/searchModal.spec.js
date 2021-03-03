cddcd8fba5ee48e692238a4b2c4b80298b2105f4
365kim <365listener@gmail.com>
2021-03-03 17:55:06 +0900

    test: 검색 모달에 검색 버튼 click 및 엔터 press 할 때 제출 확인

--- cypress/integration/searchModal.spec.js
+++ cypress/integration/searchModal.spec.js
@@ -1,9 +1,26 @@
+import { MAX_RESULT_COUNT } from '../../src/js/constants.js';
+
 describe('검색 모달 테스트', () => {
   beforeEach(() => {
     cy.visit('http://localhost:5500/');
   });
 
   const KEYWORD = '테코톡';
+
+  it('검색 모달에서 엔터키를 누르면 검색 키워드가 제출된다.', () => {
+    cy.get('#search-button').click();
+    cy.get('#search-keyword-input').type(KEYWORD).type('{enter}');
+
+    cy.get('#search-result-video-wrapper').children().should('have.length', MAX_RESULT_COUNT);
+  });
+
+  it('검색 모달에서 검색 버튼을 클릭하면 검색 키워드가 제출된다.', () => {
+    cy.get('#search-button').click();
+    cy.get('#search-keyword-input').type(KEYWORD);
+    cy.get('#search-keyword-button').click();
+
+    cy.get('#search-result-video-wrapper').children().should('have.length', MAX_RESULT_COUNT);
+  });
 
   it('검색 키워드 제출 후, 데이터를 불러오기 전이면 skeleton UI가 화면에 표시된다.', () => {
     cy.get('#search-button').click();
