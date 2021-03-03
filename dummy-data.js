e3a718024f5ff3ba2733216596dd653fbef552c3
ddongule <mnk918p@gmail.com>
2021-03-03 17:10:43 +0900

    feat: 더미 데이터 생성

---
+++ dummy-data.js
@@ -0,0 +1,82 @@
+export const dummyData = [
+  {
+    url: 'kSyAkjjdpBM',
+    title:
+      '아두이노를 이용한 아크릴램프 제작 by 메이커준 | Acrylic lamp production using Arduino by MakerJun',
+    channelUrl: 'UC19IyutnJ9s95XR82ozSWyw',
+    channelTitle: '메이커준',
+    publishTime: '2021-01-29T02:05:14Z',
+  },
+  {
+    url: 'Vdjk2jozY-o',
+    title: 'Way Maker | 스캇 브래너 Scott Brenner | 레위지파',
+    channelUrl: 'UCaTL5Hq8WdrGKibZ0qNY-LA',
+    channelTitle: '레위지파 뮤직',
+    publishTime: '2020-05-22T04:18:14Z',
+  },
+  {
+    url: 'cXMVR7OQ394',
+    title: '4개의 미니게임을 하나의 맵으로! 멀티 미니게임 맵 [브롤스타즈]',
+    channelUrl: 'UCUKgpuqq9MM9Pcc4qoKpv2w',
+    channelTitle: '니프 Neep',
+    publishTime: '2021-02-26T09:06:31Z',
+  },
+  {
+    url: 'ZGVp7X5Cy8g',
+    title: '퀸누스 요거트메이커 Y2 언박싱 &amp; 리뷰',
+    channelUrl: 'UCUgHmmTNoSoCkHxksKnapnA',
+    channelTitle: 'Singhoon 씽훈',
+    publishTime: '2021-03-01T05:30:11Z',
+  },
+  {
+    url: 'oQxjEqOQRnk',
+    title:
+      '3in1 테이블쏘 만들기 리프트 조기대 만들기 / tablesaw (router table + jigsaw table)',
+    channelUrl: 'UCYwvf0aay-8qFuJPVPyo3-A',
+    channelTitle: 'LeeMaker 리메이커',
+    publishTime: '2020-07-23T10:51:13Z',
+  },
+  {
+    url: '9V1etpaFfVs',
+    title: 'OK! MAKER (2학년 임*준 ) 제3회 학생 주도형 메이커야 놀자 작품',
+    channelUrl: 'UCoPOTc-GCQVcul9WDids8tw',
+    channelTitle: '부산사직중학교',
+    publishTime: '2020-11-28T04:56:18Z',
+  },
+  {
+    url: 'vBLamdn_jRs',
+    title: '맵 메이커로 태극기 만들었습니다 [브롤스타즈 플이]',
+    channelUrl: 'UCMmlqTsmThrRulFDlyhIUsw',
+    channelTitle: '플이',
+    publishTime: '2021-03-01T08:21:31Z',
+  },
+  {
+    url: 'fhcTvN3PuEw',
+    title: '야미 너미스-미니 소다숍 Yummy Nummies-Mini Soda Shoppe Maker ASMR',
+    channelUrl: 'UC_9UV8DCYh-kZUGRtBPTo1w',
+    channelTitle: 'JerryPop',
+    publishTime: '2019-02-05T13:10:05Z',
+  },
+  {
+    url: 'GJBEvK4Xl4s',
+    title: '[ #2 ] 트레저메이커의 첫 준등기깡 (2.24~2.26)',
+    channelUrl: 'UCQY2JqXdcCNyTZu3NUIs1cg',
+    channelTitle: '김부자',
+    publishTime: '2021-02-26T14:27:52Z',
+  },
+  {
+    url: 'atvNIOD8088',
+    title:
+      'Eng)🥪샌드위치메이커가 만들어 준 즐거운 집콕🍓과일샌드위치, 🍕피자샌드위치 _ 집밥브이로그ㅣ따뜻한여시',
+    channelUrl: 'UCnRZ1vs3L0lqkZ-fgrGUrFA',
+    channelTitle: '따뜻한 여시Warm Yosh',
+    publishTime: '2021-02-24T05:14:15Z',
+  },
+  {
+    url: 'fhcTvN3PuEw',
+    title: '야미 너미스-미니 소다숍 Yummy Nummies-Mini Soda Shoppe Maker ASMR',
+    channelUrl: 'UC_9UV8DCYh-kZUGRtBPTo1w',
+    channelTitle: 'JerryPop',
+    publishTime: '2019-02-05T13:10:05Z',
+  },
+];
