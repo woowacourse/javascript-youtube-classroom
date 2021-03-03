679fd4d98de9bff99bcf93d49396db9cb9c04947
Chayan <chayans999@gmail.com>
2021-03-03 14:55:43 +0900

    feat: api 파일 분리 구현

--- src/js/model/youtube.js
+++ src/js/model/youtube.js
@@ -1,33 +1,15 @@
-import { YOUTUBE_API_KEY } from '../../../env.js';
+import { api } from '../api/youtubeAPI.js';
 
 class YoutubeModel {
   constructor() {
     this.videoIds = [];
   }
 
-  getVideoBySearch = ({ query, max = 10 }) => {
-    const videoItems = fetch(
-      `https://www.googleapis.com/youtube/v3/search?key=${YOUTUBE_API_KEY}&q=${query}&max_results=${max}&type=video&videoEmbeddable=true`,
-      {
-        method: 'GET',
-      }
-    )
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        }
-      })
-      .then(json => {
-        console.log(json.items);
-        this.getVideoById(json.items);
+  getVideoIdBySearch = ({ query, max = 10 }) => {
+    api.fetchVideoItems({ query, max }).then(json => {
+      this.videoIds = json.items.map(item => {
+        return item.id.videoId;
       });
-
-    return videoItems;
-  };
-
-  getVideoById = items => {
-    this.videoIds = items.map(item => {
-      return item.id.videoId;
     });
   };
 }
