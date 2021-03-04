b638d6b8b994201a41572816052c687e0f85cc15
ChayanRin <k852012@naver.com>
2021-03-04 20:19:15 +0900

    feat: 저장 가능 동영상 최대 100개 제한 구현

--- src/js/model/storage.js
+++ src/js/model/storage.js
@@ -12,6 +12,7 @@ class StorageModel {
   saveVideo = json => {
     // TODO : util에 addItem(변수명, 넣어줄요소) 만들기
     const myVideo = JSON.parse(localStorage.getItem('myVideo'));
+    if (myVideo.length === 100) return;
     myVideo.push(json);
     localStorage.setItem('myVideo', JSON.stringify(myVideo));
   };
