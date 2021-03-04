3222470449066ec7c15eb92098cb41e1b512692a
zigsong <wldms5764@gmail.com>
2021-03-04 20:35:18 +0900

    refactor: Video 모델 필드 프라이빗, 날짜 출력 형식 변경

--- src/js/models/Video.js
+++ src/js/models/Video.js
@@ -2,10 +2,31 @@ export default class Video {
   constructor(videoId, videoData) {
     const { title, channelId, channelTitle, publishedAt } = videoData;
 
-    this.id = videoId;
-    this.title = title;
-    this.channelId = channelId;
-    this.channelTitle = channelTitle;
-    this.publishedAt = publishedAt;
+    this._id = videoId;
+    this._title = title;
+    this._channelId = channelId;
+    this._channelTitle = channelTitle;
+    this._publishedAt = new Date(publishedAt);
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  get title() {
+    return this._title;
+  }
+
+  get channelId() {
+    return this._channelId;
+  }
+
+  get channelTitle() {
+    return this._channelTitle;
+  }
+
+  get publishedAt() {
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    return this._publishedAt.toLocaleDateString('ko-KR', options);
   }
 }
