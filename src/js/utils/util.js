3b776e958a29d88ba7d17908eb70f37079feb710
ddongule <mnk918p@gmail.com>
2021-03-05 08:18:05 +0900

    refactor: parseDOMFromString util화

--- src/js/utils/util.js
+++ src/js/utils/util.js
@@ -2,5 +2,11 @@ export const $ = selector => document.querySelector(selector);
 
 export const $$ = selector => document.querySelectorAll(selector);
 
-export const escapeApostrophe = string =>
-  JSON.stringify(string).replace(/'/gi, '&#039;');
+export const escapeApostrophe = string => {
+  return JSON.stringify(string).replace(/'/gi, '&#039;');
+};
+
+export const parseDOMFromString = string => {
+  const parser = new DOMParser();
+  return parser.parseFromString(string, 'text/html').body.firstElementChild;
+};
