cffc6a7329a71c28d6865970c1d1cf169e31b433
2sooy <1401660@naver.com>
2021-03-04 21:45:07 +0900

    feat : 키워드 검색 시 검색 결과를 출력하는 기능 구현
    - 키워드 검색 시 10개 이하의 결과를 출력하는 테스트 코드 작성

--- cypress/integration/search_keyword.spec.js
+++ cypress/integration/search_keyword.spec.js
@@ -1,14 +1,27 @@
 /// <reference types="cypress" />
 
 context("모달을 통한 비디오 검색", () => {
-  beforeEach(() => {
-    cy.visit("http://127.0.0.1:5500");
+  describe("검색 결과가 있는 경우", () => {
+    before(() => {
+      cy.visit("http://127.0.0.1:5500");
+    });
+
+    it("모달창이 띄워진 상황에서, 검색 창에 검색어를 입력한 후 검색 버튼을 누르면, 검색 결과 모달에 로딩 애니메이션이 출력된다.", () => {
+      cy.get(".menu-section__video-search-btn").click();
+      cy.get(".search-modal__input").type("스낵랩");
+      cy.get(".search-modal__btn").click();
+      cy.get(".skeleton").should("be.visible");
+    });
+
+    it("데이터 로딩 중인 상태에서, 검색 결과가 존재할 경우 10개 이하의 검색 결과가 출력된다.", () => {
+      cy.get(".skeleton").should("not.exist");
+      cy.get(".search-modal__video-wrapper").find(".clip").its("length").should("be.lte", 10);
+    });
+
+    it("10개 이하의 검색 결과가 출력된 경우, 로딩 애니메이션이 사라진다.", () => {
+      cy.get(".skeleton").should("not.exist");
+    });
   });
 
-  it("모달창이 띄워진 상황에서, 검색 창에 검색어를 입력한 후 검색 버튼을 누르면, 검색 결과 모달에 로딩 애니메이션이 출력된다.", () => {
-    cy.get(".menu-section__video-search-btn").click();
-    cy.get(".search-modal__input").type("스낵랩");
-    cy.get(".search-modal__btn").click();
-    cy.get(".skeleton").should("be.visible");
-  });
+  describe("검색 결과가 없는 경우", () => {});
 });
--- src/js/components/SearchModal.js
+++ src/js/components/SearchModal.js
@@ -1,9 +1,21 @@
 import { $ } from "../utils/dom.js";
+import { API_KEY } from "../apiKey.js";
 
 class SearchModal {
   constructor() {
+    this.initState();
     this.selectDOM();
     this.bindEvent();
+  }
+
+  initState() {
+    this.videos = [];
+  }
+
+  setState({ videos }) {
+    this.videos = videos ?? this.videos;
+
+    this.render();
   }
 
   selectDOM() {
@@ -28,23 +40,94 @@ class SearchModal {
       <p class="line"></p>
     </div>`;
 
-    this.$videoWrapper.innerHTML = skeletonCardTemplate.repeat(10);
+    this.$videoWrapper.insertAdjacentHTML("beforeend", skeletonCardTemplate.repeat(10));
   }
 
-  hideLoadingAnimation() {}
+  async handleSearchKeyword() {
+    const keyword = this.$searchInput.value.trim();
 
-  handleSearchKeyword() {
-    // 로딩창 보임
-    this.showLoadingAnimation(); // TEST
-    // 요청
+    if (keyword.length === 0) {
+      alert("검색어를 입력해주세요");
+      return;
+    }
 
-    // 로딩창 제거
-    // this.hideLoadingAnimation();
+    this.showLoadingAnimation();
+
+    const res = await fetch(
+      `https://content.googleapis.com/youtube/v3/search?part=snippet&q=${encodeURI(
+        keyword,
+      )}&maxResults=10&key=${API_KEY}`,
+    );
+
+    if (!res.ok) {
+      return;
+    }
+
+    const { items } = await res.json();
+
+    const videos = items.map(
+      ({ id: { videoId }, snippet: { channelId, channelTitle, publishedAt, title } }) => ({
+        videoId,
+        channelId,
+        channelTitle,
+        publishedAt,
+        title,
+      }),
+    );
+
+    this.setState({ videos });
+  }
+
+  render() {
+    this.$videoWrapper.innerHTML = this.videos
+      .map(video => createSearchedVideoTemplate(video))
+      .join("");
   }
 
   showModal() {
     this.$target.classList.add("open");
   }
 }
+
+const createSearchedVideoTemplate = video => `
+<article class="clip">
+  <div class="preview-container">
+    <iframe
+      width="100%"
+      height="118"
+      src="https://www.youtube.com/embed/${video.videoId}"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    ></iframe>
+  </div>
+  <div class="content-container pt-2 px-1">
+    <h3>${video.title}</h3> //title
+    <div>
+      <a
+        href="https://www.youtube.com/channel/${video.channelId}"
+        target="_blank"
+        class="channel-name mt-1"
+      >
+      ${video.channelTitle}
+      </a>
+      <div class="meta">
+        <p>${changeDateFormat(video.publishedAt)}</p>
+      </div>
+      <div class="d-flex justify-end">
+        <button class="btn">⬇️ 저장</button>
+      </div>
+    </div>
+  </div>
+</article>`;
+
+const changeDateFormat = publishedAt => {
+  const date = new Date(publishedAt);
+  const year = date.getFullYear();
+  const month = date.getMonth();
+  const day = date.getDay();
+
+  return `${year}년 ${month}월 ${day}일`;
+};
 
 export default SearchModal;
