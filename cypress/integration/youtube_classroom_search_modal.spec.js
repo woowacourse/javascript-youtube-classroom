f51c01a8fa067e54949373cfb729ec9abf6129f3
zereight <fullstackmachinelearning@gmail.com>
2021-03-04 14:00:34 +0900

    Test: 사용자가 최근 검색어를 클릭하면, 해당 검색어로 검색이 되는지 확인하는 테스트 코드 추가

--- cypress/integration/youtube_classroom_search_modal.spec.js
+++ cypress/integration/youtube_classroom_search_modal.spec.js
@@ -24,35 +24,54 @@ describe('유튜브 강의실 영상 검색 모달', () => {
   //     cy.get('.clip').its('length').should('be.lte', 10);
   //   });
 
-  it('사용자가 검색어를 입력하고 검색 버튼을 눌렀을 때, 최근 검색어에 추가 된다.', () => {
-    const searchTerm = '서니';
+  // it('사용자가 검색어를 입력하고 검색 버튼을 눌렀을 때, 최근 검색어에 추가 된다.', () => {
+  //   const searchTerm = '서니';
+  //   const searchTerm1 = '도비';
+  //   const searchTerm2 = '하루';
+  //   const searchTerm3 = '메이커준';
+
+  //   cy.get('#search-button').click();
+  //   cy.get('#youtube-search-input').type(searchTerm);
+  //   cy.get('#youtube-search-button').click();
+
+  //   cy.get('.chip').first().should('have.text', searchTerm);
+
+  //   cy.get('#youtube-search-input').clear();
+  //   cy.get('#youtube-search-input').type(searchTerm1);
+  //   cy.get('#youtube-search-button').click();
+
+  //   cy.get('.chip').first().should('have.text', searchTerm1);
+
+  //   cy.get('#youtube-search-input').clear();
+  //   cy.get('#youtube-search-input').type(searchTerm2);
+  //   cy.get('#youtube-search-button').click();
+
+  //   cy.get('.chip').first().should('have.text', searchTerm2);
+
+  //   cy.get('#youtube-search-input').clear();
+  //   cy.get('#youtube-search-input').type(searchTerm3);
+  //   cy.get('#youtube-search-button').click();
+
+  //   cy.get('.chip').first().should('have.text', searchTerm3);
+  // });
+
+  it('사용자가 최근 검색어를 클릭하면 해당 검색어로 새로 검색이 된다.', () => {
+    const searchTerm = '치킨';
     const searchTerm1 = '도비';
-    const searchTerm2 = '하루';
-    const searchTerm3 = '메이커준';
 
     cy.get('#search-button').click();
     cy.get('#youtube-search-input').type(searchTerm);
     cy.get('#youtube-search-button').click();
 
-    cy.get('.chip').first().should('have.text', searchTerm);
-
     cy.get('#youtube-search-input').clear();
     cy.get('#youtube-search-input').type(searchTerm1);
     cy.get('#youtube-search-button').click();
-
-    cy.get('.chip').first().should('have.text', searchTerm1);
-
-    cy.get('#youtube-search-input').clear();
-    cy.get('#youtube-search-input').type(searchTerm2);
-    cy.get('#youtube-search-button').click();
-
-    cy.get('.chip').first().should('have.text', searchTerm2);
-
-    cy.get('#youtube-search-input').clear();
-    cy.get('#youtube-search-input').type(searchTerm3);
-    cy.get('#youtube-search-button').click();
-
-    cy.get('.chip').first().should('have.text', searchTerm3);
+    cy.wait(1000);
+    cy.get('.chip').last().click({ force: true });
+    cy.wait(3000);
+    cy.get('.modal .video-title').each(($elem) => {
+      cy.wrap($elem).contains(searchTerm);
+    });
   });
 
   //   it('사용자가 검색어를 아무것도 입력하지 않았을 때, 아무반응도 일어나지 않는다.', () => {
