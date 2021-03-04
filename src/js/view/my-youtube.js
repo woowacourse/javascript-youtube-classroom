cfa72b1906efdb0845e63eac693e02ae64a27f6f
ChayanRin <k852012@naver.com>
2021-03-04 11:37:41 +0900

    feat: 검색결과 없음 템플릿, 뷰, css 구현

--- src/js/view/my-youtube.js
+++ src/js/view/my-youtube.js
@@ -1,11 +1,16 @@
 import { $ } from '../utils/util.js';
-import { searchVideoTemplate } from './template.js';
+import { searchNotFoundTemplate, searchVideoTemplate } from './template.js';
 class MyYoutubeView {
   renderVideoArticles = infos => {
     const $searchVideoWrapper = $('#search-video-wrapper');
     infos.forEach(info => {
       $searchVideoWrapper.innerHTML += searchVideoTemplate(info);
     });
+  };
+
+  renderNotFound = () => {
+    const $searchVideoWrapper = $('#search-video-wrapper');
+    $searchVideoWrapper.innerHTML = searchNotFoundTemplate();
   };
 
   renderSkeletonArticles = () => {
