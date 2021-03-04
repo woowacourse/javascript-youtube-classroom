a470ad775d4741ef18ad125fa9bf0800e6dadf5a
yujo <yujo@student.42seoul.kr>
2021-03-04 22:28:19 +0900

    feat: 최근 검색 키워드를 render하는 기능

--- src/js/handler/onModalShow.js
+++ src/js/handler/onModalShow.js
@@ -1,5 +1,21 @@
 import { openModal } from '../view/viewModal.js';
 
+const recentKeywordsLabel = () => {
+  return `<span class="text-gray-700">최근 검색어: </span>`;
+};
+
+const recentKeywordTemplate = (keyword) => {
+  return `<a class="chip">${keyword}</a>`;
+};
+
+const renderRecentKeywords = (recentKeywords) => {
+  $('[data-js="youtube-search-modal__recent-keywords"]').innerHTML =
+    recentKeywordsLabel() + recentKeywords.map(recentKeywordTemplate).join('');
+};
+
 export const onModalShow = () => {
+  const recentKeywords = localStorage.get('recentKeywords') ?? [];
+
   openModal();
+  renderRecentKeywords(recentKeywords);
 };
