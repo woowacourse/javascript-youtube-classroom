129d72c3fb065221220d02dc1409348fa7e0179a
sunhpark42 <sunhpark42@gmail.com>
2021-03-06 15:33:32 +0900

    Refactor: createElement에 default parameter 설정

--- src/js/utils/utils.js
+++ src/js/utils/utils.js
@@ -19,10 +19,10 @@ export const $$ = (selector, target = document) => {
   return target.querySelectorAll(selector);
 };
 
-export const createElement = ({ tag = 'div', classes = [], textContent }) => {
+export const createElement = ({ tag = 'div', classes = [], textContent= '' }) => {
   const element = document.createElement(tag);
   element.classList.add(...classes);
-  element.textContent = textContent ?? '';
+  element.textContent = textContent;
   return element;
 };
 
