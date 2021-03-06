14f60d7037c464aca2e34a623a54738253d93611
365kim <365listener@gmail.com>
2021-03-06 11:06:26 +0900

    chore: 사이프레스 테스트 only 구문 삭제

--- cypress/integration/searchModal.spec.js
+++ cypress/integration/searchModal.spec.js
@@ -52,7 +52,7 @@ describe('검색 모달 테스트', () => {
     cy.get('#search-result-wrapper').find('img').should('have.attr', 'src').should('include', 'not_found');
   });
 
-  it.only('스크롤바를 최하단으로 이동시킬 경우, 다음 10개 아이템을 추가로 화면에 표시한다.', () => {
+  it('스크롤바를 최하단으로 이동시킬 경우, 다음 10개 아이템을 추가로 화면에 표시한다.', () => {
     cy.get('#search-button').click();
     cy.get('#search-keyword-input').type(KEYWORD);
     cy.get('#search-keyword-form').submit();
