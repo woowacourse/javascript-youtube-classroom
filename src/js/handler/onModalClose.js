829d9717942bbd394b9d10b519526b18088fef7c
yujo <yujo@student.42seoul.kr>
2021-03-06 21:39:32 +0900

    feat: dimmed 클릭 시에 modal이 닫히는 기능

--- src/js/handler/onModalClose.js
+++ src/js/handler/onModalClose.js
@@ -1,5 +1,17 @@
 import { closeModal } from '../view/modal.js';
 
-export const onModalClose = () => {
-  closeModal();
+const isDimmed = (target) => {
+  return (
+    target.classList.contains('modal') && target.classList.contains('open')
+  );
+};
+
+const isCloseButton = (target) => {
+  return target.closest('[data-js="youtube-serach-modal__close"]') !== null;
+};
+
+export const onModalClose = ({ target }) => {
+  if (isDimmed(target) || isCloseButton(target)) {
+    closeModal();
+  }
 };
--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,18 @@
-import { onModalShow } from './handler/onModalShow.js';
-
 import { $ } from './utils/querySelector.js';
+import storage from './utils/localStorage.js';
+import { throttling } from './utils/throttling.js';
+import { LOCAL_STORAGE_KEY } from './utils/constant.js';
+import { onModalShow } from './handler/onModalShow.js';
 import { onSearchClip } from './handler/onSearchClip.js';
 import { onModalScroll } from './handler/onModalScroll.js';
 import { onSaveClip } from './handler/onSaveClip.js';
-import { renderSavedClips } from './view/main.js';
-import storage from './utils/localStorage.js';
-import { LOCAL_STORAGE_KEY } from './utils/constant.js';
-import { throttling } from './utils/throttling.js';
 import { onModalClose } from './handler/onModalClose.js';
+import { renderSavedClips } from './view/main.js';
 
 export const YoutubeClassRoom = () => {
   $('[data-js="navigator__search-button"]').addEventListener(
     'click',
     onModalShow,
-  );
-  $('[data-js="youtube-serach-modal__close"]').addEventListener(
-    'click',
-    onModalClose,
   );
   $('[data-js="youtube-search-modal__form"]').addEventListener(
     'submit',
@@ -33,6 +28,7 @@ export const YoutubeClassRoom = () => {
     'click',
     onSaveClip,
   );
+  window.addEventListener('click', onModalClose);
 };
 
 window.onload = () => {
