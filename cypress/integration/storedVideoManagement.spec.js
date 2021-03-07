3d2cf2add8676c0b5d50c34a07d518f1f11b798a
365kim <365listener@gmail.com>
2021-03-07 16:20:04 +0900

    test: 저장된 영상이 있는 경우에 페이지 재방문시, 시청중인 영상목록 표시

--- cypress/integration/storedVideoManagement.spec.js
+++ cypress/integration/storedVideoManagement.spec.js
@@ -5,9 +5,32 @@ describe('저장된 비디오 관리 기능 테스트', () => {
     cy.visit('http://localhost:5500/');
   });
 
-  it('저장된 영상이 없는 경우, 비어있다는 것을 사용자에게 알려주는 상태를 보여준다.', () => {
+  it('저장된 영상이 없는 경우, 비어있다는 것을 사용자에게 알려주는 상태를 표시한다.', () => {
     cy.clearLocalStorage(VIDEOS_TO_WATCH);
     cy.clearLocalStorage(VIDEOS_WATCHED);
     cy.get('.js-stored-videos').find('img').should('have.attr', 'src').should('include', 'no_video_stored');
+  });
+
+  it('저장된 영상이 있는 경우에 페이지를 다시 방문하면, 시청중인 영상 목록을 표시한다.', () => {
+    const KEYWORD = '테코톡';
+    const storedVideoTitles = [];
+
+    cy.get('#search-button').click();
+    cy.get('#search-keyword-input').type(KEYWORD);
+    cy.get('#search-keyword-form').submit();
+    cy.get('.save-button').each(($el) => {
+      $el.click();
+      cy.wait(500);
+      cy.wrap($el)
+        .parent()
+        .siblings('.video-title')
+        .invoke('text')
+        .then((videoTitle) => storedVideoTitles.push(videoTitle));
+    });
+
+    cy.reload();
+    cy.get('.js-stored-videos h3').each(($el, index) => {
+      cy.wrap($el).should('have.text', storedVideoTitles[index]);
+    });
   });
 });
