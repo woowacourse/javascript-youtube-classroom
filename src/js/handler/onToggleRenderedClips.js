18fd4eef31a4e7f69b3d04d144d8d44f2d2b3479
yujo <yujo@student.42seoul.kr>
2021-03-08 17:09:48 +0900

    feat: 버튼을 선택했을 때 선택한 버튼의 Style을 변경하는 기능

--- src/js/handler/onToggleRenderedClips.js
+++ src/js/handler/onToggleRenderedClips.js
@@ -1,14 +1,29 @@
-import { $$ } from '../utils/querySelector.js';
+import { $, $$ } from '../utils/querySelector.js';
 import { showElement, hideElement } from '../utils/setAttribute.js';
 
-export const onToggleRenderedClips = ({ target }) => {
-  const savedClips = $$('[data-js="saved-page__clip"]');
+const setSelected = (isWatched) => {
+  const $watchedButton = $('[data-js="navigator__watched-button"]');
+  const $unwatchedButton = $('[data-js="navigator__unwatched-button"]');
 
+  if (isWatched) {
+    $unwatchedButton.classList.add('bg-cyan-100');
+    $watchedButton.classList.remove('bg-cyan-100');
+    return;
+  }
+
+  $watchedButton.classList.add('bg-cyan-100');
+  $unwatchedButton.classList.remove('bg-cyan-100');
+};
+
+export const onToggleRenderedClips = ({ target }) => {
   if (target.dataset.js === 'navigator__search') {
     return;
   }
 
+  const savedClips = $$('[data-js="saved-page__clip"]');
   const isWatched = target.dataset.js === 'navigator__unwatched-button';
+
+  setSelected(isWatched);
   savedClips.forEach((savedClip) => {
     savedClip.dataset.iswatched === String(isWatched)
       ? showElement(savedClip)
