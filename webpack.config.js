4502de9a228756227d0f0d6d3ef9c4283d7d1aa0
swon3210 <swon3210@gmail.com>
2021-03-08 15:57:51 +0900

    chore: 웹팩 output 설정 수정

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 const webpack = require('webpack');
 const dotenv = require('dotenv');
+const path = require('path');
 dotenv.config();
 
 module.exports = {
   mode: 'development',
   entry: ['babel-polyfill', './src/index.js'],
+  output: {
+    path: path.join(__dirname, 'bundle'),
+    filename: 'bundle.js',
+  },
   devtool: 'source-map',
   plugins: [
     new webpack.DefinePlugin({
