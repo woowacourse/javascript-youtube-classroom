00269229878f96968b3ec8a2a054590910fb662b
yujo <yujo@student.42seoul.kr>
2021-03-08 14:45:24 +0900

    test: check을 클릭했을 때 저장된 영상의 상태가 변경되는지 확인하는 테스트코드 작성

--- cypress/integration/main.spec.js
+++ cypress/integration/main.spec.js
@@ -11,4 +11,22 @@ describe('나만의 유튜브 강의실 메인 페이지 테스트', () => {
     cy.clearLocalStorage('savedClips');
     cy.get('[data-js="saved-page__not-found"]').should('be.visible');
   });
+
+  it('영상 카드의 ✅ 버튼 클릭시, 볼 영상의 상태가 본 영상으로 변경되는지 테스트한다.', () => {
+    // 더미 동영상 데이터 삽입
+    cy.reload(); // -> 새로고침
+    cy.get('[data-js="saved-clip-button-container__check"]').click();
+    cy.get('[data-js="navigator__watched-button"]').click();
+    cy.get('[data-js="saved-page__clip"]').shoulde('be.visible');
+  });
+
+  it('영상 카드의 ✅ 버튼 클릭시, 본 영상의 상태가 볼 영상으로 변경되는지 테스트한다.', () => {
+    // 더미 동영상 데이터 삽입
+    cy.reload(); // -> 새로고침
+    cy.get('[data-js="navigator__watched-button"]').click();
+    cy.get('[data-js="saved-clip-button-container__check"]').click();
+    cy.get('[data-js="navigator__unwatched-button"]').click();
+    cy.get('[data-js="saved-page__clip"]').shoulde('be.visible');
+  });
+
 });
