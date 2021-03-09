935e3d9546d07b316a570747c92d77d053fa872c
ChayanRin <k852012@naver.com>
2021-03-09 20:11:09 +0900

    feat: snackbar 클래스, 메서드 구현

--- src/js/view/snackbar.js
+++ src/js/view/snackbar.js
@@ -1,6 +1,13 @@
 class SnackBarView {
-  // snacker - view에다가 만들기
-  //// 우선 html 에 snacker id 가진 div 있다. 그리고 내부에 text만 변경해주는 view 메서드를 따로 만든다! 거기에 요청하는 방식으로
+  showSnackBar = text => {
+    const $snackBar = document.querySelector('#snackbar');
+    $snackBar.innerHTML = text;
+
+    $snackBar.classList.toggle('show');
+    setTimeout(() => {
+      $snackBar.classList.toggle('show');
+    }, 3000);
+  };
 }
 
-export default SnackBarView();
+export default SnackBarView;
