bbab8ad67c7cf1cdd8d5c934f1ab21b879db51e9
ChayanRin <k852012@naver.com>
2021-03-09 14:44:18 +0900

    feat: template 폴더 생성, 새로 생성한 클래스 파일 연동

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,25 @@
 import SearchController from './controller/search.js';
 import ModalController from './controller/modal.js';
+import SavedController from './controller/saved.js';
 import StorageModel from './model/storage.js';
 import YoutubeModel from './model/youtube.js';
 import SearchView from './view/search.js';
+import SavedView from './view/saved.js';
 
-const youtube = new YoutubeModel();
-const storage = new StorageModel();
-const view = new SearchView();
-const controller = new SearchController(youtube, storage, view);
-const modal = new ModalController();
+const youtubeModel = new YoutubeModel();
+const storageModel = new StorageModel();
 
-controller.init();
-modal.init();
+const searchView = new SearchView();
+const savedView = new SavedView();
+
+const searchController = new SearchController(
+  youtubeModel,
+  storageModel,
+  searchView
+);
+const savedController = new SavedController(storageModel, savedView);
+const modalController = new ModalController();
+
+searchController.init();
+savedController.init();
+modalController.init();
