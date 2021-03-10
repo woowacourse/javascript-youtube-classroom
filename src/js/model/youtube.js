0796d00c8297fa7832e7f47663df6c224359cc9a
ddongule <mnk918p@gmail.com>
2021-03-10 15:55:19 +0900

    fix: 동영상 검색 시 첫 화면에 not found image 뜨는 오류

--- src/js/model/youtube.js
+++ src/js/model/youtube.js
@@ -10,20 +10,22 @@ class YoutubeModel {
     this.#nextPageToken = '';
   }
 
-  getVideoInfosBySearch = ({ query, max = SEARCH.FETCH_VIDEO_LENGTH }) => {
+  getVideoInfosBySearch = async ({
+    query,
+    max = SEARCH.FETCH_VIDEO_LENGTH,
+  }) => {
     const nextPageToken = this.#nextPageToken;
+    const json = await api.fetchVideoItems({ query, nextPageToken, max });
 
-    api.fetchVideoItems({ query, nextPageToken, max }).then(json => {
-      this.#nextPageToken = json.nextPageToken;
-      this.#videoInfos = json.items.map(item => {
-        return {
-          url: item.id.videoId,
-          title: item.snippet.title,
-          channelId: item.snippet.channelId,
-          channelTitle: item.snippet.channelTitle,
-          publishTime: item.snippet.publishTime,
-        };
-      });
+    this.#nextPageToken = json.nextPageToken;
+    this.#videoInfos = json.items.map(item => {
+      return {
+        url: item.id.videoId,
+        title: item.snippet.title,
+        channelId: item.snippet.channelId,
+        channelTitle: item.snippet.channelTitle,
+        publishTime: item.snippet.publishTime,
+      };
     });
   };
 
