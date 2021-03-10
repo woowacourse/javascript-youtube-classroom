e2c4d9f793fe89137527fe2fbab76f3e684ed544
365kim <365listener@gmail.com>
2021-03-10 14:44:26 +0900

    refactor:비디오 '저장' 변수명 통일

--- cypress/integration/storedVideoManagement.spec.js
+++ cypress/integration/storedVideoManagement.spec.js
@@ -15,12 +15,12 @@ describe('저장된 비디오 관리 기능 테스트', () => {
   it('저장된 영상이 없는 경우, 비어있다는 것을 사용자에게 알려주는 상태를 표시한다.', () => {
     cy.clearLocalStorage(VIDEOS_TO_WATCH);
     cy.clearLocalStorage(VIDEOS_WATCHED);
-    cy.get('.js-stored-videos').find('img').should('have.attr', 'src').should('include', 'no_video_stored');
+    cy.get('.js-saved-videos').find('img').should('have.attr', 'src').should('include', 'no_video_saved');
   });
 
   it('저장된 영상이 있는 경우에 페이지를 다시 방문하면, 시청중인 영상 목록을 표시한다.', () => {
     const KEYWORD = '테코톡';
-    const storedVideoTitles = [];
+    const savedVideoTitles = [];
 
     cy.get('#search-button').click();
     cy.get('#search-keyword-input').type(KEYWORD);
@@ -32,18 +32,18 @@ describe('저장된 비디오 관리 기능 테스트', () => {
         .parent()
         .siblings('.video-title')
         .invoke('text')
-        .then((videoTitle) => storedVideoTitles.push(videoTitle));
+        .then((videoTitle) => savedVideoTitles.push(videoTitle));
     });
 
     cy.reload();
-    cy.get('.js-stored-videos .js-video-title').each(($el, index) => {
-      cy.wrap($el).should('have.text', storedVideoTitles[index]);
+    cy.get('.js-saved-videos .js-video-title').each(($el, index) => {
+      cy.wrap($el).should('have.text', savedVideoTitles[index]);
     });
   });
 
   it('[시청 중인 영상] 메뉴에서 영상 카드의 시청완료 체크버튼을 클릭하면, 해당 영상이 시청중인 영상에서 시청완료 영상으로 옮겨지고 알림이 표시된다.', () => {
     cy.get('.js-watching-menu-button').click();
-    cy.get('.js-stored-videos .watching').each(($el, index) => {
+    cy.get('.js-saved-videos .watching').each(($el, index) => {
       if (index > MAX_RESULT_COUNT / 2) {
         return;
       }
@@ -60,7 +60,7 @@ describe('저장된 비디오 관리 기능 테스트', () => {
 
   it('[시청 완료 영상] 메뉴에서 영상 카드의 시청완료 체크버튼을 해제하면, 해당 영상이 시청완료 영상에서 시청중인 영상으로 옮겨지고 알림이 표시된다.', () => {
     cy.get('.js-watched-menu-button').click();
-    cy.get('.js-stored-videos .watched')
+    cy.get('.js-saved-videos .watched')
       .eq(0)
       .then(($el) => {
         cy.wrap($el).get('.js-check-button').click();
@@ -75,7 +75,7 @@ describe('저장된 비디오 관리 기능 테스트', () => {
 
   it('영상 카드의 삭제버튼을 클릭하면, 나의 강의실에서 해당 영상이 삭제되고 알림이 표시된다.', () => {
     cy.get('.js-watching-menu-button').click();
-    cy.get('.js-stored-videos .watching')
+    cy.get('.js-saved-videos .watching')
       .eq(0)
       .then(($el) => {
         cy.wrap($el).get('.js-remove-button').click();
@@ -87,11 +87,11 @@ describe('저장된 비디오 관리 기능 테스트', () => {
 
   it('[시청 중인 영상]과 [시청 완료 영상] 메뉴버튼을 클릭하여 저장한 영상을 필터링할 수 있다.', () => {
     cy.get('.js-watching-menu-button').click();
-    cy.get('.js-stored-videos .watching').should('have.length.of.greaterThan', 0);
-    cy.get('.js-stored-videos .watched').should('have.length', 0);
+    cy.get('.js-saved-videos .watching').should('have.length.of.greaterThan', 0);
+    cy.get('.js-saved-videos .watched').should('have.length', 0);
 
     cy.get('.js-watched-menu-button').click();
-    cy.get('.js-stored-videos .watched').should('have.length.of.greaterThan', 0);
-    cy.get('.js-stored-videos .watching').should('have.length', 0);
+    cy.get('.js-saved-videos .watched').should('have.length.of.greaterThan', 0);
+    cy.get('.js-saved-videos .watching').should('have.length', 0);
   });
 });
