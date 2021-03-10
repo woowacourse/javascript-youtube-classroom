e8a71c0fcd5499813d657622dda59e4c5b79d06f
yujo <yujo@student.42seoul.kr>
2021-03-10 20:32:22 +0900

    test: test 동작을 위한 dummy 데이터 생성 및 삽입 코드 작성

--- cypress/integration/main.spec.js
+++ cypress/integration/main.spec.js
@@ -1,9 +1,11 @@
-import { MESSAGE } from '../../src/js/utils/constant';
+import { MESSAGE } from '../../src/js/utils/constant.js';
+import { setLocalDummy } from './dummy.js';
 
 describe('나만의 유튜브 강의실 메인 페이지 테스트', () => {
   beforeEach(() => {
     cy.visit('/');
     cy.clearLocalStorage('savedClips');
+    setLocalDummy();
   });
 
   it('페이지 접속시 기본 메인화면이 "볼 영상"인지 테스트한다. ', () => {
@@ -15,16 +17,14 @@ describe('나만의 유튜브 강의실 메인 페이지 테스트', () => {
   });
 
   it('영상 카드의 ✅ 버튼 클릭시, 볼 영상의 상태가 본 영상으로 변경되는지 테스트한다.', () => {
-    // 더미 동영상 데이터 삽입
-    cy.reload(); // -> 새로고침
+    cy.reload();
     cy.get('[data-js="saved-clip-button-container__check"]').click();
     cy.get('[data-js="navigator__watched-button"]').click();
     cy.get('[data-js="saved-page__clip"]').shoulde('be.visible');
   });
 
   it('영상 카드의 ✅ 버튼 클릭시, 본 영상의 상태가 볼 영상으로 변경되는지 테스트한다.', () => {
-    // 더미 동영상 데이터 삽입
-    cy.reload(); // -> 새로고침
+    cy.reload();
     cy.get('[data-js="navigator__watched-button"]').click();
     cy.get('[data-js="saved-clip-button-container__check"]').click();
     cy.get('[data-js="navigator__unwatched-button"]').click();
@@ -34,8 +34,7 @@ describe('나만의 유튜브 강의실 메인 페이지 테스트', () => {
   it('영상 카드의 🗑️ 버튼 클릭시, 저장된 리스트에서 영상이 삭제되는지 테스트한다.', () => {
     const stub = cy.stub();
     cy.on('window:confirm', stub);
-    // 더미 동영상 데이터 삽입
-    cy.reload(); // -> 새로고침
+    cy.reload();
     cy.get('[data-js="saved-clip-button-container__delete"]')
       .click()
       .then(() => {
