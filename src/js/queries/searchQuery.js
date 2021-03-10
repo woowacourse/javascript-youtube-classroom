d00f2d6d0bd1176d7ff1c6e51d284a31d2e6f1ab
taeeun <taeeun@taeeunui-MacBookAir.local>
2021-03-10 17:04:15 +0900

    fix: pageToken이 없는 경우 예외처리

--- src/js/queries/searchQuery.js
+++ src/js/queries/searchQuery.js
@@ -2,7 +2,7 @@ import { VIDEOS } from "../utils/constants.js";
 import YOUTUBE_API_KEY from "../config/youtubeAPI.js";
 
 export function getSearchQueryString(keyword, pageToken) {
-  const pageTokenQuery = pageToken && `pageToken=${pageToken}`;
+  const pageTokenQuery = pageToken ? `pageToken=${pageToken}` : "";
 
   return `part=snippet&key=${YOUTUBE_API_KEY}&q=${keyword}&maxResults=${VIDEOS.SEARCH_MAX_RESULT}&${pageTokenQuery}`;
 }
