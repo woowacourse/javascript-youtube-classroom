11e4705b12a4427d10dd17a82cfb782920f921d1
HyuuunjuKim <gus7wn@gmail.com>
2021-03-11 16:07:58 +0900

    refactor: handleMoreVidelLoad()를 intersectionObserver의 내부함수로 이동

--- src/js/states/intersectionObserver.js
+++ src/js/states/intersectionObserver.js
@@ -1,5 +1,9 @@
-import handleMoreVideoLoad from '../handlers/moreVideoLoad.js';
+import { fetchSearchResult } from '../API.js';
 import $ from '../utils/DOM.js';
+import { appendVideoList } from '../viewControllers/searchModal.js';
+import latestKeywords from './latestKeywords.js';
+import pageToken from './pageToken.js';
+import videoInfos from './videoInfos.js';
 
 const intersectionObserver = {
   value: {},
@@ -10,9 +14,7 @@ const intersectionObserver = {
   },
 
   init() {
-    this.set(
-      new IntersectionObserver(handleMoreVideoLoad.bind(this), this.options)
-    );
+    this.set(new IntersectionObserver(this.moreVideoLoad, this.options));
   },
 
   set(observer) {
@@ -29,6 +31,23 @@ const intersectionObserver = {
 
   observe($target) {
     this.value.observe($target);
+  },
+
+  async moreVideoLoad(entries) {
+    const [$lastVideo] = entries;
+    if (!$lastVideo.isIntersecting) return;
+
+    this.disconnect(); // 이전 마지막 비디오와 observer의 연결 해제
+
+    const { nextPageToken, items: loadVideoList } = await fetchSearchResult(
+      latestKeywords.getLastKeyword(),
+      pageToken.get()
+    );
+    pageToken.set(nextPageToken);
+    appendVideoList(loadVideoList, videoInfos.get());
+
+    const $newLastVideo = $('#video-search-result .js-video:last-child');
+    this.observe($newLastVideo);
   },
 };
 
