c498b868e049f30ce214470af716fa5b9e5ee237
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-03-11 17:57:45 +0900

    test:  저장된 영상의 watch button 동작을 테스트한다.
    - 기능 테스트와 레이아웃 테스트 분리

---
+++ cypress/integration/youtubeClassroom/watch/feature.spec.js
@@ -0,0 +1,56 @@
+describe("youtube classroom 기능 테스트", () => {
+  before(() => {
+    cy.visit("http://127.0.0.1:5500/");
+  });
+
+  it("저장버튼을 누르면 볼 영상에 추가된다.", () => {
+    cy.get("#search-button").click();
+    cy.get("#search-form > input").type("javascript");
+    cy.get("#search-form > button").click();
+
+    cy.get("#search-results button[data-video-id]")
+      .eq(0)
+      .click()
+      .then(([$clickedButton]) => {
+        return $clickedButton.dataset.videoId;
+      })
+      .then((savedVideoId) => {
+        cy.get("#search-modal-close").click();
+        cy.get("#watch-later-videos").children().should("have.length", 1);
+        cy.get("#watch-later-videos > article button")
+          .eq(0)
+          .then(([$watchedViewButton]) => {
+            expect($watchedViewButton.dataset.watchedButton).to.eq(
+              savedVideoId
+            );
+          });
+      });
+  });
+
+  it("watch 버튼을 누르면, 본 영상에 추가된다.", () => {
+    cy.get("#watch-later-videos button[data-watched-button]")
+      .eq(0)
+      .click()
+      .then(([$clickedButton]) => {
+        return $clickedButton.dataset.watchedButton;
+      })
+      .then((watchedVideoId) => {
+        cy.get("#watch-later-videos").should("not.be.visible");
+        cy.get("#not-saved").should("be.visible");
+
+        cy.get("#watched-view-button").click();
+        cy.get("#watched-videos").children().should("have.length", 1);
+        cy.get("#watched-videos > article button")
+          .eq(0)
+          .then(([$watchedViewButton]) => {
+            expect($watchedViewButton.dataset.watchedButton).to.eq(
+              watchedVideoId
+            );
+          });
+
+        cy.get("#watched-videos button[data-watched-button]")
+          .eq(0)
+          .should("not.have.class", "opacity-hover");
+      });
+  });
+});
--- cypress/integration/youtubeClassroom/watch/layout.spec.js
+++ cypress/integration/youtubeClassroom/watch/layout.spec.js
@@ -1,4 +1,4 @@
-describe("youtube classroom 기능 테스트", () => {
+describe("youtube classroom 레이아웃 테스트", () => {
   before(() => {
     cy.visit("http://127.0.0.1:5500/");
   });
@@ -8,40 +8,17 @@ describe("youtube classroom 기능 테스트", () => {
   });
 
   it("최초 프로그램 실행하면, 본 영상이 없다는 이미지를 보여준다.", () => {
-    cy.get("#watched-button").click();
+    cy.get("#watched-view-button").click();
     cy.get("#not-watched").should("be.visible");
   });
 
   it("선택된 볼 영상, 본 영상 버튼의 색깔이 바뀐다.", () => {
-    cy.get("#watch-later-button").click();
-    cy.get("#watched-button").should("not.have.class", "bg-cyan-100");
-    cy.get("#watch-later-button").should("have.class", "bg-cyan-100");
+    cy.get("#watch-later-view-button").click();
+    cy.get("#watched-view-button").should("not.have.class", "bg-cyan-100");
+    cy.get("#watch-later-view-button").should("have.class", "bg-cyan-100");
 
-    cy.get("#watched-button").click();
-    cy.get("#watch-later-button").should("not.have.class", "bg-cyan-100");
-    cy.get("#watched-button").should("have.class", "bg-cyan-100");
-  });
-
-  it("저장버튼을 누르면 볼 영상에 추가된다.", () => {
-    cy.get("#search-button").click();
-    cy.get("#search-form > input").type("javascript");
-    cy.get("#search-form > button").click();
-
-    cy.get("#search-results button[data-video-id]")
-      .eq(0)
-      .click()
-      .then(([$clickedButton]) => {
-        console.log($clickedButton);
-        return $clickedButton.dataset.videoId;
-      })
-      .then((savedVideoId) => {
-        cy.get("#search-modal-close").click();
-        cy.get("#watch-later-videos").children().should("have.length", 1);
-        cy.get("#watch-later-videos > article button")
-          .eq(0)
-          .then(([$watchedButton]) => {
-            expect($watchedButton.dataset.watchedButton).to.eq(savedVideoId);
-          });
-      });
+    cy.get("#watched-view-button").click();
+    cy.get("#watch-later-view-button").should("not.have.class", "bg-cyan-100");
+    cy.get("#watched-view-button").should("have.class", "bg-cyan-100");
   });
 });
