dc15f32b0f278b97995462395ad4aef1edec96a3
jho2301 <jho2301@gmail.com>
2021-03-13 18:35:31 +0900

    chore: 모듈 번들링

--- src/js/API.js
+++ src/js/API.js
@@ -1,9 +1,8 @@
-import API_KEY from './apiKey.js';
 import { YOUTUBE_BASE_URL } from './constants/API.js';
 import { FETCH_VIDEO_COUNT } from './constants/classroom.js';
 
 function fetchSearchResult(keyword, nextPageToken = '') {
-  const query = `part=snippet&order=viewCount&maxResults=${FETCH_VIDEO_COUNT}&key=${API_KEY}&pageToken=${nextPageToken}&q=${keyword}`;
+  const query = `part=snippet&order=viewCount&maxResults=${FETCH_VIDEO_COUNT}&key=${process.env.API_KEY}&pageToken=${nextPageToken}&q=${keyword}`;
 
   return fetch(`${YOUTUBE_BASE_URL}/search?${query}`)
     .then(data => data.json())
@@ -12,7 +11,7 @@ function fetchSearchResult(keyword, nextPageToken = '') {
 
 function fetchLatestVideoInfos(videoIds) {
   const videoIdString = videoIds.join('&id=');
-  const query = `part=snippet&id=${videoIdString}&key=${API_KEY}`;
+  const query = `part=snippet&id=${videoIdString}&key=${process.env.API_KEY}`;
 
   return fetch(`${YOUTUBE_BASE_URL}/videos?${query}`)
     .then(data => data.json())
