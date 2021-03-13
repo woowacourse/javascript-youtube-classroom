c06c6897994d544e6b919f7a479094591424ae39
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-03-14 05:57:43 +0900

    refactor: HTML 적절한 속성 값으로 수정 및 aria-* 속성 추가

--- src/js/templates/keywordHistoryTemplate.js
+++ src/js/templates/keywordHistoryTemplate.js
@@ -9,7 +9,7 @@ export default function getKeywordHistoryElement() {
       return `<li>
         <a class="chip">
           <div class="icon" data-keyword=${rejoinedKeyword}>${keyword}</div>
-          <button type="button" class="small-btn">
+          <button class="small-btn">
             <i class="fas fa-window-close ml-2 js-remove-btn" data-keyword=${rejoinedKeyword}></i>
           </button>
         </a>
