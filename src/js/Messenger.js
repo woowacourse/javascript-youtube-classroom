db09ef2803014b0089eec9187d5da2e2d2e1c7fe
bigsaigon333 <likepeppermint@gmail.com>
2021-03-14 16:58:23 +0900

    refactor: deliverMessage가 정해진 message만 받게 한다

--- src/js/Messenger.js
+++ src/js/Messenger.js
@@ -6,19 +6,26 @@ class Messenger {
   }
 
   deliverMessage(message, data) {
+    this.validateMessage(message);
+
     this.listeners[message].forEach((listener) => listener(data));
   }
 
   addMessageListener(message, messageHandler) {
-    if (!Object.keys(MESSAGE).includes(message)) {
-      throw new Error(`적합한 Message가 아닙니다. message: ${MESSAGE}`);
-    }
+    this.validateMessage(message);
 
     if (!this.listeners[message]) {
       this.listeners[message] = [];
     }
 
     this.listeners[message].push(messageHandler);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  validateMessage(message) {
+    if (!Object.keys(MESSAGE).includes(message)) {
+      throw new Error(`적합한 Message가 아닙니다. message: ${MESSAGE}`);
+    }
   }
 }
 
