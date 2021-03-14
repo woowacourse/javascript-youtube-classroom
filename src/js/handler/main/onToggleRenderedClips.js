b4eec1bc3e0eef524f8fcb209be5e697a39b6494
yujo <bedro27@gmail.com>
2021-03-14 17:52:14 +0900

    feat: 네비게이터의 버튼을 클릭했을 때 해당하는 클립들을 보여주는 기능

--- src/js/handler/main/onToggleRenderedClips.js
+++ src/js/handler/main/onToggleRenderedClips.js
@@ -1,9 +1,6 @@
 import $DOM from '../../utils/DOM.js';
 import storage from '../../utils/localStorage.js';
-import {
-  LOCAL_STORAGE_KEY,
-  LOCAL_STORAGE_VALUE,
-} from '../../utils/constant.js';
+import { LOCAL_STORAGE_KEY } from '../../utils/constant.js';
 
 const setSelected = (selectedButton) => {
   $DOM.NAVIGATOR.BUTTONS.forEach((button) =>
@@ -13,28 +10,27 @@ const setSelected = (selectedButton) => {
   selectedButton.classList.add('bg-cyan-100');
 };
 
+const setSection = (selected) => {
+  const sections = {
+    'navigator__unwatched-button': 'watched-section',
+    'navigator__watched-button': 'unwatched-section',
+    'navigator__like-button': 'like-section',
+  };
+
+  const savePageVideoWrapper = $DOM.SAVE_PAGE.VIDEO_WRAPPER;
+  savePageVideoWrapper.classList.remove('watched-section');
+  savePageVideoWrapper.classList.remove('unwatched-section');
+  savePageVideoWrapper.classList.remove('like-section');
+
+  savePageVideoWrapper.classList.add(sections[selected.dataset.js]);
+};
+
 export const onToggleRenderedClips = ({ target }) => {
   if (target.dataset.js === 'navigator__search-button') {
     return;
   }
 
-  const isWatchedButton = target.dataset.js === 'navigator__watched-button';
-  const savePageVideoWrapper = $DOM.SAVE_PAGE.VIDEO_WRAPPER;
-
   setSelected(target);
-
-  storage.set(
-    LOCAL_STORAGE_KEY.CURRENT_TAB,
-    isWatchedButton
-      ? LOCAL_STORAGE_VALUE.WATCHED
-      : LOCAL_STORAGE_VALUE.UNWATCHED,
-  );
-
-  if (isWatchedButton) {
-    savePageVideoWrapper.classList.add('watched-section');
-    savePageVideoWrapper.classList.remove('unwatched-section');
-    return;
-  }
-  savePageVideoWrapper.classList.add('unwatched-section');
-  savePageVideoWrapper.classList.remove('watched-section');
+  setSection(target);
+  storage.set(LOCAL_STORAGE_KEY.CURRENT_TAB, target.dataset.js);
 };
