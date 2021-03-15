ff5893971613ede46f00f8340dd05ad7563617a4
Tanney <dhxodms89@gmail.com>
2021-03-15 12:50:18 +0900

    test: 좋아요한 영상 버튼을 누르면 색이 변하고 no-liked 이미지를 보여준다.

--- cypress/integration/youtubeClassroom/watch/layout.spec.js
+++ cypress/integration/youtubeClassroom/watch/layout.spec.js
@@ -12,13 +12,29 @@ describe("youtube classroom 레이아웃 테스트", () => {
     cy.get("#not-watched").should("be.visible");
   });
 
-  it("선택된 볼 영상, 본 영상 버튼의 색깔이 바뀐다.", () => {
-    cy.get("#watch-later-view-button").click();
-    cy.get("#watched-view-button").should("not.have.class", "bg-cyan-100");
-    cy.get("#watch-later-view-button").should("have.class", "bg-cyan-100");
+  it("최초 프로그램 실행하면, 좋아요한 영상이 없다는 이미지를 보여준다.", () => {
+    cy.get("#liked-view-button").click();
+    cy.get("#no-liked").should("be.visible");
+  });
 
+  it("선택된 볼 영상 버튼의 색깔이 바뀐다.", () => {
+    cy.get("#watch-later-view-button").click();
+    cy.get("#watch-later-view-button").should("have.class", "bg-cyan-100");
+    cy.get("#watched-view-button").should("not.have.class", "bg-cyan-100");
+    cy.get("#liked-view-button").should("not.have.class", "by-cyan-100");
+  });
+
+  it("선택된 본 영상 버튼의 색깔이 바뀐다.", () => {
     cy.get("#watched-view-button").click();
     cy.get("#watch-later-view-button").should("not.have.class", "bg-cyan-100");
     cy.get("#watched-view-button").should("have.class", "bg-cyan-100");
+    cy.get("#liked-view-button").should("not.have.class", "by-cyan-100");
+  });
+
+  it("선택된 좋아요 한 영상 버튼의 색깔이 바뀐다.", () => {
+    cy.get("#liked-view-button").click();
+    cy.get("#watch-later-view-button").should("not.have.class", "bg-cyan-100");
+    cy.get("#watched-view-button").should("not.have.class", "bg-cyan-100");
+    cy.get("#liked-view-button").should("have.class", "bg-cyan-100");
   });
 });
