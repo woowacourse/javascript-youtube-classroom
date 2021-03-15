ac714a0c931c252b861d16fc07ee5b5d23c6d829
Tanney <dhxodms89@gmail.com>
2021-03-15 16:30:44 +0900

    feat: 본영상과 볼영상에서 좋아요 버튼을 누르면 좋아요한 영상에 추가된다.

--- src/js/youtube-class-room/watch/WatchController.js
+++ src/js/youtube-class-room/watch/WatchController.js
@@ -1,15 +1,41 @@
 import WatchView from "./WatchView.js";
 
-import { CONFIRM_MESSAGE } from "../../utils/constants.js";
+import { showSnackbar } from "../../utils/snackbar.js";
+import {
+  CONFIRM_MESSAGE,
+  ERROR_MESSAGE,
+  SUCCESS_MESSAGE,
+  VIDEO_VIEW_NAME,
+} from "../../utils/constants.js";
 
 import videos from "../../state/videos.js";
+import videoViewIndex from "../../state/videoViewIndex.js";
+
 export default class WatchController {
   constructor() {
     this.watchView = new WatchView();
   }
 
-  updateWatchLaterView(savedVideos) {
-    const watchLaterVideos = savedVideos.filter((video) => !video.watched);
+  updateCurrentView() {
+    const currentViewName = videoViewIndex.getCurrentViewName();
+
+    switch (currentViewName) {
+      case VIDEO_VIEW_NAME.WATCH_LATER:
+        this.updateWatchLaterView();
+        break;
+      case VIDEO_VIEW_NAME.WATCHED:
+        this.updateWatchedView();
+        break;
+      case VIDEO_VIEW_NAME.LIKED:
+        this.updateLikedView();
+        break;
+    }
+  }
+
+  updateWatchLaterView() {
+    const watchLaterVideos = videos
+      .getSavedVideos()
+      .filter((video) => !video.watched);
 
     this.watchView.markWatchLaterViewButton();
 
@@ -20,8 +46,10 @@ export default class WatchController {
     }
   }
 
-  updateWatchedView(savedVideos) {
-    const watchedVideos = savedVideos.filter((video) => video.watched);
+  updateWatchedView() {
+    const watchedVideos = videos
+      .getSavedVideos()
+      .filter((video) => video.watched);
 
     this.watchView.markWatchedViewButton();
 
@@ -32,8 +60,8 @@ export default class WatchController {
     }
   }
 
-  updateLikedView(savedVideos) {
-    const likedVideos = savedVideos.filter((video) => video.liked);
+  updateLikedView() {
+    const likedVideos = videos.getSavedVideos().filter((video) => video.liked);
 
     this.watchView.markLikedViewButton();
 
@@ -45,11 +73,32 @@ export default class WatchController {
   }
 
   watchVideo(videoId) {
-    videos.setVideoWatched(videoId, true);
+    const succeed = videos.setVideoWatched(videoId, true);
+
+    succeed
+      ? showSnackbar(SUCCESS_MESSAGE.WATCH_VIDEO)
+      : showSnackbar(ERROR_MESSAGE.INVALID_ACTION_ERROR);
   }
 
   clearWatchedViedoLog(videoId) {
-    videos.setVideoWatched(videoId, false);
+    const succeed = videos.setVideoWatched(videoId, false);
+
+    succeed
+      ? showSnackbar(SUCCESS_MESSAGE.CLEAR_WATCH_VIDEO_LOG)
+      : showSnackbar(ERROR_MESSAGE.INVALID_ACTION_ERROR);
+  }
+
+  toggleLike(videoId) {
+    const liked = videos
+      .getSavedVideos()
+      .some((video) => video.videoId === videoId && video.liked);
+    const succeed = videos.setVideoLiked(videoId, !liked);
+
+    succeed
+      ? showSnackbar(
+          liked ? SUCCESS_MESSAGE.DISLIKE_VIDEO : SUCCESS_MESSAGE.LIKE_VIDEO
+        )
+      : showSnackbar(ERROR_MESSAGE.LIKE_ERROR);
   }
 
   deleteVideo(videoId) {
