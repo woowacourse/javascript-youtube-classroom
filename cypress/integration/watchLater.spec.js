73a7a7a8142f68102af946a6cf9a904b3b249964
bigsaigon333 <likepeppermint@gmail.com>
2021-03-21 01:20:19 +0900

    test: 볼 영상, 본 영상이 없는 경우에 표시하는 이미지 경로를 수정한다

--- cypress/integration/watchLater.spec.js
+++ cypress/integration/watchLater.spec.js
@@ -19,6 +19,10 @@ describe("볼 영상 화면을 테스트한다.", () => {
         pageToken: REGULAR_EXPRESSION.NOT_EMPTY_STRING,
       },
     }).as("searchFromScroll");
+
+    cy.window().then((window) => {
+      cy.stub(window, "confirm").returns(true).as("window:confirm");
+    });
   });
 
   const search = (keyword) => {
@@ -29,7 +33,7 @@ describe("볼 영상 화면을 테스트한다.", () => {
   };
 
   it("가장 처음에 저장된 영상이 없는 경우, 비어있다는 것을 사용자에게 알려준다,", () => {
-    cy.get(`.${CLASSNAME.NO_SAVED_VIDEO_IMAGE}`).should("be.visible");
+    cy.get(`.${CLASSNAME.NO_WATCHING_VIDEO_IMAGE}`).should("be.visible");
   });
 
   it("저장된 영상이 있는 경우, 비어있지 않다는 것을 사용자에게 알려준다,", () => {
@@ -41,7 +45,7 @@ describe("볼 영상 화면을 테스트한다.", () => {
     cy.wait("@searchFromKeyword");
 
     cy.get(`.${CLASSNAME.SAVE_VIDEO_BUTTON}`).first().click();
-    cy.get(`.${CLASSNAME.NO_SAVED_VIDEO_IMAGE}`).should("not.be.visible");
+    cy.get(`.${CLASSNAME.NO_WATCHING_VIDEO_IMAGE}`).should("not.be.visible");
   });
 
   it("검색된 영상을 저장하면, 해당 영상이 볼 영상 화면에 표시된다.", () => {
@@ -59,7 +63,7 @@ describe("볼 영상 화면을 테스트한다.", () => {
       });
 
     cy.get(`.${CLASSNAME.MODAL_CLOSE}`).click();
-    cy.get(`.${CLASSNAME.NO_SAVED_VIDEO_IMAGE}`).should("not.be.visible");
+    cy.get(`.${CLASSNAME.NO_WATCHING_VIDEO_IMAGE}`).should("not.be.visible");
     cy.get(`.${CLASSNAME.WATCH_LATER_VIDEO_WRAPPER}`)
       .find("iframe")
       .invoke("attr", "src")
@@ -79,10 +83,11 @@ describe("볼 영상 화면을 테스트한다.", () => {
       .should("have.length", 1);
 
     cy.get(`.${CLASSNAME.DELETE_ICON}`).first().click();
+    cy.get("@window:confirm").should("be.called");
+
     cy.get(`.${CLASSNAME.WATCH_LATER_VIDEO_WRAPPER}`)
       .children()
       .should("have.length", 0);
-    cy.get(`.${CLASSNAME.NO_SAVED_VIDEO_IMAGE}`).should("be.visible");
   });
 
   it("✅ 버튼을 누르면, 해당 영상이 볼 영상 리스트에서 삭제되고, ✅ 본 영상 화면에 나타난다", () => {
@@ -96,7 +101,7 @@ describe("볼 영상 화면을 테스트한다.", () => {
     cy.get(`.${CLASSNAME.WATCH_LATER_VIDEO_WRAPPER}`)
       .children()
       .should("have.length", 0);
-    cy.get(`.${CLASSNAME.NO_SAVED_VIDEO_IMAGE}`).should("be.visible");
+    cy.get(`.${CLASSNAME.NO_WATCHING_VIDEO_IMAGE}`).should("be.visible");
 
     cy.get(`.${CLASSNAME.HISTORY_TAB}`).click();
     cy.get(`.${CLASSNAME.HISTORY_VIDEO_WRAPPER}`)
