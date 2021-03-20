87ffe779f2c3a2d1c89f235a90773723af3a39aa
seojihwan <seojihwan213@naver.com>
2021-03-20 09:53:16 +0900

    fix: 비디오 영상 필터링 수정

--- src/js/handler/shared/isEmptyDisplayClipCountFromCurrentTab.js
+++ src/js/handler/shared/isEmptyDisplayClipCountFromCurrentTab.js
@@ -1,9 +1,9 @@
 export const isEmptyDisplayClipCountFromCurrentTab = {
   '볼 영상': (savedClips) =>
-    Object.keys(savedClips).filter((id) => savedClips[id].isWatched).length ===
+    Object.keys(savedClips).filter((id) => !savedClips[id].isWatched).length ===
     0,
   '본 영상': (savedClips) =>
-    Object.keys(savedClips).filter((id) => !savedClips[id].isWatched).length ===
+    Object.keys(savedClips).filter((id) => savedClips[id].isWatched).length ===
     0,
   '좋아하는 영상': (savedClips) =>
     Object.keys(savedClips).filter((id) => savedClips[id].isLiked).length === 0,
