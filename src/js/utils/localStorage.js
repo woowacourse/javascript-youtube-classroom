a85d7624cab8a1e6b9758e2443e8410df64ce3d5
seojihwan <seojihwan213@naver.com>
2021-03-25 02:19:11 +0900

    fix: 잘못 작성된 localstorgae value type 조건 수정

--- src/js/utils/localStorage.js
+++ src/js/utils/localStorage.js
@@ -6,7 +6,7 @@ const isObject = (value) => typeof value === 'object' && value !== null;
 
 export default {
   set: (key, value) => {
-    if (!isString(value) || !isArray(value) || !isObject(value)) {
+    if (!(isString(value) || isArray(value) || isObject(value))) {
       throw new Error(
         MESSAGE.ERROR.ONLY_STRING_OR_ARRAY_CAN_BE_STORED_IN_LOCAL_STORAGE,
       );
