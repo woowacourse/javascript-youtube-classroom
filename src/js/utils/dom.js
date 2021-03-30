8b4eb31fb9da112a404ccd01c432056963ddfae0
2sooy <1401660@naver.com>
2021-03-31 02:45:59 +0900

    refactor: snackbar 띄우는 작업 throttle 적용

--- src/js/utils/dom.js
+++ src/js/utils/dom.js
@@ -3,10 +3,18 @@ import { STANDARD_NUMS } from "./constants.js";
 export const $ = selector => document.querySelector(selector);
 export const $$ = selector => document.querySelectorAll(selector);
 
-export const popMessage = ($target, message) => {
-  $target.innerText = message;
-  $target.classList.toggle("show");
-  setTimeout(() => {
-    $target.classList.toggle("show");
-  }, STANDARD_NUMS.SNACKBAR_DELAY);
-};
+export const popMessage = (() => {
+  let timerId = null;
+
+  return ($target, message) => {
+    $target.innerText = message;
+
+    if (!timerId) {
+      $target.classList.toggle("show");
+      timerId = setTimeout(() => {
+        timerId = null;
+        $target.classList.toggle("show");
+      }, STANDARD_NUMS.SNACKBAR_DELAY);
+    }
+  };
+})();
