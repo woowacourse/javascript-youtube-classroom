2a631679652e9629194e51f6a04f75c316863ea0
권세진 <0307kwon@gmail.com>
2021-04-01 16:22:32 +0900

    refactor: 상수 네이밍을 조금 더 알맞게 수정한다

--- src/constants.js
+++ src/constants.js
@@ -79,16 +79,10 @@ export const CONFIRM_MESSAGE = Object.freeze({
 });
 
 export const SNACKBAR_MESSAGE = Object.freeze({
-  //TODO: CHECK를 SAVE로 바꾸기
-  WATCHED_VIDEO_CHECK_SUCCESS: '본 영상으로 저장되었습니다.',
-
-  WATCHING_VIDEO_CHECK_SUCCESS: '볼 영상으로 이동되었습니다.',
+  WATCHED_VIDEO_SAVE_SUCCESS: '본 영상으로 저장되었습니다.',
   WATCHING_VIDEO_SAVE_SUCCESS: '볼 영상으로 이동되었습니다.',
-
   FAVORITE_VIDEO_SAVE_SUCCESS: '좋아요 한 영상으로 저장되었습니다.',
-
   VIDEO_DELETE_SUCCESS: '영상에서 삭제되었습니다.',
-
   SAVE_LIMIT_EXCEEDED: `${SETTINGS.MAX_SAVE_COUNT}개 보다 많은 영상을 저장할 수 없습니다.`,
 });
 
--- src/storage.js
+++ src/storage.js
@@ -4,11 +4,9 @@ import PrevSearchResultStorage from './storage/PrevSearchResultStorage.js';
 import ArrayStorage from './storage/ArrayStorage.js';
 
 const video = new VideoStorage(LOCAL_STORAGE_KEY.VIDEOS);
-
 const prevSearchResult = new PrevSearchResultStorage(
   LOCAL_STORAGE_KEY.PREV_SEARCH_RESULT
 );
-
 const searchQuery = new ArrayStorage(LOCAL_STORAGE_KEY.SEARCH_QUERIES);
 
 const storage = {
