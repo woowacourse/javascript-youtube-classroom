69d6326a279a6632f6856546c39ee2795e45937e
shinsehantan <shinsehantan@users.noreply.github.com>
2021-04-03 17:47:15 +0900

    test: cypress의 beforeEach에 로컬스토리지 초기화 로직 추가

--- cypress/integration/video_view_section.spec.js
+++ cypress/integration/video_view_section.spec.js
@@ -5,6 +5,7 @@ import { STORAGE, CLASS_NAME, STANDARD_NUMS, COLOR } from "../../src/js/utils/co
 context("저장된 비디오 관리", () => {
   beforeEach(() => {
     cy.visit("http://127.0.0.1:5500");
+    cy.clearLocalStorage(STORAGE.SAVED_VIDEOS);
   });
 
   it("초기에 저장된 비디오가 존재하지 않는다면, 비어있다는 것을 사용자에게 알려주는 이미지를 보여준다.", () => {
