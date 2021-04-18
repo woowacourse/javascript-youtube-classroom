e8f47cd81fa139b70d04b8a0685e409625190678
권세진 <0307kwon@gmail.com>
2021-04-18 22:00:32 +0900

    refactor: 필요없는 파일을 삭제한다

--- src/service.js
+++
@@ -1,9 +0,0 @@
-import modalService from './service/modalService';
-import videoService from './service/videoService';
-
-const service = {
-  modal: modalService,
-  video: videoService,
-};
-
-export default service;
