8238d7b50913ba1486b4a791f7fb70c008433431
kkojae <jaejeung3210@gmail.com>
2022-03-08 21:20:39 +0900

    chore: 바벨 폴리필 설정
    - @babel/plugin-transform-runtime 설치
    - @babel/runtime 설치

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,7 @@ module.exports = {
             loader: "babel-loader",
             options: {
               presets: ["@babel/preset-env"],
+              plugins: ["@babel/transform-runtime"],
             },
           },
         ],
