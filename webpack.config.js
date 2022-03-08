61d0d91170a8df2a09b343896dbd8aefcf6c0b42
prefer2 <sunho6200@gmail.com>
2022-03-08 15:36:05 +0900

    chore: webpack 설정
    Co-authored-by: Soyi Jeon <soyi.dev@gmail.com>

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  resolve: {
+    extensions: [".js", ".css"],
+  },
+  devServer: {
+    port: 9000,
+  },
+  devtool: "source-map",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+  ],
+};
