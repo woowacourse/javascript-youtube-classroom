cef3c9d01fda058251eb9a59ed95bcb4525f4d0c
yunjin-kim <79692272+yunjin-kim@users.noreply.github.com>
2022-03-08 18:30:29 +0900

    feat:검색결과 mock 데이터 추가
    Co-authored-by: TaeYoon Kim <uni613@naver.com>

---
+++ src/js/utils/mock.js
@@ -0,0 +1,224 @@
+const mockDatas = [
+  {
+    etag: '13LgdvBoYOy6x3R7D21_y5pZvts',
+    id: {
+      kind: 'youtube#video',
+      videoId: '6PmRRLrLDwQ',
+    },
+    kind: 'youtube#searchResult',
+    snippet: {
+      channelId: 'UCK8S6QMrTk1G8TYQwIyDo-w',
+      channelTitle: 'LQ KPOP',
+      description: '',
+      liveBroadcastContent: 'none',
+      publishTime: '2022-02-11T05:57:06Z',
+      publishedAt: '2022-02-11T05:57:06Z',
+    },
+    thumbnails: {
+      default: { url: 'https://i.ytimg.com/vi/6PmRRLrLDwQ/default.jpg', width: 120, height: 90 },
+      igh: { url: 'https://i.ytimg.com/vi/6PmRRLrLDwQ/hqdefault.jpg', width: 480, height: 360 },
+      medium: { url: 'https://i.ytimg.com/vi/6PmRRLrLDwQ/mqdefault.jpg', width: 320, height: 180 },
+    },
+    title: 'B T S PLAYLIST 2022 UPDATED | 방탄소년단 노래 모음',
+  },
+  {
+    etag: '13LgdvBoYOy6x3R7D21_y5pZvts',
+    id: {
+      kind: 'youtube#video',
+      videoId: '6PmRRLrLDwQ',
+    },
+    kind: 'youtube#searchResult',
+    snippet: {
+      channelId: 'UCK8S6QMrTk1G8TYQwIyDo-w',
+      channelTitle: 'LQ KPOP',
+      description: '',
+      liveBroadcastContent: 'none',
+      publishTime: '2022-02-11T05:57:06Z',
+      publishedAt: '2022-02-11T05:57:06Z',
+    },
+    thumbnails: {
+      default: { url: 'https://i.ytimg.com/vi/6PmRRLrLDwQ/default.jpg', width: 120, height: 90 },
+      igh: { url: 'https://i.ytimg.com/vi/6PmRRLrLDwQ/hqdefault.jpg', width: 480, height: 360 },
+      medium: { url: 'https://i.ytimg.com/vi/6PmRRLrLDwQ/mqdefault.jpg', width: 320, height: 180 },
+    },
+    title: 'B T S PLAYLIST 2022 UPDATED | 방탄소년단 노래 모음',
+  },
+  {
+    etag: '13LgdvBoYOy6x3R7D21_y5pZvts',
+    id: {
+      kind: 'youtube#video',
+      videoId: '6PmRRLrLDwQ',
+    },
+    kind: 'youtube#searchResult',
+    snippet: {
+      channelId: 'UCK8S6QMrTk1G8TYQwIyDo-w',
+      channelTitle: 'LQ KPOP',
+      description: '',
+      liveBroadcastContent: 'none',
+      publishTime: '2022-02-11T05:57:06Z',
+      publishedAt: '2022-02-11T05:57:06Z',
+    },
+    thumbnails: {
+      default: { url: 'https://i.ytimg.com/vi/6PmRRLrLDwQ/default.jpg', width: 120, height: 90 },
+      igh: { url: 'https://i.ytimg.com/vi/6PmRRLrLDwQ/hqdefault.jpg', width: 480, height: 360 },
+      medium: { url: 'https://i.ytimg.com/vi/6PmRRLrLDwQ/mqdefault.jpg', width: 320, height: 180 },
+    },
+    title: 'B T S PLAYLIST 2022 UPDATED | 방탄소년단 노래 모음',
+  },
+  {
+    etag: '13LgdvBoYOy6x3R7D21_y5pZvts',
+    id: {
+      kind: 'youtube#video',
+      videoId: '6PmRRLrLDwQ',
+    },
+    kind: 'youtube#searchResult',
+    snippet: {
+      channelId: 'UCK8S6QMrTk1G8TYQwIyDo-w',
+      channelTitle: 'LQ KPOP',
+      description: '',
+      liveBroadcastContent: 'none',
+      publishTime: '2022-02-11T05:57:06Z',
+      publishedAt: '2022-02-11T05:57:06Z',
+    },
+    thumbnails: {
+      default: { url: 'https://i.ytimg.com/vi/6PmRRLrLDwQ/default.jpg', width: 120, height: 90 },
+      igh: { url: 'https://i.ytimg.com/vi/6PmRRLrLDwQ/hqdefault.jpg', width: 480, height: 360 },
+      medium: { url: 'https://i.ytimg.com/vi/6PmRRLrLDwQ/mqdefault.jpg', width: 320, height: 180 },
+    },
+    title: 'B T S PLAYLIST 2022 UPDATED | 방탄소년단 노래 모음',
+  },
+  {
+    etag: '13LgdvBoYOy6x3R7D21_y5pZvts',
+    id: {
+      kind: 'youtube#video',
+      videoId: '6PmRRLrLDwQ',
+    },
+    kind: 'youtube#searchResult',
+    snippet: {
+      channelId: 'UCK8S6QMrTk1G8TYQwIyDo-w',
+      channelTitle: 'LQ KPOP',
+      description: '',
+      liveBroadcastContent: 'none',
+      publishTime: '2022-02-11T05:57:06Z',
+      publishedAt: '2022-02-11T05:57:06Z',
+    },
+    thumbnails: {
+      default: { url: 'https://i.ytimg.com/vi/6PmRRLrLDwQ/default.jpg', width: 120, height: 90 },
+      igh: { url: 'https://i.ytimg.com/vi/6PmRRLrLDwQ/hqdefault.jpg', width: 480, height: 360 },
+      medium: { url: 'https://i.ytimg.com/vi/6PmRRLrLDwQ/mqdefault.jpg', width: 320, height: 180 },
+    },
+    title: 'B T S PLAYLIST 2022 UPDATED | 방탄소년단 노래 모음',
+  },
+  {
+    etag: '13LgdvBoYOy6x3R7D21_y5pZvts',
+    id: {
+      kind: 'youtube#video',
+      videoId: '6PmRRLrLDwQ',
+    },
+    kind: 'youtube#searchResult',
+    snippet: {
+      channelId: 'UCK8S6QMrTk1G8TYQwIyDo-w',
+      channelTitle: 'LQ KPOP',
+      description: '',
+      liveBroadcastContent: 'none',
+      publishTime: '2022-02-11T05:57:06Z',
+      publishedAt: '2022-02-11T05:57:06Z',
+    },
+    thumbnails: {
+      default: { url: 'https://i.ytimg.com/vi/6PmRRLrLDwQ/default.jpg', width: 120, height: 90 },
+      igh: { url: 'https://i.ytimg.com/vi/6PmRRLrLDwQ/hqdefault.jpg', width: 480, height: 360 },
+      medium: { url: 'https://i.ytimg.com/vi/6PmRRLrLDwQ/mqdefault.jpg', width: 320, height: 180 },
+    },
+    title: 'B T S PLAYLIST 2022 UPDATED | 방탄소년단 노래 모음',
+  },
+  {
+    etag: '13LgdvBoYOy6x3R7D21_y5pZvts',
+    id: {
+      kind: 'youtube#video',
+      videoId: '6PmRRLrLDwQ',
+    },
+    kind: 'youtube#searchResult',
+    snippet: {
+      channelId: 'UCK8S6QMrTk1G8TYQwIyDo-w',
+      channelTitle: 'LQ KPOP',
+      description: '',
+      liveBroadcastContent: 'none',
+      publishTime: '2022-02-11T05:57:06Z',
+      publishedAt: '2022-02-11T05:57:06Z',
+    },
+    thumbnails: {
+      default: { url: 'https://i.ytimg.com/vi/6PmRRLrLDwQ/default.jpg', width: 120, height: 90 },
+      igh: { url: 'https://i.ytimg.com/vi/6PmRRLrLDwQ/hqdefault.jpg', width: 480, height: 360 },
+      medium: { url: 'https://i.ytimg.com/vi/6PmRRLrLDwQ/mqdefault.jpg', width: 320, height: 180 },
+    },
+    title: 'B T S PLAYLIST 2022 UPDATED | 방탄소년단 노래 모음',
+  },
+  {
+    etag: '13LgdvBoYOy6x3R7D21_y5pZvts',
+    id: {
+      kind: 'youtube#video',
+      videoId: '6PmRRLrLDwQ',
+    },
+    kind: 'youtube#searchResult',
+    snippet: {
+      channelId: 'UCK8S6QMrTk1G8TYQwIyDo-w',
+      channelTitle: 'LQ KPOP',
+      description: '',
+      liveBroadcastContent: 'none',
+      publishTime: '2022-02-11T05:57:06Z',
+      publishedAt: '2022-02-11T05:57:06Z',
+    },
+    thumbnails: {
+      default: { url: 'https://i.ytimg.com/vi/6PmRRLrLDwQ/default.jpg', width: 120, height: 90 },
+      igh: { url: 'https://i.ytimg.com/vi/6PmRRLrLDwQ/hqdefault.jpg', width: 480, height: 360 },
+      medium: { url: 'https://i.ytimg.com/vi/6PmRRLrLDwQ/mqdefault.jpg', width: 320, height: 180 },
+    },
+    title: 'B T S PLAYLIST 2022 UPDATED | 방탄소년단 노래 모음',
+  },
+  {
+    etag: '13LgdvBoYOy6x3R7D21_y5pZvts',
+    id: {
+      kind: 'youtube#video',
+      videoId: '6PmRRLrLDwQ',
+    },
+    kind: 'youtube#searchResult',
+    snippet: {
+      channelId: 'UCK8S6QMrTk1G8TYQwIyDo-w',
+      channelTitle: 'LQ KPOP',
+      description: '',
+      liveBroadcastContent: 'none',
+      publishTime: '2022-02-11T05:57:06Z',
+      publishedAt: '2022-02-11T05:57:06Z',
+    },
+    thumbnails: {
+      default: { url: 'https://i.ytimg.com/vi/6PmRRLrLDwQ/default.jpg', width: 120, height: 90 },
+      igh: { url: 'https://i.ytimg.com/vi/6PmRRLrLDwQ/hqdefault.jpg', width: 480, height: 360 },
+      medium: { url: 'https://i.ytimg.com/vi/6PmRRLrLDwQ/mqdefault.jpg', width: 320, height: 180 },
+    },
+    title: 'B T S PLAYLIST 2022 UPDATED | 방탄소년단 노래 모음',
+  },
+  {
+    etag: '13LgdvBoYOy6x3R7D21_y5pZvts',
+    id: {
+      kind: 'youtube#video',
+      videoId: '6PmRRLrLDwQ',
+    },
+    kind: 'youtube#searchResult',
+    snippet: {
+      channelId: 'UCK8S6QMrTk1G8TYQwIyDo-w',
+      channelTitle: 'LQ KPOP',
+      description: '',
+      liveBroadcastContent: 'none',
+      publishTime: '2022-02-11T05:57:06Z',
+      publishedAt: '2022-02-11T05:57:06Z',
+    },
+    thumbnails: {
+      default: { url: 'https://i.ytimg.com/vi/6PmRRLrLDwQ/default.jpg', width: 120, height: 90 },
+      igh: { url: 'https://i.ytimg.com/vi/6PmRRLrLDwQ/hqdefault.jpg', width: 480, height: 360 },
+      medium: { url: 'https://i.ytimg.com/vi/6PmRRLrLDwQ/mqdefault.jpg', width: 320, height: 180 },
+    },
+    title: 'B T S PLAYLIST 2022 UPDATED | 방탄소년단 노래 모음',
+  },
+];
+
+export default mockDatas;
