acc8d36dc00ab744bcc1254fa098a5cab30bce8f
최광희 <polyckh@gmail.com>
2022-03-08 17:13:26 +0900

    feat: 검색 기능 이벤트 바인딩
    feat: enter 키 이벤트 바인딩
    
    Co-authored-by: intae92 <intae92@users.noreply.github.com>

--- src/js/App.js
+++ src/js/App.js
@@ -15,6 +15,15 @@ class App {
 
     const dimmer = document.querySelector('.dimmer');
     dimmer.addEventListener('click', this.closeModalHandler.bind(this));
+
+    const searchButton = document.querySelector('#search-button');
+    searchButton.addEventListener('click', this.searchHandler.bind(this));
+
+    const searchInputKeyword = document.querySelector('#search-input-keyword');
+    searchInputKeyword.addEventListener(
+      'keypress',
+      this.enterKeyHandler.bind(this)
+    );
   }
 
   openModalHandler() {
@@ -25,7 +34,26 @@ class App {
   closeModalHandler() {
     const modalContainer = document.querySelector('.modal-container');
     modalContainer.classList.add('hide');
-    console.log('close');
+  }
+
+  searchHandler(e) {
+    e.preventDefault();
+    const searchInputKeyword = document.querySelector('#search-input-keyword');
+    const keyword = searchInputKeyword.value;
+
+    if (keyword.trim().length === 0) {
+      console.log('empty search keyword');
+      return;
+    }
+    console.log('search keyword:', keyword);
+  }
+
+  enterKeyHandler(e) {
+    const keycode = e.keyCode;
+    const enterKeyCode = 13;
+    if (keycode === enterKeyCode) {
+      this.searchHandler(e);
+    }
   }
 }
 
