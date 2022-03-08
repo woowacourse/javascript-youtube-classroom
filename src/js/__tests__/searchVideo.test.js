98a0e6e0a306efc73a05d1753d1e4c59e8387a1e
yunjin-kim <79692272+yunjin-kim@users.noreply.github.com>
2022-03-08 20:49:22 +0900

    test: 검색한 결과 테스트 작성
    Co-authored-by: TaeYoon Kim <uni613@naver.com>

--- src/js/__tests__/searchVideo.test.js
+++ src/js/__tests__/searchVideo.test.js
@@ -2,9 +2,10 @@
 //   - [ ] 공백인 경우를 체크한다.
 //   - [ ] 검색해서 받아 오는지 확인한다.
 
-const axios = require('axios').default;
+// const axios = require('axios').default;
 
 import SearchVideo from '../searchVideo.js';
+import mockDatas from '../Utils/mock.js';
 
 const validateSearchInput = (searchInput) => {
   if (!searchInput) {
@@ -12,18 +13,27 @@ const validateSearchInput = (searchInput) => {
   }
 };
 
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve({ items: mockDatas }),
+  })
+);
+
 describe('동영샹 검색 테스트', () => {
   test('입력한 검색값이 공백인 경우를 체크한다.', () => {
     const searchInput = '      ';
     expect(() => validateSearchInput(searchInput.trim())).toThrow('공백은 검색할 수 없습니다.');
   });
   
-  // const searchVideo = new SearchVideo();
-  // test('검색해서 받아 오는지 확인한다.', () => {
-  //   const searchResults = searchVideo.searchResults;
-  //   console.log(searchResults);
-  //   expect(searchResults.length).toBe(10);
-  // });
+  
+  test('검색하면 검색한 데이터(10개)가 오는지 확인한다.', () => {
+    const searchVideo = new SearchVideo();
+    searchVideo.getYoutubeVideos().then((searchResults) => {
+      expect(searchResults.length).toEqual(10);
+    }).catch((error) => {
+      expect(error).toEqual('[404] 개발자에게 문의하세요');
+    });
+  });
 });
 
 
