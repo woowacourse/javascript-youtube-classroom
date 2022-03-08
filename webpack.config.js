00c1805d6ade2cc25b2a6bc70e02c6cee6f3b1e1
Ahn <jin7969@gmail.com>
2022-03-08 17:17:40 +0900

    chore: webpack, eslint, prettier 환경 설정
    Co-authored-by: cks3066 <cks3066@users.noreply.github.com>
    Co-authored-by: liswktjs <liswktjs@users.noreply.github.com>

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/js/index.js',
+  resolve: {
+    extensions: ['.js', '.css'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+  ],
+};
