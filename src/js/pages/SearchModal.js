17dc7529081b3c741d03ad4c1579a7723f84cbf2
최광희 <polyckh@gmail.com>
2022-03-08 21:13:25 +0900

    feat: 응답 받은 결과 값으로 화면에 출력
    Co-authored-by: intae92 <intae92@users.noreply.github.com>

--- src/js/pages/SearchModal.js
+++ src/js/pages/SearchModal.js
@@ -6,6 +6,25 @@ const validateKeyword = (keyword) => {
   }
 };
 
+const URL = 'https://www.googleapis.com/youtube/v3/search?';
+
+const KEY = 'AIzaSyDw-RH8HlDQGae-opd0hT1-N2pnbIYejO0';
+
+const option = {
+  part: 'snippet',
+  maxResults: 10,
+  order: 'date',
+  key: KEY,
+};
+
+const stringQuery = (url, keyword, options) => {
+  const query = Object.entries(options).reduce(
+    (acc, [key, value]) => (acc += `${key}=${value}&`),
+    `${url}q=${keyword}&`
+  );
+  return query;
+};
+
 export default class SearchModal {
   constructor(element) {
     this.element = element;
@@ -34,28 +53,38 @@ export default class SearchModal {
       '#search-error-message'
     );
 
-    const URL = 'https://www.googleapis.com/youtube/v3/search?';
-    const KEY = 'AIzaSyDw-RH8HlDQGae-opd0hT1-N2pnbIYejO0';
-    const option = {
-      part: 'snippet',
-      maxResults: 10,
-      order: 'date',
-      key: KEY,
-    };
-
-    const stringQuery = (url, options) => {
-      const query = Object.entries(options).reduce(
-        (acc, [key, value]) => (acc += `${key}=${value}&`),
-        url
-      );
-      return query;
-    };
-
     try {
       validateKeyword(keyword);
 
-      const result = await fetch(stringQuery(URL, option));
+      const result = await fetch(stringQuery(URL, keyword, option));
       const json = await result.json();
+      this.element.querySelector('.video-list').innerHTML = json.items
+        .map((item) => {
+          const {
+            id: { videoId },
+            snippet: {
+              thumbnails: {
+                medium: { url },
+              },
+              publishTime,
+              channelTitle,
+              title,
+            },
+          } = item;
+
+          return `
+        <li class="video-item" data-video-id="${videoId}">
+          <img
+            src="${url}"
+            alt="video-item-thumbnail" class="video-item__thumbnail">
+          <h4 class="video-item__title">${title}</h4>
+          <p class="video-item__channel-name">${channelTitle}</p>
+          <p class="video-item__published-date">${publishTime}</p>
+          <button class="video-item__save-button button">⬇ 저장</button>
+        </li>
+          `;
+        })
+        .join('');
 
       searchErrorMessage.textContent = '';
     } catch (error) {
