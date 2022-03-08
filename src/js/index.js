77d436a2db05a2780deef8706a9fb250eee8ad8b
Ahn <jin7969@gmail.com>
2022-03-08 23:29:44 +0900

    feat: 검색된 데이터를 받아온다
    Co-authored-by: cks3066 <cks3066@users.noreply.github.com>
    Co-authored-by: liswktjs <liswktjs@users.noreply.github.com>

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,32 @@
 import { API_KEY } from '../api.js';
-import { $, $$ } from './util/dom.js';
+import { $ } from './util/dom.js';
+import dummyData from '../../dummyData.js';
+
+const getURL = (searchInput, nextPageToken) => {
+  const URL = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${searchInput}&maxResults=10&type=video&key=${API_KEY}`;
+  if (nextPageToken) {
+    return URL.concat(`&pageToken=${nextPageToken}`);
+  }
+  return URL;
+};
+
+const getSearchData = searchInput => {
+  const URL = getURL(searchInput);
+  fetch(URL)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data.items);
+    })
+    .catch(error => console.log('error:', error));
+};
+
+const getNextSearchData = ({ nextPageToken }) => {
+  const nextURL = URL + `&pageToken=${nextPageToken}`;
+  fetch(nextURL)
+    .then(response => response.json())
+    .then(data => console.log(data.items))
+    .catch(error => console.log('error:', error));
+};
 
 export default function App() {
   $('#search-modal-button').addEventListener('click', () => {
@@ -16,11 +43,7 @@ export default function App() {
     try {
       const searchInput = $('#search-input-keyword').value.trim();
       validateInput(searchInput);
-      const URL = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${searchInput}&maxResults=10&type=video&key=${API_KEY}`;
-      fetch(URL)
-        .then(response => response.json())
-        .then(data => console.log(data.items))
-        .catch(error => console.log('error:', error));
+      getSearchData(searchInput);
     } catch (error) {
       alert(error.message);
     }
