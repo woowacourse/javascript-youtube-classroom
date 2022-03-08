34768452d91ed83bf5b2748dc3fc41e4dfeff739
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-03-09 00:02:00 +0900

    :truck: chore(settings): 커밋 컨벤션 스코프 추가
    Co-authored-by: Daye Lee <508yeah@gmail.com>

--- .cz-config.js
+++ .cz-config.js
@@ -42,6 +42,7 @@ module.exports = {
     { name: 'todo' },
     { name: 'ui' },
     { name: 'element' },
+    { name: 'domain' },
     { name: 'validation' },
     { name: 'util' },
     { name: 'test' },
