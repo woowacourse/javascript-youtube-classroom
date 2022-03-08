c1f80bdcf38eee037c7659f93a2cc69718aa7ce0
sangminsong <ssmin940606@gmail.com>
2022-03-08 17:45:15 +0900

    chore: eslint + prettier 설정
    Co-authored-by: lokba <lokba@users.noreply.github.com>

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'max-depth': ['error', 1],
+    'max-lines-per-function': ['error', 15],
+    'no-new': 'off',
+    'no-alert': 'off',
+    'import/extensions': 'off',
+  },
+};
