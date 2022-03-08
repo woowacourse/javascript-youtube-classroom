dfa9fe04215729762c6c9b6dea31b606031bc965
yunjin-kim <79692272+yunjin-kim@users.noreply.github.com>
2022-03-08 16:18:26 +0900

    chore: eslint, prettier, webpack 설정
    Co-authored-by: TaeYoon Kim <uni613@naver.com>

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/js/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devServer: {
+    port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: './index.html' })],
+};
