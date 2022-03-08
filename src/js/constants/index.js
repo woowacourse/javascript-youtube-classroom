f9a48724a560ea0c634cc0497a0f847ebbf4b72e
2019015414 <2019015414@hanyang.ac.kr>
2022-03-08 21:33:29 +0900

    refactor: 모달 끄는 코드 리팩터링
    Co-authored-by: soobin <sanaandmomo@users.noreply.github.com>

--- src/js/constants/index.js
+++ src/js/constants/index.js
@@ -1,4 +1,5 @@
 export const SELECTOR = {
   SEARCH_MODAL_BUTTON: '#search-modal-button',
   SEARCH_MODAL: '#search-modal',
+  MODAL_BACKGROUND: '#modal-background',
 };
--- src/js/index.js
+++ src/js/index.js
@@ -4,3 +4,13 @@ import { $ } from './utils/index.js';
 $(SELECTOR.SEARCH_MODAL_BUTTON).addEventListener('click', () => {
   $(SELECTOR.SEARCH_MODAL).classList.remove('hide');
 });
+
+const closeModal = () => $(SELECTOR.SEARCH_MODAL).classList.add('hide');
+
+$(SELECTOR.MODAL_BACKGROUND).addEventListener('click', closeModal);
+
+document.addEventListener('keyup', (e) => {
+  if (e.key === 'Escape') {
+    closeModal();
+  }
+});
