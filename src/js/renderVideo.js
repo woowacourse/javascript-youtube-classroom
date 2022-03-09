06c6b6146b66d6aa8ea13c1dfd08aa28f3fd1dfa
yunjin-kim <79692272+yunjin-kim@users.noreply.github.com>
2022-03-09 16:49:24 +0900

    feat: 무한스크롤 및 스켈로톤 UI 구현
    Co-authored-by: TaeYoon <uni613@naver.com>

--- src/js/renderVideo.js
+++ src/js/renderVideo.js
@@ -1,5 +1,9 @@
 import SearchVideo from './searchVideo.js';
-import { videoTemplate, videoSkeletonTemplate, videoNotFoundTemplate } from './template/videoTemplate.js';
+import {
+  videoTemplate,
+  videoSkeletonTemplate,
+  videoNotFoundTemplate,
+} from './template/videoTemplate.js';
 import { selectDom, addEvent } from './utils/selectDom.js';
 
 class RenderVideo {
@@ -7,32 +11,57 @@ class RenderVideo {
     this.searchVideo = new SearchVideo();
     this.searchModalButton = selectDom('#search-modal-button');
     this.modalContainer = selectDom('.modal-container');
-    this.searchInput = selectDom('#search-input-keyword');
-    this.searchButton = selectDom('#search-button');
+    this.searchForm = selectDom('#search-form');
+    this.searchInput = selectDom('#search-input-keyword', this.searchForm);
     this.videoListContainer = selectDom('.video-list');
     this.addEvents();
   }
 
   addEvents() {
     addEvent(this.searchModalButton, 'click', this.onSearchModalButtonClick);
-    addEvent(this.searchButton, 'click', this.onSearchButtonClick);
+    addEvent(this.searchForm, 'submit', this.onSearchFormSubmit);
+    addEvent(this.videoListContainer, 'scroll', this.onScrollVideoList);
   }
+
+  onScrollVideoList = () => {
+    // scrollHeight - offsetHeight === scrollTop
+    const { scrollHeight, offsetHeight, scrollTop } = this.videoListContainer;
+    if (scrollHeight - offsetHeight === scrollTop) {
+      this.renderVideoSkeleton();
+      setTimeout(async () => {
+        try {
+          // await this.searchVideo.handleSearchVideo(searchKeyword.trim());
+          this.renderSearchVideo(this.searchVideo.searchResults);
+        } catch (error) {
+          this.searchInput.value = '';
+          this.searchInput.focus();
+          this.videoListContainer.innerHTML = '';
+          return alert(error);
+        }
+      }, 500);
+    }
+  };
 
   onSearchModalButtonClick = () => {
     this.modalContainer.classList.remove('hide');
   };
 
-  onSearchButtonClick = async () => {
+  onSearchFormSubmit = async (e) => {
+    e.preventDefault();
     const searchKeyword = this.searchInput.value;
-    try {
-      this.renderVideoSkeleton();
-      setTimeout(async () => {
-        await this.searchVideo.handleSearchVideo(searchKeyword);
+    this.renderVideoSkeleton();
+
+    setTimeout(async () => {
+      try {
+        await this.searchVideo.handleSearchVideo(searchKeyword.trim());
         this.renderSearchVideo(this.searchVideo.searchResults);
-      }, 500);
-    } catch (error) {
-      return alert(error);
-    }
+      } catch (error) {
+        this.searchInput.value = '';
+        this.searchInput.focus();
+        this.videoListContainer.innerHTML = '';
+        return alert(error);
+      }
+    }, 500);
   };
 
   renderSearchVideo(searchVideo) {
@@ -40,12 +69,19 @@ class RenderVideo {
       this.videoListContainer.innerHTML = videoNotFoundTemplate;
       return;
     }
-    this.videoListContainer.innerHTML = searchVideo
-      .map((video) => videoTemplate(video)).join(' ');
+    Array.from(selectDom('.video-list').children).forEach((child) => {
+      if (child.className === 'skeleton') {
+        child.remove();
+      }
+    });
+    this.videoListContainer.insertAdjacentHTML('beforeend', searchVideo.map((video) => videoTemplate(video)).join(' '));
   }
 
   renderVideoSkeleton() {
-    this.videoListContainer.innerHTML = Array.from({ length: 10 }, () => videoSkeletonTemplate).join(' ');
+    this.videoListContainer.insertAdjacentHTML('beforeend', Array.from(
+      { length: 10 },
+      () => videoSkeletonTemplate
+    ).join(' '));
   }
 }
 
