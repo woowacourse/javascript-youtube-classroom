437d7e45cfc2ace71c825d657d1ed9686adea488
이아현 <lah1203@naver.com>
2022-03-09 13:22:19 +0900

    refactor: 에러 메시지 상수화
    Co-authored-by: 홍영민 <typingjs@gmail.com>

---
+++ src/js/constants/index.js
@@ -0,0 +1,5 @@
+const ERROR_MESSAGE = {
+  BLANK_SEARCH_INPUT: '검색어를 입력해주세요!',
+};
+
+export { ERROR_MESSAGE };
--- src/js/ui/Search.js
+++ src/js/ui/Search.js
@@ -1,11 +1,11 @@
+import { ERROR_MESSAGE } from '../constants';
 import { $, showExceptionSnackBar } from '../utils/dom';
-
 export default class Search {
   constructor() {
     $('#search-form').addEventListener('submit', e => {
       e.preventDefault();
       if ($('#search-input-keyword').value === '') {
-        showExceptionSnackBar('테스트');
+        showExceptionSnackBar(ERROR_MESSAGE.BLANK_SEARCH_INPUT);
         return;
       }
     });
