2ab88ba0ac5b7572324b40d844ad2dbc21b8287c
yunjin-kim <79692272+yunjin-kim@users.noreply.github.com>
2022-03-09 17:36:32 +0900

    chore: eslint gitignore webpack 설정
    Co-authored-by: TaeYoon <uni613@naver.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -39,58 +38,26 @@ module.exports = {
         ],
       },
       {
+        test: /\.(jpg|png|gif|swg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              publicPath: './',
+              name: '[path][name].[ext]',
+            },
+          },
+        ]
+      },
+      {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
     ],
   },
   plugins: [
-    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './index.html',
     }),
   ],
 };
-
-// const path = require('path');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-// module.exports = {
-//   mode: 'development',
-//   entry: './src/js/index.js',
-//   output: {
-//     filename: 'bundle.js',
-//     path: path.resolve(__dirname, 'dist'),
-//     clean: true,
-//   },
-//   devtool: 'source-map',
-//   devServer: {
-//     port: 9000,
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.js$/,
-//         exclude: /node_modules/,
-//         use: [
-//           {
-//             loader: 'babel-loader',
-//             options: {
-//               presets: ['@babel/preset-env'],
-//             },
-//           },
-//         ],
-//       },
-//       {
-//         test: /\.css$/,
-//         use: ['style-loader', 'css-loader'],
-//       },
-//     ],
-//   },
-//   plugins: [
-//     new CleanWebpackPlugin(),
-//     new HtmlWebpackPlugin({
-//       template: './index.html',
-//     })
-//   ],
-// };
