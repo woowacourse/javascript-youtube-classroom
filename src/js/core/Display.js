80b03899d32b859d5b472ef01f73d93f4ef087f9
moonheekim0118 <hellomooneekim@gmail.com>
2022-03-09 14:47:29 +0900

    ✨ : Display Core 추가

---
+++ src/js/core/Display.js
@@ -0,0 +1,29 @@
+export default class Display {
+  container;
+
+  constructor() {
+    if (this.constructor === Display) {
+      throw new Error('추상 클래스는 인스턴스화 할 수 없습니다.');
+    }
+    this.init();
+  }
+
+  init() {
+    this.setContainer();
+    this.bindEvents();
+  }
+
+  setContainer() {}
+
+  bindEvents() {}
+
+  addEvent(eventType, selector, handler) {
+    const children = [...this.container.querySelectorAll(selector)];
+    const isTarget = target => children.includes(target) || target.closest(selector);
+
+    this.container.addEventListener(eventType, event => {
+      if (!isTarget(event.target)) return false;
+      handler(event);
+    });
+  }
+}
