b1154d23b85f6931927e92d2a8619cd2b0c9efc0
kkojae <jaejeung3210@gmail.com>
2022-03-09 22:32:59 +0900

    test: 내가 검색한 영상들 중 저장 버튼을 누르면 저장 버튼이 사라지는지 확인하는 테스트 작성

--- cypress/integration/app.test.js
+++ cypress/integration/app.test.js
@@ -1,9 +1,6 @@
-// - [ ] 최초 검색 결과는 10개까지만 보여준다.
-//   - 브라우저 스크롤 바를 끝까지 내려 그 다음 10개 아이템을 추가로 불러온다.
-//   - 스로틀를 사용하여 이벤트 콜백함수 호출을 최소화한다.
 // - [ ] 내가 검색한 영상들의 JSON 데이터를 localStorage에 저장한다.
-//   - 저장 가능한 최대 동영상의 갯수는 100개이다.
-// - [ ] 이미 저장된 영상이라면 저장 버튼이 보이지 않도록 한다.
+//   - 이미 저장된 영상이라면 저장 버튼이 보이지 않도록 한다.
+// - [ ] 저장 가능한 최대 동영상의 갯수는 100개이다.
 
 // before(() => {
 //   cy.visit("./index.html");
@@ -64,4 +61,21 @@ it("브라우저 스크롤 바를 끝까지 내려 그 다음 10개 아이템을
   cy.get(".video-list").scrollTo("bottom");
 
   cy.get(".video-list").children().should("have.length", 20);
+});
+
+// - [ ] 내가 검색한 영상들의 JSON 데이터를 localStorage에 저장한다.
+//   - 이미 저장된 영상이라면 저장 버튼이 보이지 않도록 한다.
+// - [ ] 저장 가능한 최대 동영상의 갯수는 100개이다.
+
+it("내가 검색한 영상들 중 저장 버튼을 누르면 저장 버튼이 사라진다.", () => {
+  const searchKeyword = "xooos";
+
+  cy.visit("./index.html");
+
+  cy.get("#search-modal-button").click();
+  cy.get("#search-input-keyword").type(searchKeyword);
+  cy.get("#search-button").click();
+
+  cy.get(".video-item__save-button").eq(0).click();
+  cy.get(".video-item__save-button").should("be.not.visible");
 });
