4d030b9ce8ad6d41720a63043ddc1dbbf15a4022
moonheekim0118 <hellomooneekim@gmail.com>
2022-03-09 21:14:25 +0900

    📦 : 환경변수 설정

--- webpack.config.js
+++ webpack.config.js
@@ -2,52 +2,63 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const dotenv = require('dotenv');
+const webpack = require('webpack');
 
-module.exports = {
-  mode: 'development',
-  entry: './src/js/index.js',
-  resolve: {
-    extensions: ['.js', '.css'],
-    alias: {
-      '@Display': path.resolve(__dirname, 'src/js/display/'),
-      '@Utils': path.resolve(__dirname, 'src/js/utils/'),
-      '@Core': path.resolve(__dirname, 'src/js/core/'),
+module.exports = () => {
+  dotenv.config();
+
+  return {
+    mode: 'development',
+    entry: './src/js/index.js',
+    resolve: {
+      extensions: ['.js', '.css'],
+      alias: {
+        '@Display': path.resolve(__dirname, 'src/js/display/'),
+        '@Domain': path.resolve(__dirname, 'src/js/domain/'),
+        '@Utils': path.resolve(__dirname, 'src/js/utils/'),
+        '@Core': path.resolve(__dirname, 'src/js/core/'),
+      },
     },
-  },
-  devServer: {
-    port: 9000,
-  },
-  devtool: 'source-map',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env'],
+    devServer: {
+      port: 9000,
+    },
+    devtool: 'source-map',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env'],
+              },
             },
-          },
-        ],
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-        exclude: /node_modules/,
-      },
+          ],
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: './index.html',
+      }),
+      new MiniCssExtractPlugin({ filename: 'css/style.css' }),
+      new webpack.DefinePlugin({
+        'process.env.YOUTUBE_API_KEY': JSON.stringify(process.env.YOUTUBE_API_KEY),
+      }),
+      new webpack.EnvironmentPlugin(['YOUTUBE_API_KEY']),
     ],
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: './index.html',
-    }),
-    new MiniCssExtractPlugin({ filename: 'css/style.css' }),
-  ],
+  };
 };
