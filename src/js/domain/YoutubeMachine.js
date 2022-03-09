6df88e39719dd0ebc973e40f0abf9052fe0864ea
Ahn <jin7969@gmail.com>
2022-03-09 15:48:45 +0900

    feat: 최초 검색 결과를 불러 온다
    Co-authored-by: cks3066 <cks3066@users.noreply.github.com>
    Co-authored-by: liswktjs <liswktjs@users.noreply.github.com>

--- src/js/domain/YoutubeMachine.js
+++ src/js/domain/YoutubeMachine.js
@@ -13,6 +13,14 @@ export default class YoutubeMachine {
     return this.#searchTarget;
   }
 
+  set data(data) {
+    this.#data = data;
+  }
+
+  get data() {
+    return this.#data;
+  }
+
   getURL(nextPageToken) {
     const URL = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${this.searchTarget}&maxResults=10&type=video&key=${API_KEY}`;
     if (nextPageToken) {
