8800e17fda6a44530e45ef5ae2ddc0407d368120
KIM YEJI <rladpwl0512@gmail.com>
2022-03-09 17:44:12 +0900

    :bug: fix: iframe에 썸네일 이미지 override 및 동영상 autoplay
    Co-authored-by: wonsss <bigdatajang@gmail.com>

--- src/js/views/SearchResultView.js
+++ src/js/views/SearchResultView.js
@@ -12,12 +12,36 @@ export default class SearchResultView {
   }
 }
 class Template {
+  getThumbnail(imgUrl, videoId) {
+    return `
+      <style>
+        * {
+          margin: 0;
+          padding: 0;
+          overflow: hidden;
+        }
+        img {
+          object-fit: cover;
+          width: 100%;
+          height: 135px;
+        }
+      </style>
+      <a href='https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1'>
+        <img src=${imgUrl}>
+      </a>
+    `;
+  }
+
   getVideo({ snippet, id }) {
     return `
     <li class="video-item" data-video-id="">
-      <iframe class="video-item__thumbnail" id="ytplayer" type="text/html"
-      src="https://www.youtube.com/embed/${id.videoId}"
-      frameborder="0"></iframe>
+      <iframe 
+        class="video-item__thumbnail" 
+        srcdoc="${this.getThumbnail(snippet.thumbnails.high.url, id.videoId)}" 
+        frameborder="0"
+        allow="autoplay"
+        allowfullscreen>
+      </iframe>
       <h4 class="video-item__title">${snippet.title}</h4>
       <p class="video-item__channel-name">${snippet.channelTitle}</p>
       <p class="video-item__published-date">${snippet.publishTime}</p>
