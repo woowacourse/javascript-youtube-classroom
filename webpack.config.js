34b95af27392d2a6348fed8176eb6f71caf3a706
Ahn <jin7969@gmail.com>
2022-03-09 15:20:01 +0900

    chore: webpack 환경 설정
    Co-authored-by: cks3066 <cks3066@users.noreply.github.com>
    Co-authored-by: liswktjs <liswktjs@users.noreply.github.com>

--- webpack.config.js
+++ webpack.config.js
@@ -7,12 +7,28 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.css'],
   },
+  devServer: {
+    port: 9000,
+  },
+  devtool: 'source-map',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        ],
+      },
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
