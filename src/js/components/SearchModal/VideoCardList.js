12063c6fbeb7152d26406a1bf72549269fd023b8
gytks4 <gytks4@naver.com>
2022-03-09 17:44:26 +0900

    refactor: 무한 스크롤 IntersectionObserver로 구현
    Co-authored-by: Jeongmin Woo <greenblues1190@users.noreply.github.com>

--- src/js/components/SearchModal/VideoCardList.js
+++ src/js/components/SearchModal/VideoCardList.js
@@ -17,6 +17,15 @@ export default class VideoCardList extends Component {
       };
     });
 
+    this.observer = new IntersectionObserver(
+      this.onLastChildVisible.bind(this),
+      {
+        root: this.target,
+        rootMargin: '0px',
+        threshold: 0,
+      }
+    );
+
     this.state = { videos, isLoading: false };
   }
 
@@ -36,56 +45,64 @@ export default class VideoCardList extends Component {
       .forEach(
         (videoCard, index) => new VideoCard(videoCard, { video: videos[index] })
       );
+
+    this.observeLastChild();
   }
 
-  setEvent() {
-    this.addEvent('scroll', '#video-list', async (e) => {
-      if (
-        this.isScrollBelowLastCard(this.target, e.target) &&
-        !this.state.isLoading
-      ) {
-        // 스크롤이 마지막 요소에 닿으면 로딩 시작
+  observeLastChild() {
+    if (this.target.lastElementChild) {
+      this.observer.observe(this.target.lastElementChild);
+    }
+  }
 
-        // 로딩 전 요소들 캐싱
-        const prev = this.state.videos;
+  onLastChildVisible(entries, observer) {
+    // 화면에 들어온 이미지를 잡아서, data-src에 있는 url를 src로 넣어준다.
+    entries.forEach(async (entry) => {
+      if (!entry.isIntersecting || this.state.isLoading) return;
 
-        // 로딩 될 요소들의 스켈레톤 생성해 state에 추가
-        this.setState({
-          isLoading: true,
-          videos: [
-            ...prev,
-            ...Array(10).fill({
-              loading: true,
-              videoId: null,
-              thumbnailUrl: null,
-              title: null,
-              channelTitle: null,
-              publishTime: null,
-              saved: false,
-            }),
-          ],
-        });
-        const newVideos = await this.loadNextVideos();
+      observer.disconnect();
 
-        // 로딩이 끝난 후 로딩 된 요소들을 원하는 프로퍼티를 가진 객체로 매핑
-        const savedVideos = Component.webStore.load();
-        const newVideoProps = newVideos.map((item) => {
-          return {
-            loading: false,
-            videoId: item.id.videoId,
-            thumbnailUrl: item.snippet.thumbnails.default.url,
-            title: item.snippet.title,
-            channelTitle: item.snippet.channelTitle,
-            publishTime: item.snippet.publishTime,
-            saved: savedVideos.includes(item.id.videoId),
-          };
-        });
+      // 스크롤이 마지막 요소에 닿으면 로딩 시작
+      // 로딩 전 요소들 캐싱
+      const prev = this.state.videos;
 
-        this.setState({
-          isLoading: false,
-          videos: [...prev, ...newVideoProps],
-        });
-      }
+      // 로딩 될 요소들의 스켈레톤 생성해 state에 추가
+      this.setState({
+        isLoading: true,
+        videos: [
+          ...prev,
+          ...Array(10).fill({
+            loading: true,
+            videoId: null,
+            thumbnailUrl: null,
+            title: null,
+            channelTitle: null,
+            publishTime: null,
+            saved: false,
+          }),
+        ],
+      });
+
+      const newVideos = await this.loadNextVideos();
+
+      // 로딩이 끝난 후 로딩 된 요소들을 원하는 프로퍼티를 가진 객체로 매핑
+      const savedVideos = Component.webStore.load();
+      const newVideoProps = newVideos.map((item) => {
+        return {
+          loading: false,
+          videoId: item.id.videoId,
+          thumbnailUrl: item.snippet.thumbnails.default.url,
+          title: item.snippet.title,
+          channelTitle: item.snippet.channelTitle,
+          publishTime: item.snippet.publishTime,
+          saved: savedVideos.includes(item.id.videoId),
+        };
+      });
+
+      this.setState({
+        isLoading: false,
+        videos: [...prev, ...newVideoProps],
+      });
     });
   }
 
