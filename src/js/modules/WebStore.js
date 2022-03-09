033e30d3b615981b9421d6a69bd3fad50bfa94da
junzzi <jjh01027717502@gmail.com>
2022-03-10 00:09:28 +0900

    refactor: web store를 video 도메인에 맞게 수정

--- src/js/modules/WebStore.js
+++ src/js/modules/WebStore.js
@@ -1,16 +1,20 @@
+import { findVideoInVideoInfoList } from '../utils/util';
+
 // singleton
 export default {
   getData(key) {
     return JSON.parse(localStorage.getItem(key));
   },
-  setDataInArray(key, value) {
-    const currentData = this.getData(key) ?? [];
-
+  setSavedVideoList(saveVideo) {
+    const currentData = this.getData('saved-video-list') ?? [];
     if (currentData.length === 100) {
       throw new Error('100개까지만 저장이 가능합니다.');
     }
-
-    localStorage.setItem(key, JSON.stringify([...currentData, value]));
+    if (findVideoInVideoInfoList(currentData, saveVideo.videoId)) {
+      throw new Error('이미 저장한 비디오입니다.');
+    }
+    localStorage.setItem('saved-video-list', JSON.stringify([...currentData, saveVideo]));
   },
+
   // clear 하는 함수가 있으면 좋을 것 같다..?
 };
