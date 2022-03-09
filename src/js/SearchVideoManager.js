1d74ebcb0d3ad26a1376ea48d613a2848d149cef
prefer2 <sunho6200@gmail.com>
2022-03-09 11:45:49 +0900

    feat: 검색 결과 스크롤시 추가 로딩 구현
    Co-authored-by: Soyi Jeon <soyi.dev@gmail.com>

--- src/js/SearchVideoManager.js
+++ src/js/SearchVideoManager.js
@@ -2,6 +2,8 @@ const DUMMY_YOUTUBE_API_ENDPOINT = (keyword) =>
   `https://brave-lichterman-77e301.netlify.app/dummy/youtube/v3/search?part=snippet&q=${keyword}&maxResults=10`;
 const YOUTUBE_API_ENDPOINT = (keyword) =>
   `https://brave-lichterman-77e301.netlify.app/youtube/v3/search?part=snippet&q=${keyword}&maxResults=10`;
+const YOUTUBE_API_ENDPOINT2 = (keyword) =>
+  `https://elastic-goldstine-10f16a.netlify.app/youtube/v3/search?part=snippet&q=${keyword}&maxResults=10`;
 
 export default class SearchVideoManager {
   constructor() {
@@ -9,10 +11,19 @@ export default class SearchVideoManager {
     this.nextPageToken = '';
   }
 
+  resetNextPageToken() {
+    this.nextPageToken = '';
+  }
+
   fetchYoutubeData(keyword) {
-    return fetch(YOUTUBE_API_ENDPOINT(keyword))
+    return fetch(
+      this.nextPageToken
+        ? `${YOUTUBE_API_ENDPOINT2(keyword)}&pageToken=${this.nextPageToken}`
+        : YOUTUBE_API_ENDPOINT2(keyword)
+    )
       .then((data) => data.json())
       .then((result) => {
+        console.log(result);
         this.keyword = keyword;
         this.nextPageToken = result.nextPageToken;
         return result.items.map((item) => ({
--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,8 @@ import { $ } from './util';
 const youtubeClassRoomView = new YoutubeClassRoomView();
 const searchVideoManager = new SearchVideoManager();
 
+let keyword = '';
+
 $('#search-modal-button').addEventListener('click', () => {
   $('#modal-container').classList.remove('hide');
 });
@@ -15,11 +17,32 @@ $('.dimmer').addEventListener('click', (e) => {
 
 $('#search-form').addEventListener('submit', (e) => {
   e.preventDefault();
-  const keyword = $('#search-input-keyword').value;
+  keyword = $('#search-input-keyword').value;
   if (searchVideoManager.isKeywordChanged(keyword)) {
     youtubeClassRoomView.initializeSearchResultVideoList();
+    searchVideoManager.resetNextPageToken();
   }
   searchVideoManager.fetchYoutubeData(keyword).then((videos) => {
     youtubeClassRoomView.updateOnSearchDataReceived(videos);
   });
 });
+
+let debounce;
+function detectScroll() {
+  const { scrollTop, clientHeight, scrollHeight } = $('#search-result-video-list');
+  if (debounce) {
+    console.log('요청 해제');
+    clearTimeout(debounce);
+  }
+
+  if (scrollTop + clientHeight + 20 >= scrollHeight) {
+    console.log('요청 보내기');
+    debounce = setTimeout(() => {
+      searchVideoManager.fetchYoutubeData(keyword).then((videos) => {
+        youtubeClassRoomView.updateOnSearchDataReceived(videos);
+      });
+    }, 1000);
+  }
+}
+
+$('#search-result-video-list').addEventListener('scroll', detectScroll);
