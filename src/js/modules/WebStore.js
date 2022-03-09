173b99b4c522deb66c36dfb77b4f1167a1302ee3
junzzi <jjh01027717502@gmail.com>
2022-03-10 00:09:28 +0900

    refactor: web store를 video 도메인에 맞게 수정

--- src/js/modules/WebStore.js
+++ src/js/modules/WebStore.js
@@ -11,6 +11,11 @@ export default {
     if (currentData.length === 100) {
       throw new Error(ERROR_MESSAGE.SAVE_VIDEO_COUNT_OVER);
     }
+    if (findVideoInVideoInfoList(currentData, saveVideo.videoId)) {
+      throw new Error('이미 저장한 비디오입니다.');
+    }
+    localStorage.setItem('saved-video-list', JSON.stringify([...currentData, saveVideo]));
+  },
 
     localStorage.setItem(
       WEB_STORE_KEY.SAVED_VIDEO_LIST_KEY,
