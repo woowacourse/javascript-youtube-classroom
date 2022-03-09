73b1892a62c59ba42f0a6807bad914f9f99b8a5e
woose28 <dldntmdsla@naver.com>
2022-03-09 15:07:45 +0900

    refactor: fetcher 테스트 코드 삭제

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
 import './css/index.css';
-import { fetcher } from './js/modules/fetcher';
-
-const test = async () => {
-  try {
-    const data = await fetcher('https://jsonplaceholder.typicode.com/posts/1');
-  } catch (error) {
-    alert(error);
-  }
-};
-
-test();
