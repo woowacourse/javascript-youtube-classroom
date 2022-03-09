b306875194fe657dde3adc8d393ebcbc20da59db
sangminsong <ssmin940606@gmail.com>
2022-03-09 17:14:35 +0900

    test: 유튜브 검색 결과를 저장 실패 테스트
    Co-authored-by: lokba <lokba@users.noreply.github.com>

--- src/js/__test__/app.test.js
+++ src/js/__test__/app.test.js
@@ -7,6 +7,14 @@ test('유튜브 검색 기능 정상 작동', async () => {
   const keyword = '지피티';
 
   const response = await searchEngine.searchKeyword(keyword);
-
+  // TODO : isNull을 toBe(null)로 대체가능하면 하기
   expect(isNull(response)).toBe(false);
+});
+
+test('유튜브 검색 결과를 webstorage에 저장할 수 있다', () => {
+  const storeEngine = new StoreEngine();
+
+  storeEngine.saveData();
+
+  expect(storeEngine.getData()).not.toBe(null);
 });
