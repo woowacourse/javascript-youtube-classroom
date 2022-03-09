45680f7fd3455afa06fbc5d543d0acabd78bc622
이아현 <lah1203@naver.com>
2022-03-09 15:37:35 +0900

    feat: 검색 결과 임시 화면 구성
    Co-authored-by: 홍영민 <typingjs@gmail.com>

--- src/js/ui/Search.js
+++ src/js/ui/Search.js
@@ -1,4 +1,6 @@
 import { ERROR_MESSAGE } from '../constants';
+import { API_KEY } from '../domain/key';
+import { request } from '../domain/youtubeApi';
 import { $, showExceptionSnackBar } from '../utils/dom';
 export default class Search {
   constructor() {
@@ -8,6 +10,37 @@ export default class Search {
         showExceptionSnackBar(ERROR_MESSAGE.BLANK_SEARCH_INPUT);
         return;
       }
+
+      this.renderVideoList();
+      // TODO: 스켈레톤 UI를 보여준다
+
+      // try {
+      //   request($('#search-input-keyword').value, API_KEY).then(json =>
+      //     console.log(json),
+      //   );
+      // } catch ({ message }) {
+      //   console.log(message);
+      // }
     });
+  }
+
+  videoTemplate() {
+    return `
+      <li class="video-item" data-video-id="">
+        <img
+          src="https://i.ytimg.com/vi/ECfuKi5-Cfs/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDvmIcX-TgdcH2g_Bd4AUxw6hjmvQ"
+          alt="video-item-thumbnail" class="video-item__thumbnail">
+        <h4 class="video-item__title">[Playlist] 너무 좋은데 괜찮으시겠어요?</h4>
+        <p class="video-item__channel-name">essential;</p>
+        <p class="video-item__published-date">2022년 3월 2일</p>
+        <button class="video-item__save-button button">⬇ 저장</button>
+      </li>`;
+  }
+
+  renderVideoList() {
+    $('.video-list').insertAdjacentHTML(
+      'beforeend',
+      this.videoTemplate().repeat(8),
+    );
   }
 }
