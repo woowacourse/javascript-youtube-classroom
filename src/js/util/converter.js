73bb88c54bb335a5ff48e1cc248c9fea2a7f2437
Hwang Jung Min <hwangstar156@gmail.com>
2022-03-11 02:07:21 +0900

    fix: getMonth가 0월부터 시작하는것을 고려하여 +1 추가

--- src/js/util/converter.js
+++ src/js/util/converter.js
@@ -1,5 +1,5 @@
 export const convertDataToDateString = (publishTime) => {
   const dateObject = new Date(publishTime);
 
-  return `${dateObject.getFullYear()}년 ${dateObject.getMonth()}월 ${dateObject.getDate()}일`;
+  return `${dateObject.getFullYear()}년 ${dateObject.getMonth() + 1}월 ${dateObject.getDate()}일`;
 };
