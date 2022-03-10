3d41598fb3d80053c427400ea020735c5bb968e3
2019015414 <2019015414@hanyang.ac.kr>
2022-03-10 17:48:47 +0900

    fix: 날짜 유효성 검사
    Co-authored-by: soobin <sanaandmomo@users.noreply.github.com>

--- src/js/utils/index.js
+++ src/js/utils/index.js
@@ -3,11 +3,11 @@ export const $ = (selector, node = document) => node.querySelector(selector);
 export const isEmpty = (value) => value === '';
 
 export const convertYYYYMMDD = (publishTime) => {
-  const videoTime = new Date(publishTime);
+  const videoTime = new Date(publishTime.slice(0, 10));
   return (
     videoTime.getFullYear().toString() +
     '년 ' +
-    videoTime.getMonth().toString().padStart(2, '0') +
+    (videoTime.getMonth() + 1).toString().padStart(2, '0') +
     '월 ' +
     videoTime.getDate().toString().padStart(2, '0') +
     '일'
