5bd8573c11a4e578be76131fad05af4216442066
yunjin-kim <79692272+yunjin-kim@users.noreply.github.com>
2022-03-10 20:47:38 +0900

    chore: webpack 설정
    Co-authored-by: TaeYoon <uni613@naver.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -56,6 +57,7 @@ module.exports = {
     ],
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './index.html',
     }),
