bd94badc9b42f21301a9759637abea5fd7c36083
yunjin-kim <79692272+yunjin-kim@users.noreply.github.com>
2022-03-10 20:46:51 +0900

    fix: renderVideo 인스턴스 생성 수정
    Co-authored-by: TaeYoon <uni613@naver.com>

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import '../css/index.css';
-import RenderVideo from './renderVideo';
+import RenderVideo from './renderVideo.js';
 
-const renderVideo = new RenderVideo();
+// eslint-disable-next-line no-new
+new RenderVideo();
