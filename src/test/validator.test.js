a3a891635d1aebde02077f5c64607f9e3bc876c3
2019015414 <2019015414@hanyang.ac.kr>
2022-03-10 17:48:16 +0900

    test: 날짜 유효성 검사
    Co-authored-by: soobin <sanaandmomo@users.noreply.github.com>

--- src/test/validator.test.js
+++ src/test/validator.test.js
@@ -1,7 +1,14 @@
-import { isEmpty } from '../js/utils/index.js';
+import { isEmpty, convertYYYYMMDD } from '../js/utils/index.js';
 
 test('빈 값을 입력하면 검색이 불가능하다.', () => {
   const keyword = '';
 
   expect(isEmpty(keyword)).toBe(true);
+});
+
+test('string으로 온 날짜값을 YYYY년 MM월 DD일 형식에 맞춰 출력할 수 있어야한다.', () => {
+  const dateString = '2020-10-23T16:49:44Z';
+  const expectString = '2020년 10월 23일';
+
+  expect(convertYYYYMMDD(dateString)).toBe(expectString);
 });
