d626712da5218283e3c14c25e33766de3cb2b041
2019015414 <2019015414@hanyang.ac.kr>
2022-03-10 17:17:53 +0900

    fix: 시간을 YYYY MM DD형식으로 변환
    Co-authored-by: soobin <sanaandmomo@users.noreply.github.com>

--- src/js/videoTemplate.js
+++ src/js/videoTemplate.js
@@ -1,13 +1,24 @@
-const videoTemplate = (id, snippet) => {
+function convertYYYYMMDD(publishTime) {
+  const videoTime = new Date(publishTime);
+  return (
+    videoTime.getFullYear().toString() +
+    '년 ' +
+    videoTime.getMonth().toString().padStart(2, '0') +
+    '월 ' +
+    videoTime.getDate().toString().padStart(2, '0') +
+    '일'
+  );
+}
+
+const videoTemplate = (id, snippet) =>
   `<li class="video-item" data-video-id="${id.videoId}">
     <img
       src="${snippet.thumbnails.default.url}"
       alt="video-item-thumbnail" class="video-item__thumbnail">
     <h4 class="video-item__title">[Playlist] ${snippet.title}</h4>
     <p class="video-item__channel-name">${snippet.channelTitle}</p>
-    <p class="video-item__published-date">${snippet.pulishTime}</p>
+    <p class="video-item__published-date">${convertYYYYMMDD(snippet.publishTime)}</p>
     <button class="video-item__save-button button">⬇ 저장</button>
   </li>`;
-};
 
 export default videoTemplate;
