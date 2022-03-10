7e44fbd946d61551396da1166777c61f13c5e7ae
intae92 <kangit2013@gmail.com>
2022-03-11 00:18:30 +0900

    refactor: 날짜 텍스트 포맷 해서 출력하기

--- src/js/common/VideoCard.js
+++ src/js/common/VideoCard.js
@@ -21,8 +21,9 @@ export default class VideoCard {
     const storeButton = videoIds.includes(videoId)
       ? ''
       : '<button class="video-item__save-button button">⬇ 저장</button>';
-    console.log(publishTime);
-    const timeFormatter = publishTime.split('T')[0];
+
+    const date = new Date(publishTime);
+    const dateText = `${date.getFullYear()}년 ${date.getMonth() + 1}월 ${date.getDate()}일`;
 
     return `
       <li class="video-item" data-video-id="${videoId}">
@@ -31,7 +32,7 @@ export default class VideoCard {
           alt="video-item-thumbnail" class="video-item__thumbnail">
         <h4 class="video-item__title">${title}</h4>
         <p class="video-item__channel-name">${channelTitle}</p>
-        <p class="video-item__published-date">${timeFormatter}</p>
+        <p class="video-item__published-date">${dateText}</p>
         ${storeButton}
       </li>
       `;
