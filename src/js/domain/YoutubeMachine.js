d4c756f71757588ddbc8bd6015ab149d98d3f3ef
cks3066 <cks3066@naver.com>
2022-03-11 02:17:06 +0900

    chore: 배포 관련 설정
    Co-authored-by: jin7969 <jin7969@users.noreply.github.com>
    Co-authored-by: liswktjs <liswktjs@users.noreply.github.com>

--- src/js/domain/YoutubeMachine.js
+++ src/js/domain/YoutubeMachine.js
@@ -22,7 +22,7 @@ export default class YoutubeMachine {
   }
 
   getURL(nextPageToken) {
-    const URL = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${this.searchTarget}&maxResults=10&type=video&key=${process.env.API_KEY_ON}`;
+    const URL = `https://onstar.netlify.app/youtube/v3/search?part=snippet&q=${this.searchTarget}&maxResults=10&type=video`;
     if (nextPageToken) {
       return URL.concat(`&pageToken=${nextPageToken}`);
     }
