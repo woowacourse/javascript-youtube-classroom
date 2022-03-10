847a1d44367d338d6b0cfda02d40fa49decb016e
junzzi <jjh01027717502@gmail.com>
2022-03-10 22:38:23 +0900

    chore: file loader 설정 및 설치

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,13 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.png$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+        },
+      },
     ],
   },
   plugins: [
