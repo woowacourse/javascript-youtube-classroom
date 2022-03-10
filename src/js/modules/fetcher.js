d79b18e515ced17a33a8e367d3a0bb34bb2f5d49
junzzi <jjh01027717502@gmail.com>
2022-03-11 00:41:34 +0900

    refactor: generateQueryString 함수의 로직을 수정

--- src/js/modules/fetcher.js
+++ src/js/modules/fetcher.js
@@ -1,19 +1,19 @@
 import { API_URL } from '../constants/fetcher';
 
 const generateQueryString = (params) =>
-  Object.entries(params).reduce((prev, [key, value]) => `${prev}&${key}=${value}`, '');
+  Object.entries(params).reduce(
+    (prev, [key, value]) => (value ? `${prev}&${key}=${value}` : prev),
+    ''
+  );
 
 export const youtubeAPIFetcher = async ({ path, params }) => {
   const response = await fetch(
     `${API_URL}${path}?key=${API_KEY}${generateQueryString(params)}`,
     {}
   );
-
   if (response.ok) {
     const data = await response.json();
-
     return data;
   }
-
   throw new Error(`api 요청 중 에러 발생: ${response.status}`);
 };
