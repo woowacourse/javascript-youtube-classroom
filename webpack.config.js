b0dfc988ae2b3c7b9e8a835b19fac80a1e7e452a
DomMorello <zuzudnf@gmail.com>
2022-03-10 18:26:14 +0900

    build: file-loader 추가
    Co-authored-by: uk960214 <uk960214@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -41,8 +41,15 @@ const config = {
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
         type: 'asset',
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+            },
+          },
+        ],
       },
-
       // Add your rules for custom modules here
       // Learn more about loaders from https://webpack.js.org/loaders/
     ],
