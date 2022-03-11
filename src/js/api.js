d11842a37c912f1362a8a03a6d9983b1a5beb90c
최광희 <polyckh@gmail.com>
2022-03-11 17:03:58 +0900

    feat: 실제 api키 사용되도록 변경

--- src/js/api.js
+++ src/js/api.js
@@ -1,7 +1,7 @@
 import { RULES } from './constants';
 
 const YOUTUBE_URL =
-  'https://622752939a5410d43ba3fbcd--modest-euler-778376.netlify.app/dummy/youtube/v3/search?';
+  'https://622752939a5410d43ba3fbcd--modest-euler-778376.netlify.app/youtube/v3/search?';
 
 const OPTIONS = {
   part: 'snippet',
@@ -16,10 +16,7 @@ const makeURLQuery = (props) => {
     `${url}q=${keyword}&`,
   );
 
-  if (pageToken === '') {
-    return query;
-  }
-  return `${query}pageToken=${pageToken}`;
+  return pageToken ? `${query}pageToken=${pageToken}` : query;
 };
 
 const fetchData = async (query) => {
