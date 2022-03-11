9c8a396987a7fcdd67cc4cd83abad0e106cc67d3
gytks4 <gytks4@naver.com>
2022-03-11 09:00:22 +0900

    refactor: api 호출 함수 수정

--- src/js/api/api.js
+++ src/js/api/api.js
@@ -1,24 +1,32 @@
 import { YOUTUBE_API_KEY } from '../../../api_key.js';
 
-const API_SERVER = 'https://www.googleapis.com/youtube/v3';
-const QUERY_OPTIONS = {
-  SEARCH: {
-    part: 'snippet',
-    type: 'video',
-    maxResults: 10,
-  },
+const BASE_URL = 'https://www.googleapis.com/youtube/v3/search';
+const OPTIONS = {
+  part: 'snippet',
+  type: 'video',
+  maxResults: 10,
+  key: YOUTUBE_API_KEY,
 };
 
 export const searchVideos = (query, nextPageToken = null) => {
-  const options = QUERY_OPTIONS.SEARCH;
-  const spreadQuery = `part=${
-    options.part
-  }&q=${query}&key=${YOUTUBE_API_KEY}&type=${options.type}&maxResults=${
-    options.maxResults
-  }${nextPageToken ? `&pageToken=${nextPageToken}` : ''}`;
-  const url = `${API_SERVER}/search?${spreadQuery}`;
+  const url = `${BASE_URL}?${spreadOptions({
+    ...OPTIONS,
+    q: makeQueryFormal(query),
+    nextPageToken,
+  })}`;
 
   return fetch(url)
     .then((res) => res.json())
     .catch((err) => err);
 };
+
+function spreadOptions(options) {
+  return Object.keys(options)
+    .map((key) => options[key] && `&${key}=${options[key]}`)
+    .join('')
+    .slice(1);
+}
+
+function makeQueryFormal(query) {
+  return query.trim().replace(/\s/g, ' ').replace(/\s/g, '+');
+}
