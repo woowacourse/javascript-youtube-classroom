84f271ea700398b6489746dda21c02be29b76aba
prefer2 <sunho6200@gmail.com>
2022-03-11 12:59:44 +0900

    refactor: 스크롤 이벤트 호출 throttle 방식으로 변경
    Co-authored-by: Soyi Jeon <soyi.dev@gmail.com>

--- src/js/util.js
+++ src/js/util.js
@@ -1,11 +1,18 @@
 export const $ = (selector, node = document) => node.querySelector(selector);
 export const $$ = (selector, node = document) => node.querySelectorAll(selector);
 
-export const debounce = (callback, delay) => {
+export const throttle = (callback, delay) => {
   let timerId;
 
   return (event) => {
-    if (timerId) clearTimeout(timerId);
-    timerId = setTimeout(callback, delay, event);
+    if (timerId) return;
+    timerId = setTimeout(
+      () => {
+        callback(event);
+        timerId = null;
+      },
+      delay,
+      event
+    );
   };
 };
