976a11f9f42eceb425187f1aaad6078ae441ceeb
sangminsong <ssmin940606@gmail.com>
2022-03-11 15:48:15 +0900

    test: util 테스트 추가 및 네이밍 수정
    Co-authored-by: lokba <lokba@users.noreply.github.com>

--- src/js/__test__/domain.test.js
+++ src/js/__test__/domain.test.js
@@ -1,18 +1,21 @@
 import SearchEngine from '../domain/searchEngine.js';
 import StorageEngine from '../domain/storageEngine.js';
 
-const generate100Data = () => {
+import { MAX_SAVED_VIDEOS_LENGTH } from '../util/constants.js';
+
+const generate100Videos = () => {
   const sample = { videoId: 'eMf0jojpdJQ' };
 
   return Array.from({ length: 100 }, () => sample);
 };
 
-test('유튜브 검색 기능 정상 작동', async () => {
+test('유튜브 검색 기능 정상 작동', () => {
   const searchEngine = new SearchEngine();
   const keyword = '지피티';
 
-  const response = await searchEngine.searchKeyword(keyword);
-  expect(response).not.toBe(null);
+  searchEngine.searchKeyword(keyword).then((response) => {
+    expect(response).not.toBe(null);
+  });
 });
 
 test('유튜브 검색 결과를 webstorage에 저장할 수 있다', () => {
@@ -26,12 +29,12 @@ test('유튜브 검색 결과를 webstorage에 저장할 수 있다', () => {
 
 test('유튜브 검색 결과를 webstorage에 100개까지 저장할 수 있다.', () => {
   const storageEngine = new StorageEngine();
-  const mockData = generate100Data();
+  const mockVideos = generate100Videos();
 
-  localStorage.setItem('savedVideos', JSON.stringify(mockData));
-  expect(storageEngine.getSavedVideos()).toHaveLength(100);
+  localStorage.setItem('savedVideos', JSON.stringify(mockVideos));
+  expect(storageEngine.getSavedVideos()).toHaveLength(MAX_SAVED_VIDEOS_LENGTH);
 
   const videoId = 'eMf0jojpdJQ';
   storageEngine.saveVideo(videoId);
-  expect(storageEngine.getSavedVideos()).toHaveLength(100);
+  expect(storageEngine.getSavedVideos()).toHaveLength(MAX_SAVED_VIDEOS_LENGTH);
 });
