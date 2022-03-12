f8f2823a3a5b1a56e4603d477689d78e9e6c913c
Soyi Jeon <soyi.dev@gmail.com>
2022-03-12 15:27:47 +0900

    test: Storage 분리에 따른 테스트 수정

--- __tests__/saveVideo.test.js
+++ __tests__/saveVideo.test.js
@@ -1,14 +1,22 @@
-import SaveVideoManager from '../src/js/SaveVideoManager';
+/**
+ * @jest-environment jsdom
+ */
 
-describe('localStorage에 영상 ID 저장 기능 테스트', () => {
+import SaveVideoManager from '../src/js/SaveVideoManager';
+import Storage from '../src/js/Storage';
+
+describe('Storage 영상 ID 저장 기능 테스트', () => {
+  let storage;
+  let saveVideoManager;
+
   beforeEach(() => {
     jest.clearAllMocks();
+    storage = new Storage();
+    saveVideoManager = new SaveVideoManager(storage);
   });
 
   it('영상 ID를 저장한다.', () => {
     const videoID = 'testVideoID';
-
-    const saveVideoManager = new SaveVideoManager();
     saveVideoManager.saveVideoById(videoID);
 
     expect(JSON.parse(localStorage.getItem('id').includes(videoID))).toBeTruthy();
@@ -16,10 +24,8 @@ describe('localStorage에 영상 ID 저장 기능 테스트', () => {
 
   it('영상을 이미 저장했는지 확인할 수 있다.', () => {
     const videoID = 'testVideoID';
-
-    const saveVideoManager = new SaveVideoManager();
     saveVideoManager.saveVideoById(videoID);
 
-    expect(saveVideoManager.findVideoById(videoID)).toBeTruthy();
+    expect(storage.findVideoById(videoID)).toBeTruthy();
   });
 });
