8344ee394d5df32803f382743bef966d029d0320
junzzi <jjh01027717502@gmail.com>
2022-03-14 03:15:20 +0900

    refactor:  상태 초기화, 구독 리스트 초기화 함수로 분리

--- src/js/modules/stateStore.js
+++ src/js/modules/stateStore.js
@@ -1,32 +1,35 @@
 import { STATE_STORE_KEY } from '../constants/stateStore';
 import { WEB_STORE_KEY } from '../constants/webStore';
 import webStore from './webStore';
-
+const getInitialState = () => ({
+  [STATE_STORE_KEY.IS_MODAL_SHOW]: false,
+  [STATE_STORE_KEY.SEARCH_RESULT]: {
+    videoList: [],
+    keyword: null,
+    prevVideoListLength: 0,
+    nextPageToken: null,
+  },
+  [STATE_STORE_KEY.IS_WAITING_RESPONSE]: false,
+  [STATE_STORE_KEY.SAVED_VIDEO]: webStore.getData(WEB_STORE_KEY.SAVED_VIDEO_LIST_KEY) ?? [],
+});
+const getInitialSubcribeList = () => ({
+  [STATE_STORE_KEY.IS_MODAL_SHOW]: new Set(),
+  [STATE_STORE_KEY.SEARCH_RESULT]: new Set(),
+  [STATE_STORE_KEY.IS_WAITING_RESPONSE]: new Set(),
+  [STATE_STORE_KEY.SAVED_VIDEO]: new Set(),
+});
 export const { subscribe, setState, getState } = (function () {
-  const state = {
-    [STATE_STORE_KEY.IS_MODAL_SHOW]: false,
-    [STATE_STORE_KEY.SEARCH_RESULT]: {
-      videoList: [],
-      keyword: null,
-      prevVideoListLength: 0,
-      nextPageToken: null,
-    },
-    [STATE_STORE_KEY.IS_WAITING_RESPONSE]: false,
-    [STATE_STORE_KEY.SAVED_VIDEO]: webStore.getData(WEB_STORE_KEY.SAVED_VIDEO_LIST_KEY) ?? [],
-  };
-  const components = {
-    [STATE_STORE_KEY.IS_MODAL_SHOW]: new Set(),
-    [STATE_STORE_KEY.SEARCH_RESULT]: new Set(),
-    [STATE_STORE_KEY.IS_WAITING_RESPONSE]: new Set(),
-    [STATE_STORE_KEY.SAVED_VIDEO]: new Set(),
-  };
+  const state = getInitialState();
+  const subcribeList = getInitialSubcribeList();
+
   function notify(stateKey) {
-    const subscribedComponents = components[stateKey];
+    const subscribedComponents = subcribeList[stateKey];
     subscribedComponents.forEach((component) => component.wakeUp(state[stateKey], stateKey));
   }
+
   return {
     subscribe: (key, component) => {
-      components[key].add(component);
+      subcribeList[key].add(component);
       return state[key];
     },
     setState(key, valueOrFunction) {
