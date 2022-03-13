6623bf93981039dc03e0b1a55259813d7b18d3c3
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-03-13 16:29:46 +0900

    ♻️ : API 사용 시 URL 전달 방식 개선

--- src/js/api.js
+++ src/js/api.js
@@ -1,8 +1,9 @@
 import { CLASS_ROOM_SETTING } from '@Constants/Setting';
+import { getUrlSearchParams } from '@Utils/ManageData';
 
 const request = async (url, option) => {
   const response = await fetch(url, option);
-  if (response.ok === false) throw new Error('서버 오류');
+  if (!response.ok) throw new Error('서버 오류가 발생되었습니다.');
   const data = await response.json();
 
   return data;
@@ -10,16 +11,19 @@ const request = async (url, option) => {
 
 export const requestYoutubeSearch = async (keyword = '', nextPageToken = '') => {
   try {
-    const response = await request(
-      `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&q=${encodeURIComponent(
-        keyword,
-      )}&maxResults=${CLASS_ROOM_SETTING.MAX_VIDEO_NUMBER}&key=${
-        process.env.YOUTUBE_API_KEY
-      }&pageToken=${nextPageToken}`,
-      {
-        method: 'GET',
-      },
-    );
+    const url = getUrlSearchParams('https://www.googleapis.com/youtube/v3/search', {
+      part: 'snippet',
+      type: 'video',
+      q: keyword,
+      maxResults: CLASS_ROOM_SETTING.MAX_VIDEO_NUMBER,
+      key: process.env.YOUTUBE_API_KEY,
+      pageToken: nextPageToken,
+    });
+
+    const response = await request(url, {
+      method: 'GET',
+    });
+
     return response;
   } catch (error) {
     return { error: true };
--- src/js/utils/ManageData.js
+++ src/js/utils/ManageData.js
@@ -3,3 +3,12 @@ export const getParsedTime = timeString => {
 
   return `${time.getFullYear()}년 ${time.getMonth() + 1}월 ${time.getDate()}일`;
 };
+
+export const getUrlSearchParams = (url, params) => {
+  const paramsResult = Object.entries(params).reduce((previous, [key, value]) => {
+    previous.set(key, value);
+    return previous;
+  }, new URLSearchParams());
+
+  return `${url}?${paramsResult.toString()}`;
+};
