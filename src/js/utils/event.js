57a90954283535960a18f46fafeb5a7783ff4456
KIM YEJI <rladpwl0512@gmail.com>
2022-03-14 00:26:00 +0900

    :recycle: refactor: 사용하지 않는 import 삭제

--- src/js/utils/event.js
+++ src/js/utils/event.js
@@ -1,4 +1,3 @@
-// emit은 view에서 쓴다. element 커스텀이벤트를 붙여준다.
 export const emit = (target, eventName, detail) => {
   const event = new CustomEvent(eventName, { detail });
   target.dispatchEvent(event);
