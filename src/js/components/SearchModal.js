f662dcffa277df1ebc6b216a4cfdde7c75ec8a5a
InKyoJeong <viapolar@naver.com>
2022-03-14 02:40:07 +0900

    feat: 모달 닫기 기능 추가

--- src/js/components/SearchModal.js
+++ src/js/components/SearchModal.js
@@ -4,6 +4,8 @@ import SearchResult from "./SearchResult.js";
 export default class SearchModal {
   constructor({ searchManager }) {
     this.modalContainer = document.getElementById("modal-container");
+    this.modalCloseButton = document.getElementById("modal-close-button");
+    this.modalCloseButton.addEventListener("click", this.close);
 
     new SearchInput({ searchManager });
     new SearchResult({ searchManager });
@@ -12,4 +14,8 @@ export default class SearchModal {
   show() {
     this.modalContainer.classList.remove("hide");
   }
+
+  close = () => {
+    this.modalContainer.classList.add("hide");
+  };
 }
