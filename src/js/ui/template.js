d31d9d944df2d0cc0aeb0a6715ae02539b37186d
이아현 <lah1203@naver.com>
2022-03-13 20:02:19 +0900

    feat: 동영상, 채널로 이동 시 새 창으로 이동할 수 있도록 추가

--- src/js/ui/template.js
+++ src/js/ui/template.js
@@ -20,15 +20,22 @@ const getFoundResultTemplate = items => {
         item.snippet;
       return `
       <li class="video-item" data-video-id=${item.id.videoId}>
-        <a href="https://www.youtube.com/watch?v=${item.id.videoId}">
+        <a
+          href="https://www.youtube.com/watch?v=${item.id.videoId}"
+          target="_blank"
+        >
           <img
             src=${thumbnails.high.url}
             alt="video-item-thumbnail" class="video-item__thumbnail">
         </a>
-        <a href="https://www.youtube.com/watch?v=${item.id.videoId}">
+        <a href="https://www.youtube.com/watch?v=${item.id.videoId}"
+          target="_blank"
+        >
           <h4 class="video-item__title">${title}</h4>
         </a>
-        <a href="https://www.youtube.com/channel/${channelId}"><p class="video-item__channel-name">${channelTitle}</p></a>
+        <a href="https://www.youtube.com/channel/${channelId}" target="_blank">
+          <p class="video-item__channel-name">${channelTitle}</p>
+        </a>
         <p class="video-item__published-date">
           ${convertToKoreaLocaleDate(publishedAt)}
         </p>
