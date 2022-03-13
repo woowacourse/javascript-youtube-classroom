060e56e5df8159b2c29976522512dc055c104439
nan-noo <54002105+nan-noo@users.noreply.github.com>
2022-03-13 16:25:15 +0900

    refactor: rename file and variables

--- src/js/constants/contants.js
+++ src/js/constants/contants.js
@@ -1,12 +1,12 @@
-const MAX_VIDEO_COUNT = 10;
+const MAX_VIDEO_REQUEST_COUNT = 10;
 const MAX_VIDEO_LIST_LENGTH = 100;
 const MAX_SAVE_VIDEO_COUNT = 100;
 
-const BUTTON_DISABLED_TEXT = '저장됨';
+const BUTTON_SAVED_TEXT = '저장됨';
 
 const VIDEO_ID_LIST_KEY = 'VIDEO_ID_LIST';
 
-const REDIRECT_SERVER_HOST = 'https://youtube-classroom-project.netlify.app';
+const REDIRECT_SERVER_HOST = 'https://www.googleapis.com'; //'https://youtube-classroom-project.netlify.app';
 
 const ERROR_MESSAGE = {
   CANNOT_GET_YOUTUBE_VIDEO: '[404] 개발자에게 문의하세요.',
@@ -16,10 +16,10 @@ const ERROR_MESSAGE = {
 
 export {
   ERROR_MESSAGE,
-  MAX_VIDEO_COUNT,
+  MAX_VIDEO_REQUEST_COUNT,
   MAX_VIDEO_LIST_LENGTH,
   MAX_SAVE_VIDEO_COUNT,
-  BUTTON_DISABLED_TEXT,
+  BUTTON_SAVED_TEXT,
   VIDEO_ID_LIST_KEY,
   REDIRECT_SERVER_HOST,
 };
