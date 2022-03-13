0c69e1d2470424cc4a71a7338bafb2c0ec880b6d
moonheekim0118 <hellomooneekim@gmail.com>
2022-03-13 15:38:14 +0900

    ♻️ : 리듀서 내부 함수 메서드로 분리

--- src/js/domain/YoutubeSearchStore.js
+++ src/js/domain/YoutubeSearchStore.js
@@ -14,38 +14,45 @@ class YoutubeSearchStore extends Store {
 
   setReducer() {
     this.reducer = {
-      [YOUTUBE_SEARCH_ACTION.UPDATE_SEARCH_KEYWORD]: searchKeyword => {
-        this.setState({
-          ...this.state,
-          searchKeyword,
-          isLoading: true,
-          isLoaded: false,
-          items: [],
-          nextPageToken: '',
-          error: false,
-        });
-      },
-      [YOUTUBE_SEARCH_ACTION.UPDATE_SEARCH_LOADING_STATUS]: isLoading => {
-        this.setState({ ...this.state, isLoading });
-      },
-      [YOUTUBE_SEARCH_ACTION.UPDATE_SEARCH_RESULT]: async () => {
-        if (this.state.nextPageToken === undefined) return;
-        const {
-          items = [],
-          nextPageToken = '',
-          error = false,
-        } = await requestYoutubeSearch(this.state.searchKeyword, this.state.nextPageToken);
-
-        this.setState({
-          ...this.state,
-          isLoading: false,
-          isLoaded: true,
-          items: [...this.state.items, ...items],
-          error,
-          nextPageToken,
-        });
-      },
+      [YOUTUBE_SEARCH_ACTION.UPDATE_SEARCH_KEYWORD]: this.updateSearchKeyword.bind(this),
+      [YOUTUBE_SEARCH_ACTION.UPDATE_SEARCH_LOADING_STATUS]:
+        this.updateSearchLoadingStatus.bind(this),
+      [YOUTUBE_SEARCH_ACTION.UPDATE_SEARCH_RESULT]: this.updateSearchResult.bind(this),
     };
+  }
+
+  updateSearchKeyword(searchKeyword) {
+    this.setState({
+      ...this.state,
+      searchKeyword,
+      isLoading: true,
+      isLoaded: false,
+      items: [],
+      nextPageToken: '',
+      error: false,
+    });
+  }
+
+  updateSearchLoadingStatus(isLoading) {
+    this.setState({ ...this.state, isLoading });
+  }
+
+  async updateSearchResult() {
+    if (this.state.nextPageToken === undefined) return;
+    const {
+      items = [],
+      nextPageToken = '',
+      error = false,
+    } = await requestYoutubeSearch(this.state.searchKeyword, this.state.nextPageToken);
+
+    this.setState({
+      ...this.state,
+      isLoading: false,
+      isLoaded: true,
+      items: [...this.state.items, ...items],
+      error,
+      nextPageToken,
+    });
   }
 }
 
