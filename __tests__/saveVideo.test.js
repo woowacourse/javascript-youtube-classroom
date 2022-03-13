9922efea1d81aed3cefaf7426232d7932b747b3e
prefer2 <sunho6200@gmail.com>
2022-03-13 19:13:30 +0900

    refactor: 중복 테스트 케이스 제거

--- __tests__/saveVideo.test.js
+++ __tests__/saveVideo.test.js
@@ -5,15 +5,6 @@ describe('localStorage에 영상 ID 저장 기능 테스트', () => {
     jest.clearAllMocks();
   });
 
-  it('영상 ID를 저장한다.', () => {
-    const videoID = 'testVideoID';
-
-    const saveVideoManager = new SaveVideoManager();
-    saveVideoManager.saveVideoById(videoID);
-
-    expect(JSON.parse(localStorage.getItem('id').includes(videoID))).toBeTruthy();
-  });
-
   it('영상을 이미 저장했는지 확인할 수 있다.', () => {
     const videoID = 'testVideoID';
 
