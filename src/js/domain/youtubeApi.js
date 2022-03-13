719a2b9d4853703702890d82a1fe33d22afd0dc0
이아현 <lah1203@naver.com>
2022-03-13 16:44:25 +0900

    refactor: api fetch 방식 변경

--- src/js/domain/youtubeApi.js
+++ src/js/domain/youtubeApi.js
@@ -2,15 +2,23 @@ import 'regenerator-runtime';
 import { GET_VIDEO_UNIT, MESSAGE } from '../constants';
 
 const request = async (searchText, nextPageToken = '') => {
-  const response = await fetch(
-    `https://halee-youtube-api.netlify.app/youtube/v3/search?part=snippet&q=${searchText}&maxResults=${GET_VIDEO_UNIT}&type=video&pageToken=${nextPageToken}`,
-  );
+  let searchResult;
 
-  if (response.status === 200) {
-    const searchResult = await response.json();
-    return searchResult;
-  }
-  throw new Error(MESSAGE.ERROR_GET_REQUEST);
+  await fetch(
+    `https://halee-youtube-api.netlify.app/youtube/v3/search?part=snippet&q=${searchText}&maxResults=${GET_VIDEO_UNIT}&type=video&pageToken=${nextPageToken}`,
+  )
+    .then(async response => {
+      if (response.status === 200) {
+        searchResult = await response.json();
+      } else if (response.status === 403) {
+        throw Error(MESSAGE.ERROR_GET_REQUEST);
+      }
+    })
+    .catch(error => {
+      throw Error(error.message);
+    });
+
+  return searchResult;
 };
 
 export { request };
