48fad862a5322d35efea534cfdb71c1e086c9a7f
moonheekim0118 <hellomooneekim@gmail.com>
2022-03-14 01:13:23 +0900

    ✅ : 검색결과 업데이트 dispatch 메서드 테스트 추가

--- src/js/domain/YoutubeSearchStore.test.js
+++ src/js/domain/YoutubeSearchStore.test.js
@@ -1,5 +1,6 @@
 import YoutubeSearchStore from './YoutubeSearchStore';
 import { YOUTUBE_SEARCH_ACTION } from '../constants/action';
+import * as apiModules from '../api';
 
 describe('유튜브 강의실 검색 시도 상태 테스트', () => {
   test('검색어를 입력했을 때 상태가 정상적으로 저장되어야 한다.', () => {
@@ -12,6 +13,24 @@ describe('유튜브 강의실 검색 시도 상태 테스트', () => {
       isLoaded: false,
       items: [],
       nextPageToken: '',
+      error: false,
+    });
+  });
+
+  test('검색 결과를 업데이트 할 때 상태가 정상적으로 저장되어야 한다.', async () => {
+    YoutubeSearchStore.dispatch(YOUTUBE_SEARCH_ACTION.UPDATE_SEARCH_KEYWORD, '검색어');
+    const spy = jest.spyOn(apiModules, 'requestYoutubeSearch');
+    spy.mockReturnValue({
+      items: ['1', '2', '3'],
+      nextPageToken: '1',
+    });
+    await YoutubeSearchStore.dispatch(YOUTUBE_SEARCH_ACTION.UPDATE_SEARCH_RESULT);
+    expect(YoutubeSearchStore.getState()).toStrictEqual({
+      searchKeyword: '검색어',
+      isLoading: false,
+      isLoaded: true,
+      items: ['1', '2', '3'],
+      nextPageToken: '1',
       error: false,
     });
   });
