71852299462e7c126d25551606e80f4ec176bee8
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-03-14 05:28:20 +0900

    ✅ : 테스트 코드 재작성 - 실용성 없는 테스트 코드 제거 - 검색 결과 상태 처리 부분 테스트 코드 추가

--- src/js/__tests__/YoutubeSearchStore.test.js
+++ src/js/__tests__/YoutubeSearchStore.test.js
@@ -1,18 +1,43 @@
 import YoutubeSearchStore from '../domain/YoutubeSearchStore';
 import { ACTION_TYPE } from '../constants/String';
 
+const defaultState = {
+  searchKeyword: '',
+  isLoading: false,
+  isLoaded: false,
+  items: [],
+  totalResults: Number.MAX_SAFE_INTEGER,
+  nextPageToken: '',
+  error: false,
+};
+
 describe('유튜브 강의실 검색 시도 상태 테스트', () => {
   test('검색어를 입력했을 때 상태가 정상적으로 저장되어야 한다.', () => {
     const inputKeyword = '검색어';
-    YoutubeSearchStore.dispatch(ACTION_TYPE.UPDATE_SEARCH_KEYWORD, inputKeyword);
 
-    expect(YoutubeSearchStore.getState()).toStrictEqual({
+    const testState = {
+      ...defaultState,
       searchKeyword: inputKeyword,
       isLoading: true,
       isLoaded: false,
-      items: [],
-      nextPageToken: '',
-      error: false,
-    });
+    };
+
+    YoutubeSearchStore.dispatch(ACTION_TYPE.UPDATE_SEARCH_KEYWORD, inputKeyword);
+    expect(YoutubeSearchStore.getState()).toStrictEqual(testState);
+  });
+
+  test('현재 로드한 동영상의 갯수가 검색 결과의 총 갯수를 모두 불러왔을 때, 더 이상 동영상 목록을 요청하지 않아야 한다.', () => {
+    const testState = {
+      ...defaultState,
+      isLoaded: true,
+      searchKeyword: '우테코',
+      items: Array(24),
+      totalResults: 24,
+    };
+
+    YoutubeSearchStore.setState(testState);
+    YoutubeSearchStore.dispatch(ACTION_TYPE.UPDATE_SEARCH_RESULT);
+
+    expect(YoutubeSearchStore.getState()).toStrictEqual(testState);
   });
 });
