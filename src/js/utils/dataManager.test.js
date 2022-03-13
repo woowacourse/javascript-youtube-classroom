fc6bf100aa2cb58845c4bb6ec91a1eed76668fa8
moonheekim0118 <hellomooneekim@gmail.com>
2022-03-14 00:55:52 +0900

    ✅ : describe문 추가

--- src/js/utils/dataManager.test.js
+++ src/js/utils/dataManager.test.js
@@ -1,17 +1,19 @@
 import { uriBuilder, getParsedTime } from './dataManager';
 
-test('시스템 시간을 YYYY년 MM월 DD일의 형태로 반환한다.', () => {
-  const timeString = '2020-11-27T08:13:35Z';
-  const parsedTime = '2020년 11월 27일';
-  expect(getParsedTime(timeString)).toBe(parsedTime);
-});
+describe('dataManager 유틸 함수 테스트', () => {
+  test('시스템 시간을 YYYY년 MM월 DD일의 형태로 반환한다.', () => {
+    const timeString = '2020-11-27T08:13:35Z';
+    const parsedTime = '2020년 11월 27일';
+    expect(getParsedTime(timeString)).toBe(parsedTime);
+  });
 
-test('파라미터값을 받아서 올바른 uri을 생성한다.', () => {
-  const baseUri = 'http://google.com';
-  const params = {
-    q: 'keyword',
-    maxResult: 5,
-  };
-  const uri = `${baseUri}?q=keyword&maxResult=5`;
-  expect(uriBuilder(baseUri, params)).toBe(uri);
+  test('파라미터값을 받아서 올바른 uri을 생성한다.', () => {
+    const baseUri = 'http://google.com';
+    const params = {
+      q: 'keyword',
+      maxResult: 5,
+    };
+    const uri = `${baseUri}?q=keyword&maxResult=5`;
+    expect(uriBuilder(baseUri, params)).toBe(uri);
+  });
 });
