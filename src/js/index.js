3748d39fc99dc0681daaa450b801a9feb626a384
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-03-14 21:17:52 +0900

    ♻️ : Index 처리 방식 간소화

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,12 @@
-import App from '@Display/App';
+import Navigation from '@Display/YoutubeClassRoom/Navigation';
+import Modal from '@Display/Share/Modal';
+import SearchForm from '@Display/YoutubeClassRoom/SearchForm';
+import SearchResult from '@Display/YoutubeClassRoom/SearchResult';
+import '@Style/index.scss';
 
-new App();
+document.addEventListener('DOMContentLoaded', () => {
+  new Modal();
+  new Navigation();
+  new SearchForm();
+  new SearchResult();
+});
