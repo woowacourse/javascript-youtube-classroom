efa385b732fec1b7a8a953081aa8734ad8eb46a4
Ahn <jin7969@gmail.com>
2022-03-15 00:46:03 +0900

    test: 테스트 코드 삭제 및 추가

--- src/__test__/index.test.js
+++ src/__test__/index.test.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 import YoutubeMachine from '../js/domain/YoutubeMachine.js';
 import storage from '../js/storage/storage.js';
-import dummyData from '../../dummyData.js';
+import { MAX_VIDEO_COUNT } from '../js/constants/constants.js';
 
 describe('유튜브 검색 앱 테스트', () => {
   test('입력된 검색어가 없으면, 에러 메시지를 보여준다.', () => {
@@ -9,19 +9,6 @@ describe('유튜브 검색 앱 테스트', () => {
     const searchInput = '';
 
     expect(() => (youtubeMachine.searchTarget = searchInput)).toThrow(Error);
-  });
-
-  test('정상으로 데이터를 받아오면, 데이터가 필요한 프로퍼티를 가지고 있는지 확인한다.', () => {
-    const youtubeMachine = new YoutubeMachine();
-    youtubeMachine.data = dummyData;
-
-    youtubeMachine.data.items.forEach(video => {
-      expect(video).toHaveProperty('id');
-      expect(video.snippet).toHaveProperty('title');
-      expect(video.snippet).toHaveProperty('channelTitle');
-      expect(video.snippet).toHaveProperty('thumbnails');
-      expect(video.snippet).toHaveProperty('publishedAt');
-    });
   });
 
   test('유튜브 영상 id를 webstorage에 저장할 수 있다.', () => {
@@ -39,5 +26,16 @@ describe('유튜브 검색 앱 테스트', () => {
     storage.updateLocalStorage(videoId);
 
     expect(storage.getLocalStorage()).toHaveLength(1);
+  });
+
+  test('유튜브 영상 id는 100개 초과로 저장 할 수 없다.', () => {
+    const videoId = 'eMf0jojpdJQ';
+    const savedId = Array.from({ length: MAX_VIDEO_COUNT }, () => videoId);
+
+    // 100개를 저장하고, 1개를 더 시도 한다
+    storage.setLocalStorage(savedId);
+    storage.updateLocalStorage(videoId);
+
+    expect(storage.getLocalStorage()).toHaveLength(MAX_VIDEO_COUNT);
   });
 });
