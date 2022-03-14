2c4d3f96c00e006faebc7b932c90f20fd2027ac7
airman5573 <rpf5573@kakao.com>
2022-03-14 19:31:01 +0900

    :white_check_mark: test(cleanup): 효용성 없는 테스트를 삭제한다
    아래 테스트가 checkSearchResult함수의 유효성을 검사해 주기 떄문에, 이 테스트는 필요없다.

--- src/js/__test__/searchModal.test.js
+++ src/js/__test__/searchModal.test.js
@@ -5,14 +5,7 @@ import SearchModal from "../searchModal";
 const searchModal = new SearchModal();
 
 describe('검색 결과에 따라 생성된 videos가 알맞은 값을 가지는지 테스트', () => {
-  test('fetch로 가져온 데이터가 null이 아닐 경우, 생성된 video의 length가 알맞은지 체크', async () => {
-    const dummy = { ...DUMMY_VIDEO_LIST };
-    dummy.items = dummy.items.slice(0, MAX_RENDER_VIDEOS_COUNT);
-    const videos = searchModal.checkSearchResult(dummy);
-    expect(videos).toHaveLength(MAX_RENDER_VIDEOS_COUNT);
-  });
-  
-  test('fetch로 가져온 데이터가 null이 아닐 경우, 생성된 video가 view 구성을 위한 프로퍼티를 가지고 있는지 체크', async () => {
+  test('Youtube API를 통해 가져온 검색 결과 리스트가 화면을 구성하는데 필요한 프로퍼티를 가지고 있는지 확인한다', async () => {
     const dummy = { ...DUMMY_VIDEO_LIST };
     const videos = searchModal.checkSearchResult(dummy);
     videos.forEach(video => {
@@ -20,6 +13,6 @@ describe('검색 결과에 따라 생성된 videos가 알맞은 값을 가지는
       expect(video).toHaveProperty('channelTitle');
       expect(video).toHaveProperty('thumbnailUrl');
       expect(video).toHaveProperty('publishedAt');
-    })
-  })
+    });
+  });
 });
