4b5373a6eea1223b06c67ff159c8944d42412ee8
sanaandmomo <qajjang46@naver.com>
2022-03-14 21:18:30 +0900

    refactor: 에러 메세지 상수화
    Co-authored-by: ronci <ronci@users.noreply.github.com>

--- src/js/Validator/index.js
+++ src/js/Validator/index.js
@@ -1,6 +1,7 @@
 import ValidationError from '../ValidationError/index.js';
 import { isEmpty } from '../utils/index.js';
+import { ERROR_MESSAGE } from '../constants/index.js';
 
 export const checkKeyword = (keyword) => {
-  if (isEmpty(keyword)) throw new ValidationError('검색어를 입력해주세요!');
+  if (isEmpty(keyword)) throw new ValidationError(ERROR_MESSAGE.EMPTY_KEYWORD);
 };
