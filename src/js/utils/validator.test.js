efbdab7a6f9b84fd555e0773a6fd0db21c339603
kamwoo <rkadndud06@naver.com>
2022-03-15 05:15:47 +0900

    test: validator 테스트 명세 수정

--- src/js/utils/validator.test.js
+++ src/js/utils/validator.test.js
@@ -1,14 +1,14 @@
 import validator from './validator.js';
 import { ERROR_MESSAGE } from './constants.js';
 
-describe('보고 싶은 영상을 검색 했을 때', () => {
-  test('입력 없이 버튼을 눌렀다면 error를 throw한다.', () => {
-    const inputValue = '';
+describe('입력된 검색 문자열을 확인하여', () => {
+  test('비어있는 문자열이라면 error를 throw한다.', () => {
+    const inputValue = '  ';
     expect(() => validator.isValidSearchInput(inputValue)).toThrowError(ERROR_MESSAGE.EMPTY_INPUT);
   });
 });
 
-describe('저장하기 버튼을 눌렸을 때', () => {
+describe('비디오 id를 LocalStorage에 저장할 때', () => {
   test('비디오 id 리스트가 100 이상이면 error를 throw한다.', () => {
     const videoIdList = Array(100).fill('id');
 
