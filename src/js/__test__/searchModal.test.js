869ebb80958d72d900fcf3d152bb86003e43a97f
jihyeok-um <75592315+jihyeok-um@users.noreply.github.com>
2022-03-14 22:38:28 +0900

    refactor: 테스트코드의 설명을 알아보기 쉽게 수정

--- src/js/__test__/searchModal.test.js
+++ src/js/__test__/searchModal.test.js
@@ -4,15 +4,15 @@ import SearchModal from "../searchModal";
 
 const searchModal = new SearchModal();
 
-describe('검색 결과에 따라 생성된 videos가 알맞은 값을 가지는지 테스트', () => {
-  test('fetch로 가져온 데이터가 null이 아닐 경우, 생성된 video의 length가 알맞은지 체크', async () => {
+describe('videos', () => {
+  test('videos가 최대 렌더할 비디오 수만큼 items를 가지고 있는지 체크', async () => {
     const dummy = { ...DUMMY_VIDEO_LIST };
     dummy.items = dummy.items.slice(0, MAX_RENDER_VIDEOS_COUNT);
     const videos = searchModal.checkSearchResult(dummy);
     expect(videos).toHaveLength(MAX_RENDER_VIDEOS_COUNT);
   });
   
-  test('fetch로 가져온 데이터가 null이 아닐 경우, 생성된 video가 view 구성을 위한 프로퍼티를 가지고 있는지 체크', async () => {
+  test('videos가 view 구성을 위한 프로퍼티를 가지고 있는지 체크', async () => {
     const dummy = { ...DUMMY_VIDEO_LIST };
     const videos = searchModal.checkSearchResult(dummy);
     videos.forEach(video => {
