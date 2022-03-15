d1e77a6d6e55d52d9dbd38bed8f2f0f680052044
gytks4 <gytks4@naver.com>
2022-03-15 20:22:33 +0900

    refactor: URL, URLSearchParams 생성자 사용

--- src/js/api/api.js
+++ src/js/api/api.js
@@ -4,28 +4,28 @@ const OPTIONS = {
   part: 'snippet',
   type: 'video',
   maxResults: 10,
+  q: null,
+  pageToken: '',
 };
 
-export const getSearchAPI = async (query, pageToken = null) => {
-  const url = `${BASE_URL}?${spreadOptions({
+export const getSearchAPI = async (query, pageToken = '') => {
+  const url = new URL(BASE_URL);
+  const parameters = new URLSearchParams({
     ...OPTIONS,
     q: encodeQuery(query),
     pageToken,
-  })}`;
+  });
+  url.search = parameters.toString();
+
   const response = await fetch(url);
   const data = await response.json();
+
   if (response.ok) {
     return [null, data];
   }
 
   return [{ message: data.error.message, statusCode: response.status }, null];
 };
-
-function spreadOptions(options) {
-  return Object.keys(options)
-    .map(key => options[key] && `${key}=${options[key]}`)
-    .join('&');
-}
 
 function encodeQuery(query) {
   return query
