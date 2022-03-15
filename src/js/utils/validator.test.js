9c584ca87c9666269b7bbdc19173120ead55f0a1
kamwoo <rkadndud06@naver.com>
2022-03-15 09:31:35 +0900

    test: validator 테스트 명세 수정

--- src/js/utils/validator.test.js
+++ src/js/utils/validator.test.js
@@ -1,19 +1,15 @@
 import validator from './validator.js';
 import { ERROR_MESSAGE } from './constants.js';
 
-describe('입력된 검색 문자열을 확인하여', () => {
-  test('비어있는 문자열이라면 error를 throw한다.', () => {
-    const inputValue = '  ';
-    expect(() => validator.checkSearchInput(inputValue)).toThrowError(ERROR_MESSAGE.EMPTY_INPUT);
-  });
+test('입력된 검색어가 비어있는 문자열이라면 error를 throw한다.', () => {
+  const inputValue = '  ';
+  expect(() => validator.checkSearchInput(inputValue)).toThrowError(ERROR_MESSAGE.EMPTY_INPUT);
 });
 
-describe('비디오 id를 LocalStorage에 저장할 때', () => {
-  test('비디오 id 리스트가 100 이상이면 error를 throw한다.', () => {
-    const videoIdList = Array(100).fill('id');
+test('비디오 id 리스트가 길이가 100 이상이면 error를 throw한다.', () => {
+  const videoIdList = Array(100).fill('id');
 
-    expect(() => validator.checkOverVideoIdListMaxLength(videoIdList)).toThrowError(
-      ERROR_MESSAGE.OVER_MAX_STORE_LENGTH
-    );
-  });
+  expect(() => validator.checkOverVideoIdListMaxLength(videoIdList)).toThrowError(
+    ERROR_MESSAGE.OVER_MAX_STORE_LENGTH
+  );
 });
