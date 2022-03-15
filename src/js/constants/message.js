34e9956bd4ce6248b671de159fedc055fecd1ec0
moonheekim0118 <hellomooneekim@gmail.com>
2022-03-15 20:08:07 +0900

    ♻️ : Object.freeze 제거

--- src/js/constants/message.js
+++ src/js/constants/message.js
@@ -1,4 +1,4 @@
-export const ERROR_MESSAGE = Object.freeze({
+export const ERROR_MESSAGE = {
   EMPTY_SEARCH_KEYWORD: '검색어를 입력해주세요.',
   MAX_SAVE_VIDEO: '동영상은 최대 100개까지 저장할 수 있습니다.',
-});
+};
--- src/js/constants/setting.js
+++ src/js/constants/setting.js
@@ -1,5 +1,5 @@
-export const YOUTUBE_SETTING = Object.freeze({
+export const YOUTUBE_SETTING = {
   URI: 'https://www.googleapis.com/youtube/v3/search',
   MAX_VIDEO_NUMBER: 10,
   MAX_SAVE_NUMBER: 100,
-});
+};
