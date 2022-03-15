787d496e65d95e98ad4da1f573878415dc9d0298
intae92 <kangit2013@gmail.com>
2022-03-15 13:07:15 +0900

    test: localStorage 최대 100개 저장 테스트
    - localStorage 최대 100개 저장 함수 분리

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
     "globals": {
         "YOUTUBE_URL": 'https://competent-haibt-c82cf4.netlify.app/youtube/v3/search?',
         "YOUTUBE_URL_DUMMY": 'https://competent-haibt-c82cf4.netlify.app/dummy/youtube/v3/search?',
-    }
+    },
+    "testEnvironment": "jsdom"
 }
--- src/js/common/VideosCardContainer.js
+++ src/js/common/VideosCardContainer.js
@@ -1,6 +1,8 @@
-import { LOCALSTORAGE_KEY, getStorageVideoIDs, setStorageVideoIDs } from '../utils/localStorage';
-import { RULES } from '../constants';
+import { LOCALSTORAGE_KEY, getStorageVideoIDs, setStorageVideoIDs, checkVideoStorageFull } from '../utils/localStorage';
 import VideoCard from './VideoCard';
+import toast from './toast';
+
+const toastPopup = toast();
 
 export default class VideoCardContainer {
   #state;
@@ -22,20 +24,22 @@ export default class VideoCardContainer {
   }
 
   storeIDHandler(e) {
-    if (e.target.className.includes('video-item__save-button')) {
-      const videoID = e.target.closest('li').dataset.videoId;
-      const videoIDs = getStorageVideoIDs(LOCALSTORAGE_KEY);
+    try {
+      if (e.target.className.includes('video-item__save-button')) {
+        const videoID = e.target.closest('li').dataset.videoId;
+        const videoIDs = getStorageVideoIDs(LOCALSTORAGE_KEY);
 
-      if (videoIDs.length >= RULES.MAX_STORED_IDS_AMOUNT) {
-        return;
+        checkVideoStorageFull(LOCALSTORAGE_KEY);
+
+        setStorageVideoIDs({
+          key: LOCALSTORAGE_KEY,
+          value: videoIDs.concat(videoID),
+        });
+
+        e.target.remove();
       }
-
-      setStorageVideoIDs({
-        key: LOCALSTORAGE_KEY,
-        value: videoIDs.concat(videoID),
-      });
-
-      e.target.remove();
+    } catch (err) {
+      toastPopup(err.message);
     }
   }
 
