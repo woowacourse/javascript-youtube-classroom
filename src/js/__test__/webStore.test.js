8d5330addbbd343e09fa94954706f9939eee2ae4
junzzi <jjh01027717502@gmail.com>
2022-03-15 18:09:14 +0900

    test: webStore 테스트 추가

---
+++ src/js/__test__/webStore.test.js
@@ -0,0 +1,29 @@
+const { default: webStore } = require('../modules/webStore');
+
+describe('webStore 모듈 테스트', () => {
+  jest.mock(
+    'local-storage',
+    () => {
+      const storage = {};
+      return {
+        getItem(key) {
+          return storage[key];
+        },
+        setItem(key, data) {
+          storage[key] = data;
+        },
+      };
+    },
+    { virtual: true }
+  );
+  test('데이터를 저장할 수 있다.', () => {
+    global.localStorage = require('local-storage');
+
+    const newVideoId = 'gadsgasdg';
+    webStore.setDataInArray('video', newVideoId);
+
+    const videoIdList = webStore.getArrayData('video');
+
+    expect(videoIdList.includes(newVideoId)).toBe(true);
+  });
+});
