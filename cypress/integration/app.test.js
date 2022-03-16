56ea8f31e1e10e48bbdb7d611cdcf5ef2854a031
junzzi <jjh01027717502@gmail.com>
2022-03-16 17:02:59 +0900

    test: 빈 검색어 입력 불가 테스트 개발

--- cypress/integration/app.test.js
+++ cypress/integration/app.test.js
@@ -8,17 +8,29 @@ describe('사용자는 검색을 통해 영상을 확인할 수 있다.', () => 
     cy.get('.modal-container').should('exist');
   });
   it('모달 버튼을 클릭 후, 검색어를 입력하면 결과를 확인할 수 있다.', () => {
-    const keyword = '이하이';
+    const validKeyword = '이하이';
     cy.showModal();
-
-    cy.get('#search-input-keyword').type(keyword);
-
+    cy.get('#search-input-keyword').type(validKeyword);
     cy.get('#search-button')
       .click()
       .then(() => {
         cy.get('.video-item').should('exist');
       });
   });
-  it('빈 검색어를 입력하면 경고창을 확인하게 된다.', () => {});
-  it('스크롤을 내리면 다음 영상들을 확인할 수 있다.', () => {});
+  it('빈 검색어를 입력하면 경고창을 확인하게 된다.', () => {
+    const inValidKeyword = ' ';
+
+    const stub = cy.stub();
+    cy.on('window:alert', stub);
+
+    cy.showModal();
+
+    cy.get('#search-input-keyword').type(inValidKeyword);
+
+    cy.get('#search-button')
+      .click()
+      .then(() => {
+        expect(stub).to.be.called;
+      });
+  });
 });
