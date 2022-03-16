54156caf51b123040ae327adf8a356b81e066f6b
woose28 <dldntmdsla@naver.com>
2022-03-16 16:22:10 +0900

    test: 빈 검색어로 검색 시 alert 창이 나타나는지 검증하는 테스트 코드 작성

---
+++ cypress/integration/search.spec.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-undef */
+/* eslint-disable max-lines-per-function */
+
+import { ERROR_MESSAGE } from '../../src/js/constants/errorMessage.js';
+
+describe('검색 테스트', () => {
+  beforeEach(() => {
+    const LOCAL_URL = 'http://localhost:9000/';
+    cy.visit(LOCAL_URL);
+  });
+  it('빈 검색어를 입력하면 alert 창이 나타나야 한다.', () => {
+    // given
+    const alertStub = cy.stub();
+    cy.on('window:alert', alertStub);
+
+    cy.get('#search-modal-button').click();
+
+    // when
+    cy.get('#search-button')
+      .click()
+      .then(() => {
+        // then
+        expect(alertStub.getCall(0)).to.be.calledWith(ERROR_MESSAGE.EMPTY_KEYWORD);
+      });
+  });
+});
