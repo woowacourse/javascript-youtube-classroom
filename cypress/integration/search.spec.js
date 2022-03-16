f54770b90b997982b87801e04e92a901f47a76d3
woose28 <dldntmdsla@naver.com>
2022-03-16 18:58:01 +0900

    test: 초기 화면에서 검색 버튼을 눌렀을 때 모달이 보이는지 검증하는 테스트 추가

--- cypress/integration/search.spec.js
+++ cypress/integration/search.spec.js
@@ -3,9 +3,22 @@
 
 import { ERROR_MESSAGE } from '../../src/js/constants/errorMessage.js';
 
+const LOCAL_URL = 'http://localhost:9000/';
+
+describe.only('모달 동작 검사', () => {
+  before(() => {
+    cy.visit(LOCAL_URL);
+
+    cy.get('#search-modal-button').click();
+  });
+
+  it('초기 화면에서 검색 버튼을 누르면 검색 모달 창이 보여야 한다.', () => {
+    cy.get('.modal-container').should('be.exist');
+  });
+});
+
 describe('검색 키워드 유효성 검사', () => {
   beforeEach(() => {
-    const LOCAL_URL = 'http://localhost:9000/';
     cy.visit(LOCAL_URL);
 
     cy.get('#search-modal-button').click();
