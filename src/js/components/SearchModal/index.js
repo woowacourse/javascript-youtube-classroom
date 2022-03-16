f6b203369ce1ae71421975c9475f470fadba02ee
InKyoJeong <viapolar@naver.com>
2022-03-16 17:30:54 +0900

    feat: 모달 닫기 기능 추가 - 모달 영역 밖을 눌러도 닫도록 추가

--- src/js/components/SearchModal/index.js
+++ src/js/components/SearchModal/index.js
@@ -6,7 +6,9 @@ export default class SearchModal {
   constructor() {
     this.searchManager = new SearchManager();
     this.modalContainer = document.querySelector(".modal");
+
     document.querySelector(".search-modal__close-button").addEventListener("click", this.closeModal);
+    document.querySelector(".dimmer").addEventListener("click", this.closeModal);
 
     new SearchInput({ searchManager: this.searchManager });
     new SearchResult({ searchManager: this.searchManager });
