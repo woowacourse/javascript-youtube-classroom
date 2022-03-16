5ac48bbe4cf1041aa03ca801bacd7c004e5f6917
woose28 <dldntmdsla@naver.com>
2022-03-16 14:07:08 +0900

    refacetor: runApp 함수 실행 위치 수정

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
 import './css/index.css';
-import runApp from './js/index';
+import './js/index';
 import '../src/assets/images/not_found.png';
-
-window.addEventListener('DOMContentLoaded', () => {
-  runApp();
-});
