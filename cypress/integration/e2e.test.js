533aa9cec6914508150ad1bfa4228cd77ab07d48
sangminsong <ssmin940606@gmail.com>
2022-03-16 20:38:34 +0900

    test: 유튜브 검색 예외 사항 테스트 작성
    - 검색 결과가 없는 경우 결과없음 페이지를 확인할 수 있다

--- cypress/integration/e2e.test.js
+++ cypress/integration/e2e.test.js
@@ -1,7 +1,5 @@
 import { VIDEO_COUNT, STORAGE_KEY_SAVED_VIDEOS } from '../../src/js/util/constants.js';
 
-// localStorage 초기화도 필요해보임
-
 describe('유튜브 검색 정상 작동 테스트', () => {
   before(() => {
     cy.visit('../../dist/index.html');
@@ -17,12 +15,7 @@ describe('유튜브 검색 정상 작동 테스트', () => {
     it('검색창에 검색어를 입력하고 검색 버튼을 클릭하면 결과물을 확인할 수 있다', () => {
       const keyword = 'javascript';
 
-      cy.intercept(
-        'GET',
-        `https://www.googleapis.com/youtube/v3/search?type=video&key=${Cypress.env(
-          'YOUTUBE_API_KEY'
-        )}&q=${keyword}&part=snippet&maxResults=${VIDEO_COUNT}`
-      ).as('getVideos');
+      cy.intercept('GET', 'https://www.googleapis.com/youtube/v3/search?*').as('getInitialVideos');
 
       // 검색
       cy.get('#search-input-keyword').type(keyword);
@@ -30,7 +23,7 @@ describe('유튜브 검색 정상 작동 테스트', () => {
       // 스켈레톤 UI
       cy.get('.video-list').children('.skeleton').should('be.visible');
 
-      cy.wait('@getVideos');
+      cy.wait('@getInitialVideos');
 
       // 결과값 보여주기
       cy.get('.video-list')
@@ -40,25 +33,20 @@ describe('유튜브 검색 정상 작동 테스트', () => {
         .and('not.have.class', 'skeleton');
     });
 
-    // it('결과물의 맨 밑까지 스크롤을 내리면 추가적인 결과물을 확인할 수 있다', () => {
-    //   const keyword = 'javascript';
+    it('결과물의 맨 밑까지 스크롤을 내리면 추가적인 결과물을 확인할 수 있다', () => {
+      const keyword = 'javascript';
 
-    //   cy.intercept(
-    //     'GET',
-    //     `https://www.googleapis.com/youtube/v3/search?type=video&key=${Cypress.env(
-    //       'YOUTUBE_API_KEY'
-    //     )}&q=${keyword}&part=snippet&maxResults=${VIDEO_COUNT}&pageToken=CAoQAA`
-    //   ).as('getNextVideos');
+      cy.intercept('GET', 'https://www.googleapis.com/youtube/v3/search?*').as('getNextVideos');
 
-    //   // 스크롤 내리기
-    //   cy.get('.video-list').scrollTo('bottom');
+      // 스크롤 내리기
+      cy.get('.video-list').scrollTo('bottom');
 
-    //   // 무한스크롤에 의한 결과값 보여주기
-    //   cy.wait('@getNextVideos');
-    //   cy.get('.video-list')
-    //     .children('.video-item')
-    //     .should('have.length', VIDEO_COUNT * 2);
-    // });
+      // 무한스크롤에 의한 결과값 보여주기
+      cy.wait('@getNextVideos');
+      cy.get('.video-list')
+        .children('.video-item')
+        .should('have.length', VIDEO_COUNT * 2);
+    });
   });
 
   context('저장 버튼을 클릭하면, 비디오ID를 저장할 수 있다', () => {
@@ -76,10 +64,24 @@ describe('유튜브 검색 정상 작동 테스트', () => {
   });
 });
 
-// describe('검색 모달창 관련 실패 테스트', () => {
-//   it('검색 결과가 없는 경우 결과없음 페이지를 확인할 수 있다', () => {
-//     // 검색어를 비워준다.
-//     // 검색을 한다
-//     // no result를 보여준다
-//   });
-// });
+describe('유튜브 검색 예외 사항 테스트', () => {
+  before(() => {
+    cy.visit('../../dist/index.html');
+    cy.get('#search-modal-button').click();
+  });
+
+  it('검색 결과가 없는 경우 결과없음 페이지를 확인할 수 있다', () => {
+    const noResultKeyword = '!@#!@$#$!#@!#';
+
+    cy.intercept('GET', `https://www.googleapis.com/youtube/v3/search?*`).as('getNoResult');
+
+    cy.get('#search-input-keyword').type(noResultKeyword);
+    cy.get('#search-button').click();
+
+    cy.wait('@getNoResult');
+
+    cy.get('.search-result--no-result').within(() => {
+      cy.get('img').should('have.attr', 'alt');
+    });
+  });
+});
