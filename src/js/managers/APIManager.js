9e2043f59ca8f1c891e67588f45dde4561efbe31
kamwoo <rkadndud06@naver.com>
2022-03-17 12:10:16 +0900

    feat: 응답 데이터 mock data 반환으로 변경

--- src/js/managers/APIManager.js
+++ src/js/managers/APIManager.js
@@ -1,14 +1,16 @@
 import { ERROR_MESSAGE } from '../utils/constants.js';
+import { videoData } from '../utils/mockData.js';
 
 const APIManager = {
   async fetchData(requestURL) {
     try {
-      const response = await fetch(requestURL);
-      if (!response.ok) {
-        throw new Error();
-      }
-      const responseData = await response.json();
-      return responseData;
+      // const response = await fetch(requestURL);
+      // if (!response.ok) {
+      //   throw new Error();
+      // }
+      // const responseData = await response.json();
+      // return responseData;
+      return videoData;
     } catch (error) {
       throw new Error(ERROR_MESSAGE.SEARCH_ERROR);
     }
