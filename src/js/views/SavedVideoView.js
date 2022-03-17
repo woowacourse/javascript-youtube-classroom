78ddd50fe9fa8ec16e5792518a28646c1696bae8
2019015414 <2019015414@hanyang.ac.kr>
2022-03-18 05:14:49 +0900

    feat: 저장된 영상 출력

--- src/js/views/SavedVideoView.js
+++ src/js/views/SavedVideoView.js
@@ -1,14 +1,40 @@
 import { $ } from './utils.js';
-import { SELECTOR } from '../constants/index.js';
+import { YOUTUBE_API_REQUEST_COUNT, SELECTOR } from '../constants/index.js';
 
 export default class SavedVideoView {
-  constructor() {
+  #$playlist;
+  #$watched;
+
+  constructor(savedVideoIds) {
+    console.log(savedVideoIds);
+    this.#$playlist = $('#playlist-video');
+    this.#$watched = $('#watched-video');
     this.bindSavedVideoList();
+    this.#appendVideos(savedVideoIds);
   }
 
   bindSavedVideoList() {
     $(SELECTOR.PLAYLIST_VIDEO).addEventListener('click', this.#changeVideoListContents.bind(this, 'add'));
     $(SELECTOR.WATCHED_VIDEO).addEventListener('click', this.#changeVideoListContents.bind(this, 'remove'));
+  }
+
+  #appendVideos(videos) {
+    console.log(videos);
+    const html = videos
+      .map(
+        (video) =>
+          `<li class="video-item">
+        <img
+          src="${video.thumbnail}"
+          alt="video-item-thumbnail" class="video-item__thumbnail">
+        <h4 class="video-item__title">[Playlist] ${video.title}</h4>
+        <p class="video-item__channel-name">${video.channelTitle}</p>
+        <p class="video-item__published-date">${video.date}</p>
+        <button data-video-id="${video.id}" class="video-item__save-button button ${video.saved ? 'saved' : ''}" type="button">⬇ 저장</button>
+      </li>`
+      )
+      .join('');
+    this.#$playlist.insertAdjacentHTML('beforeend', html);
   }
 
   #changeVideoListContents(option) {
