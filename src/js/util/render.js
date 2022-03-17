2a12e96b85864797d0dbc2cbb6e4d9f99df21937
JASUN LEE <liswktjs@gmail.com>
2022-03-17 18:06:09 +0900

    refactor: 반복되서 사용하는 template 분리

---
+++ src/js/util/render.js
@@ -0,0 +1,20 @@
+export const videoItemTemplate = {
+  videoItem: item => {
+    return `
+      <li class="watch-later-video-item video-item" data-video-id='${item.id}'>
+      <img
+        src='${item.snippet.thumbnails.high.url}'
+        alt="video-item-thumbnail"
+        class="video-item__thumbnail"
+      />
+      <h4 class="video-item__title">${item.snippet.title}</h4>
+      <p class="video-item__channel-name">${item.snippet.channelTitle}</p>
+      <p class="video-item__published-date">${item.snippet.publishTime}</p>
+      <div class="button-container">
+          <button type="button" class="video-item__watched-button button">✅</button>
+          <button type="button" class="video-item__delete-button button">🗑️</button>
+      </div>
+    </li>
+      `;
+  },
+};
