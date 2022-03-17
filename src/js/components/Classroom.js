1f2a129f751d62eed97bfa6dad2b56699c5943a8
YUNHO KANG <ansghkdbsgh@gmail.com>
2022-03-17 12:54:30 +0900

    feat: add saved video nav
    details below:
    - render saved video items

--- src/js/components/Classroom.js
+++ src/js/components/Classroom.js
@@ -1,7 +1,29 @@
+import { getVideoItemsFromLocalStorage } from '../utils/localStorage.js';
+import { makeThumbnailTemplate } from '../utils/templates.js';
+
 export class Classroom {
   constructor(props) {
     this.props = props;
+
     this.openModalButton = document.getElementById('search-modal-button');
     this.openModalButton.addEventListener('click', this.props.openModal);
+
+    this.contentsContainer = document.getElementById('classroom-contents-container');
+    this.willSeeVideoButton = document.getElementById('will-see-video-button');
+    this.willSeeVideoButton.addEventListener('click', this.handleWillSeeVideoButton);
+  }
+
+  handleWillSeeVideoButton = async () => {
+    this.clearClassroomContentsContainer();
+    const videoList = getVideoItemsFromLocalStorage();
+    console.log(videoList);
+    this.contentsContainer.insertAdjacentHTML(
+      'beforeend',
+      videoList.map((video) => makeThumbnailTemplate(video)).join(''),
+    );
+  };
+
+  clearClassroomContentsContainer() {
+    this.contentsContainer.innerHTML = ``;
   }
 }
