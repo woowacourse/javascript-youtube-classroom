80db180dac07b4a2de9b9834645603a8a51dcc8b
yunjin-kim <79692272+yunjin-kim@users.noreply.github.com>
2022-03-17 18:31:42 +0900

    test: 테스트 코드 수정

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -4,7 +4,7 @@ describe('비디오 관련 액션 테스트', () => {
     cy.visit('http://localhost:9000/');
   });
 
-  it('비디오 검색 후 비디오를 하나 저장하면 볼 영상에 저장한 비디오가 있어야 하고 본 영상 버튼을 클릭하면 본 영상 섹션에 비디오가 있어야 한다', () => {
+  it('비디오 검색, 비디오 저장, 본 비디오 체크, 비디오 삭제가 가능해야 한다', () => {
     cy.get('#search-modal-button').click();
     cy.get('.search-input__keyword').type('잔나비');
     cy.get('.search-input__search-button')
@@ -20,12 +20,12 @@ describe('비디오 관련 액션 테스트', () => {
             cy.get('#watched-section-button').click();
             const watchedVideoTitle = cy.get('.video-item').get('h4').eq(0);
             watchedVideoTitle.should('have.text', text1);
+            cy.get('.video-item').get('.delete-video-button').eq(0).click();
+            cy.on('window.confirm', () => {
+              returns(true);
+            })
+            cy.get('.watched-video-list').children().should('not.have', '');
           });
       });
   });
-
-//   it('저장된 영상 섹션에서 영상을 삭제할 수 있어야 한다', () => {
-//     cy.get('.video-item').get('.delete-video-button').eq(0).click();
-//     cy.get('.watched-video').children().should('have.legnth', 0);
-//   });
 });
