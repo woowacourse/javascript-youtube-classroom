147910379b6ba067f9785568dc7783bf40505ac0
kkojae <jaejeung3210@gmail.com>
2022-03-18 01:10:21 +0900

    test: 테스트 케이스 작성, 테스트 케이스 파일 분리
    - 메인 화면과 모달창 분리하여 테스트 케이스 작성
    - 사용자가 저장한 비디오 목록이 존재하는지 확인하는 테스트 케이스 작성

---
+++ cypress/integration/mainApp.test.js
@@ -0,0 +1,22 @@
+import videoStorage from "../../src/js/VideoStorage";
+
+describe("사용자가 저장한 비디오 목록이 존재하는지 확인한다.", () => {
+  it("사용자가 저장한 비디오 목록이 존재하지 않을 경우 나만의 유튜브 강의실 메인 화면에 '저장된 비디오 목록이 없습니다.' 메시지를 보여준다.", () => {
+    cy.visit("./index.html");
+
+    cy.get(".save-video-container__no-video-list").should("be.visible");
+  });
+
+  it("사용자가 저장한 비디오 목록이 존재할 경우 나만의 유튜브 강의실 메인 화면에 저장된 비디오 목록을 보여준다.", () => {
+    videoStorage.addVideo({
+      videoId: "kkojaeId",
+      thumbnailUrl: "https:",
+      title: "this is title",
+      channelName: "kkojae's channel",
+      publishDate: "2022년 3월 3일",
+    });
+
+    cy.visit("./index.html");
+    cy.get(".save-video-container__video-list").should("be.visible");
+  });
+});
