613f1a0a872903707c75046d122280c5cd5da65e
kamwoo <rkadndud06@naver.com>
2022-03-17 18:33:38 +0900

    feat: 비디오 id로 유튜브 api 요청 담당 객체 작성

---
+++ src/js/managers/storeVideoAPICaller.js
@@ -0,0 +1,39 @@
+import APIUtil from './APIUtil.js';
+import { ERROR_MESSAGE } from '../utils/constants.js';
+
+const storeVideoAPICaller = {
+  endPoint: 'https://vigorous-boyd-74648a.netlify.app/youtube/v3/videos',
+  queryItems: {
+    part: 'snippet',
+    type: 'video',
+    regionCode: 'KR',
+    id: '',
+  },
+
+  async getVideoListData(videoIdList) {
+    try {
+      this.queryItems.id = videoIdList.join(',');
+      const requestURL = APIUtil.createQueryString(this.endPoint, this.queryItems);
+      const rawData = await APIUtil.fetchData(requestURL);
+      return this.parsingVideoData(rawData);
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  parsingVideoData(rawData) {
+    try {
+      return rawData.items.map(item => ({
+        videoId: item.id.videoId,
+        publishedAt: item.snippet.publishedAt,
+        title: item.snippet.title,
+        url: item.snippet.thumbnails.medium.url,
+        channelTitle: item.snippet.channelTitle,
+      }));
+    } catch (error) {
+      throw new Error(ERROR_MESSAGE.DATA_PROCESSING_ERROR);
+    }
+  },
+};
+
+export default storeVideoAPICaller;
