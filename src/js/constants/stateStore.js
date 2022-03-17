0f84b38309a1ffdfa43771a50a470bb386be3532
junzzi <jjh01027717502@gmail.com>
2022-03-17 21:40:23 +0900

    feat: 저장 비디오 관련 상태 추가 및 검색 비디오 상태 키 네이밍 변경

--- src/js/constants/stateStore.js
+++ src/js/constants/stateStore.js
@@ -1,7 +1,8 @@
 export const STATE_STORE_KEY = {
   IS_MODAL_SHOW: 'isModalShow',
   SEARCH_RESULT: 'searchResult',
-  IS_WAITING_RESPONSE: 'isWaitingResponse',
+  IS_SEARCH_VIDEO_WAITING: 'isSearchVideoWaiting',
   SAVED_VIDEO: 'savedVideo',
   CURRENT_APP_SECTION: 'currentAppSection',
+  IS_SAVED_VIDEO_WAITING: 'isSavedVideoWaiting',
 };
--- src/js/modules/stateStore.js
+++ src/js/modules/stateStore.js
@@ -1,6 +1,5 @@
 import { STATE_STORE_KEY } from '../constants/stateStore';
-import { WEB_STORE_KEY } from '../constants/webStore';
-import webStore from './webStore';
+
 const getInitialState = () => ({
   [STATE_STORE_KEY.IS_MODAL_SHOW]: false,
   [STATE_STORE_KEY.SEARCH_RESULT]: {
@@ -9,16 +8,21 @@ const getInitialState = () => ({
     prevVideoListLength: 0,
     nextPageToken: null,
   },
-  [STATE_STORE_KEY.IS_WAITING_RESPONSE]: false,
-  [STATE_STORE_KEY.SAVED_VIDEO]: webStore.getArrayData(WEB_STORE_KEY.SAVED_VIDEO_LIST_KEY) ?? [],
+  [STATE_STORE_KEY.IS_SEARCH_VIDEO_WAITING]: false,
+  [STATE_STORE_KEY.SAVED_VIDEO]: {
+    videoList: [],
+    prevVideoListLength: 0,
+  },
   [STATE_STORE_KEY.CURRENT_APP_SECTION]: 'watch-video-section-button',
+  [STATE_STORE_KEY.IS_SAVED_VIDEO_WAITING]: false,
 });
 const getInitialSubcribeList = () => ({
   [STATE_STORE_KEY.IS_MODAL_SHOW]: new Set(),
   [STATE_STORE_KEY.SEARCH_RESULT]: new Set(),
-  [STATE_STORE_KEY.IS_WAITING_RESPONSE]: new Set(),
+  [STATE_STORE_KEY.IS_SEARCH_VIDEO_WAITING]: new Set(),
   [STATE_STORE_KEY.SAVED_VIDEO]: new Set(),
   [STATE_STORE_KEY.CURRENT_APP_SECTION]: new Set(),
+  [STATE_STORE_KEY.IS_SAVED_VIDEO_WAITING]: new Set(),
 });
 export const { subscribe, setState, getState } = (function () {
   const state = getInitialState();
