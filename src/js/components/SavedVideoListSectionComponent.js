a4734acd46df1c726c9a45e347334e63105ca949
woose28 <dldntmdsla@naver.com>
2022-03-17 17:10:53 +0900

    feat: 저장된 영상이 없을 때, 저장된 영상 리스트가 비어 있다는 화면을 보여주는 기능 구현

--- src/js/components/SavedVideoListSectionComponent.js
+++ src/js/components/SavedVideoListSectionComponent.js
@@ -1,10 +1,30 @@
+import { getState, subscribe } from '../modules/stateStore';
+import { STATE_STORE_KEY } from '../constants/stateStore';
+
 class SavedVideoListSection {
   #parentElement = null;
+
+  $savedVideoListContainer = null;
+
+  $savedVideoListNoResult = null;
 
   constructor(parentElement) {
     this.#parentElement = parentElement;
 
     this.#mount();
+    this.#initDOM();
+    this.#subscribeStore();
+  }
+
+  render() {
+    const savedVideoList = getState(STATE_STORE_KEY.SAVED_VIDEO);
+
+    if (savedVideoList.length > 0) {
+      this.#showSavedVideoListContainer();
+      return;
+    }
+
+    this.#showSavedVideoListNoResult();
   }
 
   #mount() {
@@ -13,13 +33,40 @@ class SavedVideoListSection {
     this.#parentElement.insertAdjacentHTML('beforeend', template);
   }
 
+  #initDOM() {
+    this.$savedVideoListContainer = this.#parentElement.querySelector(
+      '.saved-video-list__container'
+    );
+    this.$savedVideoListNoResult = this.#parentElement.querySelector(
+      '.saved-video-list__no-result'
+    );
+  }
+
+  #subscribeStore() {
+    subscribe(STATE_STORE_KEY.SAVED_VIDEO, this);
+    this.render();
+  }
+
   #generateTemplate() {
     return `
+      <div class="saved-video-list__container">
+        여기는 저장된 동영상이 보이는  곳~~
+      </div>
       <div class="saved-video-list__no-result">
         <img src="./not_found.png" alt="no result image" class="no-result__image">
         <p class="no-result__description">아직 저장된 영상이 없습니다.</p>
       </div>
     `;
+  }
+
+  #showSavedVideoListContainer() {
+    this.$savedVideoListContainer.classList.remove('hide');
+    this.$savedVideoListNoResult.classList.add('hide');
+  }
+
+  #showSavedVideoListNoResult() {
+    this.$savedVideoListContainer.classList.add('hide');
+    this.$savedVideoListNoResult.classList.remove('hide');
   }
 }
 
