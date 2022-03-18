40b8250d24833c59e46f2af6f5df90dee213ffef
moonheekim0118 <hellomooneekim@gmail.com>
2022-03-19 02:21:17 +0900

    ✅ : 단위 테스트 추가

--- src/js/domain/YoutubeSearchStore.test.js
+++ src/js/domain/YoutubeSearchStore.test.js
@@ -2,13 +2,14 @@ import YoutubeSearchStore from './YoutubeSearchStore';
 import { YOUTUBE_SEARCH_ACTION } from '../constants';
 import * as apiModules from '../api';
 
-describe('유튜브 강의실 검색 시도 상태 테스트', () => {
+describe('유튜브 강의실 검색 관련 상태 테스트', () => {
   test('검색어를 입력했을 때 상태가 정상적으로 저장되어야 한다.', () => {
     const inputKeyword = '검색어';
     YoutubeSearchStore.dispatch(YOUTUBE_SEARCH_ACTION.UPDATE_SEARCH_KEYWORD, inputKeyword);
 
     expect(YoutubeSearchStore.getState()).toStrictEqual({
       searchKeyword: inputKeyword,
+      isEnded: false,
       isLoading: true,
       isLoaded: false,
       items: [],
@@ -23,10 +24,12 @@ describe('유튜브 강의실 검색 시도 상태 테스트', () => {
     spy.mockReturnValue({
       items: [],
       nextPageToken: '',
+      pageInfo: { totalResults: 100 },
     });
     await YoutubeSearchStore.dispatch(YOUTUBE_SEARCH_ACTION.UPDATE_SEARCH_RESULT_REQUEST);
     expect(YoutubeSearchStore.getState()).toStrictEqual({
       searchKeyword: '검색어',
+      isEnded: false,
       isLoading: true,
       isLoaded: false,
       items: [],
@@ -40,9 +43,11 @@ describe('유튜브 강의실 검색 시도 상태 테스트', () => {
     YoutubeSearchStore.dispatch(YOUTUBE_SEARCH_ACTION.UPDATE_SEARCH_RESULT_SUCCESS, {
       items: [1, 2, 3],
       nextPageToken: '1',
+      pageInfo: { totalResults: 10 },
     });
     expect(YoutubeSearchStore.getState()).toStrictEqual({
       searchKeyword: '검색어',
+      isEnded: false,
       isLoading: false,
       isLoaded: true,
       items: [1, 2, 3],
@@ -52,10 +57,12 @@ describe('유튜브 강의실 검색 시도 상태 테스트', () => {
     YoutubeSearchStore.dispatch(YOUTUBE_SEARCH_ACTION.UPDATE_SEARCH_RESULT_SUCCESS, {
       items: [4, 5, 6],
       nextPageToken: '2',
+      pageInfo: { totalResults: 10 },
     });
     expect(YoutubeSearchStore.getState()).toStrictEqual({
       searchKeyword: '검색어',
       isLoading: false,
+      isEnded: false,
       isLoaded: true,
       items: [1, 2, 3, 4, 5, 6],
       nextPageToken: '2',
@@ -69,7 +76,8 @@ describe('유튜브 강의실 검색 시도 상태 테스트', () => {
     expect(YoutubeSearchStore.getState()).toStrictEqual({
       searchKeyword: '검색어',
       isLoading: false,
-      isLoaded: false,
+      isEnded: false,
+      isLoaded: true,
       items: [],
       nextPageToken: '',
       error: true,
