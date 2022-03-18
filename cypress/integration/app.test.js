70fb2e92e0ebcc1c9e10fbf6c25d065d1dcbf54f
junzzi <jjh01027717502@gmail.com>
2022-03-18 18:42:21 +0900

    test: API Request Stubbing 구현

--- cypress/integration/app.test.js
+++ cypress/integration/app.test.js
@@ -1,36 +1,45 @@
+import { API_PATHS } from '../../src/js/constants/fetcher';
+
+/* eslint-disable */
 describe('사용자는 검색을 통해 영상을 확인할 수 있다.', () => {
-  const baseURL = '../../dist/index.html';
+  const baseURL = './index.html';
+  const API_URL = 'https://jolly-agnesi-fe3944.netlify.app';
   beforeEach(() => {
     cy.visit(baseURL);
+
+    cy.showModal();
   });
   it('모달 버튼을 클릭하면, 검색 모달 창이 보인다.', () => {
-    cy.showModal();
     cy.get('.modal-container').should('exist');
   });
   it('모달 버튼을 클릭 후, 검색어를 입력하면 결과를 확인할 수 있다.', () => {
     const validKeyword = '이하이';
-    cy.showModal();
+    cy.intercept(`${API_URL}/${API_PATHS.SEARCH}*`, { fixture: 'searchResult' }).as(
+      'getSearchVideos'
+    );
+
+    cy.get('#search-modal-button').click();
+
     cy.get('#search-input-keyword').type(validKeyword);
-    cy.get('#search-button')
-      .click()
-      .then(() => {
-        cy.get('.video-item').should('exist');
-      });
+
+    cy.get('#search-button').click();
+
+    cy.get('.video-item').should('exist');
   });
-  it('빈 검색어를 입력하면 경고창을 확인하게 된다.', () => {
-    const inValidKeyword = ' ';
+  // it('빈 검색어를 입력하면 경고창을 확인하게 된다.', () => {
+  //   const inValidKeyword = ' ';
 
-    const stub = cy.stub();
-    cy.on('window:alert', stub);
+  //   const stub = cy.stub();
+  //   cy.on('window:alert', stub);
 
-    cy.showModal();
+  //   cy.showModal();
 
-    cy.get('#search-input-keyword').type(inValidKeyword);
+  //   cy.get('#search-input-keyword').type(inValidKeyword);
 
-    cy.get('#search-button')
-      .click()
-      .then(() => {
-        expect(stub).to.be.called;
-      });
-  });
+  //   cy.get('#search-button')
+  //     .click()
+  //     .then(() => {
+  //       expect(stub).to.be.called;
+  //     });
+  // });
 });
