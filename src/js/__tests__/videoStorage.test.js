d86cb2d8e53e9d04a5c31135874ce7ba93a1ea31
kkojae <jaejeung3210@gmail.com>
2022-03-19 01:55:21 +0900

    refactor: 단위 테스트 케이스 수정
    - 의미 없는 단위 테스트 제거

--- src/js/__tests__/videoStorage.test.js
+++ src/js/__tests__/videoStorage.test.js
@@ -2,39 +2,6 @@ import { ERROR_MESSAGE } from "../constants/constants";
 import videoStorage from "../videoStorage";
 
 describe("videoStorage에 동영상의 데이터가 적절히 저장되어야 한다.", () => {
-  test("videoStorage에 동영상의 Id값이 저장되어야 한다.", () => {
-    localStorage.clear();
-
-    const testdata = {
-      videoId: "kkojaeId",
-      thumbnailUrl: "https:",
-      title: "this is title",
-      channelName: "kkojae's channel",
-      publishDate: "2022년 3월 3일",
-    };
-
-    videoStorage.addVideo(testdata);
-    expect(videoStorage.isSavedVideoId("kkojaeId")).toBe(true);
-  });
-
-  test("videoStorage에 101개 이상의 데이터가 저장되면 에러 메시지를 반환한다.", () => {
-    const videoData = Array.from({ length: 101 }, (_, index) => ({
-      videoId: index,
-      thumbnailUrl: "https:",
-      title: "this is title",
-      channelName: "kkojae's channel",
-      publishDate: "2022년 3월 3일",
-    }));
-
-    expect(() =>
-      videoData.forEach((data) => {
-        videoStorage.addVideo(data);
-      })
-    ).toThrowError(ERROR_MESSAGE.USER_STORAGE_OVERFLOW);
-  });
-});
-
-describe("이미 저장된 videoData는 다시 저장될 수 없다.", () => {
   const kkojaeData = {
     videoId: "kkojaeId",
     thumbnailUrl: "https:",
@@ -43,27 +10,33 @@ describe("이미 저장된 videoData는 다시 저장될 수 없다.", () => {
     publishDate: "2022년 3월 3일",
   };
 
-  const usageData = {
-    videoId: "usageId",
-    thumbnailUrl: "https:",
-    title: "this is title",
-    channelName: "usage's channel",
-    publishDate: "2022년 3월 3일",
-  };
-
   beforeEach(() => {
     localStorage.clear();
   });
 
-  test("이미 저장된 videoData이면 true를 반환한다.", () => {
-    videoStorage.addVideo(kkojaeData);
+  test("videoStorage에 101개 이상의 데이터가 저장되면 에러 메시지를 반환한다.", () => {
+    const videoData = Array.from({ length: 100 }, (_, index) => ({
+      videoId: index,
+      thumbnailUrl: "https:",
+      title: "this is title",
+      channelName: "kkojae's channel",
+      publishDate: "2022년 3월 3일",
+    }));
 
-    expect(videoStorage.isSavedVideoId("kkojaeId")).toBe(true);
+    videoData.forEach((data) => {
+      videoStorage.addVideo(data);
+    });
+
+    expect(() => {
+      videoStorage.addVideo(kkojaeData);
+    }).toThrowError(ERROR_MESSAGE.USER_STORAGE_OVERFLOW);
   });
 
-  test("저장되지 않은 videoData이면 false를 반환한다.", () => {
-    videoStorage.addVideo(usageData);
+  test("이미 저장된 videoData를 다시 저장할 경우 에러 메시지를 반환한다.", () => {
+    videoStorage.addVideo(kkojaeData);
 
-    expect(videoStorage.isSavedVideoId("kkojaeId")).toBe(false);
+    expect(() => {
+      videoStorage.addVideo(kkojaeData);
+    }).toThrowError(ERROR_MESSAGE.DUPLICATED_VIDEO_ID);
   });
 });
