c45eb0ec975f8c23dffbf8bbf1fc8d42ab835761
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-03-18 21:40:31 +0900

    :recycle: refactor: delete useless instance

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,7 @@ import './elements/SearchModal';
 import '../css/index.css';
 
 import Search from './domains/Search';
-import Save from './domains/Save';
 
-export const search = new Search();
-export const save = new Save();
+const search = new Search();
+
+export default search;
