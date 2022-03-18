c9019b59ad6e2010aa6b503b055f4479833d331f
jswith <typingjs@gmail.com>
2022-03-18 16:04:16 +0900

    refactor: URLSearchParams 적용

--- src/js/domain/youtubeApi.js
+++ src/js/domain/youtubeApi.js
@@ -2,11 +2,26 @@ import 'regenerator-runtime';
 import { MESSAGE } from '../constants';
 import { skeleton } from '../ui/skeleton';
 
+const BASE_URL = 'https://mincho-youtube.netlify.app/youtube/v3/search';
+const parameters = {
+  part: 'snippet',
+  maxResults: 10,
+  type: 'video',
+  q: null,
+  pageToken: '',
+};
+
 const request = async (searchText, nextPageToken = '') => {
+  const url = new URL(BASE_URL);
+  const searchParams = new URLSearchParams({
+    ...parameters,
+    q: searchText,
+    pageToken: nextPageToken,
+  });
+  url.search = searchParams.toString();
+
   try {
-    const response = await fetch(
-      `https://mincho-youtube.netlify.app/youtube/v3/search?part=snippet&q=${searchText}&maxResults=10&type=video&pageToken=${nextPageToken}`,
-    );
+    const response = await fetch(url);
     if (response.ok) {
       const videoData = await response.json();
       return videoData;
