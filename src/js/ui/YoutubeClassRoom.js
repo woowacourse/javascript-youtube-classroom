0bfd453862d41da7aa6ce491e7ba51fd3de8a583
jswith <typingjs@gmail.com>
2022-03-19 01:11:35 +0900

    feat: 본 영상, 볼 영상 버튼을 눌러 필터링하는 기능

--- src/js/ui/YoutubeClassRoom.js
+++ src/js/ui/YoutubeClassRoom.js
@@ -9,12 +9,15 @@ export default class YoutubeClassRoom {
 
     $('#will-watch-video-button').addEventListener('click', e => {
       $('.will-watch-video-list').replaceChildren();
+      $('.watched-video-list').replaceChildren();
       e.target.classList.add('highlight');
       $('#watched-video-button').classList.remove('highlight');
       this.renderWillWatchVideoList();
     });
 
     $('#watched-video-button').addEventListener('click', e => {
+      $('.watched-video-list').replaceChildren();
+      $('.will-watch-video-list').replaceChildren();
       e.target.classList.add('highlight');
       $('#will-watch-video-button').classList.remove('highlight');
       this.renderWatchedVideoList();
@@ -24,6 +27,7 @@ export default class YoutubeClassRoom {
   renderWillWatchVideoList() {
     const savedVideos = store.getLocalStorage(STORAGE_KEY);
     const filterWillWatchVideoList = savedVideos.filter(savedVideo => savedVideo.watched === false);
+
     const template = filterWillWatchVideoList
       .map(videoData => {
         return `
@@ -61,6 +65,39 @@ export default class YoutubeClassRoom {
   renderWatchedVideoList() {
     const savedDatas = store.getLocalStorage(STORAGE_KEY);
     const filterWatchedVideoList = savedDatas.filter(savedData => savedData.watched === true);
+
+    const template = filterWatchedVideoList
+      .map(videoData => {
+        return `
+      <li class="video-item">
+        <a href="https://www.youtube.com/watch?v=${videoData.videoId}" target="_blank" 
+        data-thumbnails-high-url=${videoData.thumbnailsHighUrl}>
+          <img
+            src=${videoData.thumbnailsHighUrl}
+            alt="video-item-thumbnail" class="video-item__thumbnail"
+            >
+        </a>
+        <a href="https://www.youtube.com/watch?v=${videoData.videoId}" target="_blank" data-title=${encodeURIComponent(
+          videoData.title,
+        )}>
+          <h4 class="video-item__title">${decodeURIComponent(videoData.title)}</h4>
+        </a>
+        <p class="video-item__channel-name" data-channel-title=${videoData.channelTitle}>
+        ${decodeURIComponent(videoData.channelTitle)}
+        </p>
+        <p class="video-item__published-date" data-published-date=${videoData.publishedDate}>
+          ${convertToKoreaLocaleDate(videoData.publishedDate)}
+        </p>
+        <button class="video-item__check-button button highlight">✅</button>
+        <button class="video-item__remove-button button">🗑️</button>
+      </li>`;
+      })
+      .join('');
+
+    $('.watched-video-list').insertAdjacentHTML(
+      'afterbegin',
+      filterWatchedVideoList.length > 0 ? template : this.emptyVideoListTemplate(),
+    );
   }
 
   emptyVideoListTemplate() {
