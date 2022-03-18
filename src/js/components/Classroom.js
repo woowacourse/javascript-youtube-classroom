9017e9ef7d5746ed4e61f4054e1996b605296032
InKyoJeong <viapolar@naver.com>
2022-03-18 19:03:46 +0900

    feat: 저장 버튼을 누르면 강의실 목록에 표시하는 기능 추가

--- src/js/components/Classroom.js
+++ src/js/components/Classroom.js
@@ -6,6 +6,8 @@ export default class Classroom {
     this.classroom = $(".classroom");
 
     this.saveVideoManager = saveVideoManager;
+    this.saveVideoManager.subscribe(this.updateVideos);
+
     this.savedVideos = this.saveVideoManager.getSavedVideos();
     this.renderVideos();
   }
@@ -14,4 +16,11 @@ export default class Classroom {
     this.classroom.replaceChildren();
     this.classroom.insertAdjacentHTML("beforeend", this.savedVideos.map((video) => getFrameTemplate(video)).join(""));
   }
+
+  updateVideos = () => {
+    const updatedVideos = this.saveVideoManager.getSavedVideos();
+    const newVideo = updatedVideos.find((video) => !this.savedVideos.includes(video));
+    this.classroom.insertAdjacentHTML("beforeend", getFrameTemplate(newVideo));
+    this.savedVideos = this.saveVideoManager.getSavedVideos();
+  };
 }
