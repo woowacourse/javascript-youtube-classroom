41477516b730d20d242733897aa6406f6b8ee7b3
yunjin-kim <79692272+yunjin-kim@users.noreply.github.com>
2022-03-18 17:03:43 +0900

    refactor: 메서드명 역할에 맞게 변경

--- src/js/controlVideo.js
+++ src/js/controlVideo.js
@@ -1,16 +1,16 @@
-import { ERROR_MESSAGE, REDIRECT_SERVER_HOST, VIDEO_ID_LIST_KEY, WATCHED_ID_LIST_KEY } from './constants/contants.js';
+import { REDIRECT_SERVER_HOST, VIDEO_ID_LIST_KEY, WATCHED_ID_LIST_KEY } from './constants/contants.js';
 import mockDatas from './utils/mock.js';
 
 class ControlVideo {
   static setVideoIdListByDeleteButton(deleteVideoId, divisionSection) {
     if (divisionSection.contains('will-watch-video-list')) {
-      this.changeLocalStroageVideoList(
-        this.getStorageVideoList(),
+      this.changeStorageVideoList(
+        this.getStorageWillWatchVideoList(),
         deleteVideoId,
         VIDEO_ID_LIST_KEY
       );
     } else if (divisionSection.contains('watched-video-list')) {
-      this.changeLocalStroageVideoList(
+      this.changeStorageVideoList(
         this.getStorageWatchedVideoList(),
         deleteVideoId,
         WATCHED_ID_LIST_KEY
@@ -20,38 +20,38 @@ class ControlVideo {
 
   static setVideoIdListByClickWatchedButton(videoId, divisionSection) {
     if (divisionSection.contains('will-watch-video-list')) {
-      this.changeLocalStroageVideoList(
-        this.getStorageVideoList(),
+      this.changeStorageVideoList(
+        this.getStorageWillWatchVideoList(),
         videoId,
         VIDEO_ID_LIST_KEY
       );
-      this.addLocalStorageVideoList(
+      this.addStorageVideoList(
         this.getStorageWatchedVideoList(),
         videoId,
         WATCHED_ID_LIST_KEY
       );
     } else if (divisionSection.contains('watched-video-list')) {
-      this.changeLocalStroageVideoList(
+      this.changeStorageVideoList(
         this.getStorageWatchedVideoList(),
         videoId,
         WATCHED_ID_LIST_KEY
       );
-      this.addLocalStorageVideoList(this.getStorageVideoList(), videoId, VIDEO_ID_LIST_KEY);
+      this.addStorageVideoList(this.getStorageWillWatchVideoList(), videoId, VIDEO_ID_LIST_KEY);
     }
   }
 
-  static changeLocalStroageVideoList(videoList, clickVideoId, localStorageKeyName) {
+  static changeStorageVideoList(videoList, clickVideoId, localStorageKeyName) {
     localStorage.setItem(
       localStorageKeyName,
       JSON.stringify(videoList.filter((videoId) => videoId !== clickVideoId))
     );
   }
 
-  static addLocalStorageVideoList(videoList, videoId, localStorageKeyName) {
+  static addStorageVideoList(videoList, videoId, localStorageKeyName) {
     localStorage.setItem(localStorageKeyName, JSON.stringify([videoId, ...videoList]));
   }
 
-  static getStorageVideoList() {
+  static getStorageWillWatchVideoList() {
     return JSON.parse(localStorage.getItem(VIDEO_ID_LIST_KEY)) || [];
   }
 
