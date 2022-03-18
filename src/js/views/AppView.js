3b2c9afd985bd07effd01dcbe0b5daf7f351db3d
KIM YEJI <rladpwl0512@gmail.com>
2022-03-19 01:30:14 +0900

    :sparkles: feat: 탭이 클릭되었을 때 각각에 맞는 내용 보여줌

--- src/js/views/AppView.js
+++ src/js/views/AppView.js
@@ -4,15 +4,37 @@ export default class AppView {
   constructor() {
     this.$searchModalButton = $('#search-modal-button');
     this.$modalContainer = $('.modal-container');
+    this.$willSeeButton = $('#will-see-button');
+    this.$sawButton = $('#saw-button');
+    this.$willSeeWrapper = $('.will-see-wrapper');
+    this.$sawWrapper = $('.saw-wrapper');
 
     this.#bindEvents();
   }
 
   #bindEvents() {
-    this.$searchModalButton.addEventListener('click', this.#handleClickButton.bind(this));
+    this.$searchModalButton.addEventListener('click', this.#handleClickSearchButton.bind(this));
+    this.$willSeeButton.addEventListener('click', this.#handleClickWillSeeButton.bind(this));
+    this.$sawButton.addEventListener('click', this.#handleClickSawButton.bind(this));
   }
 
-  #handleClickButton() {
+  #handleClickSearchButton() {
     this.$modalContainer.classList.remove('hide');
+  }
+
+  #handleClickWillSeeButton() {
+    this.$sawButton.classList.remove('clicked');
+    this.$willSeeButton.classList.add('clicked');
+
+    this.$sawWrapper.classList.add('hide');
+    this.$willSeeWrapper.classList.remove('hide');
+  }
+
+  #handleClickSawButton() {
+    this.$willSeeButton.classList.remove('clicked');
+    this.$sawButton.classList.add('clicked');
+
+    this.$willSeeWrapper.classList.add('hide');
+    this.$sawWrapper.classList.remove('hide');
   }
 }
