6613aa3c3d577c51e85dbc070d489a91fee5cc87
junzzi <jjh01027717502@gmail.com>
2022-03-18 20:06:26 +0900

    refactor: 불필요한 메소드 제거(webStore모듈)

--- src/js/modules/WebStore.js
+++ src/js/modules/WebStore.js
@@ -2,10 +2,6 @@ export default {
   getArrayData(key) {
     return JSON.parse(localStorage.getItem(key) ?? '[]');
   },
-  setDataInArray(key, data) {
-    const currentData = this.getArrayData(key);
-    localStorage.setItem(key, JSON.stringify([...currentData, data]));
-  },
   setData(key, dataOrFunction) {
     if (typeof dataOrFunction === 'function') {
       const currentData = this.getArrayData(key);
