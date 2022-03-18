31032ea22df39f4f52a9cc75167019bf91327470
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-03-19 03:26:22 +0900

    :white_check_mark: test: add e2e test code

--- src/js/templates.js
+++ src/js/templates.js
@@ -42,7 +42,7 @@ const TEMPLATE = {
   `,
   SEARCH_RESULT: `
     <h3 hidden>검색 결과</h3>
-    <ul is="video-list"></ul>
+    <ul is="video-list" id="searched-video-list"></ul>
     <section class="search-result search-result--no-result hidden">
       <h3 hidden>검색 결과</h3>
       <div class="no-result">
