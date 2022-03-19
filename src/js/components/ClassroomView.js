4ffcaa24a1a7d8c89f7916716168b82093141b94
YUNHO KANG <ansghkdbsgh@gmail.com>
2022-03-19 21:00:07 +0900

    refactor: classroom view simplify

--- src/js/components/ClassroomView.js
+++ src/js/components/ClassroomView.js
@@ -9,45 +9,52 @@ export class ClassroomView {
 
     this.openModalButton = document.getElementById('search-modal-button');
     this.openModalButton.addEventListener('click', this.props.openModal);
-    this.openModalButton.addEventListener('click', this.removeNavButtonClicked);
 
     this.nav = document.getElementById('nav');
     this.contentsContainer = document.getElementById('classroom-contents-container');
     this.willSeeVideoButton = document.getElementById('will-see-video-button');
     this.alreadyWatchedVideoButton = document.getElementById('already-watched-video-button');
 
+    this.contentsContainer.addEventListener('click', this.handleContentsButton);
     this.willSeeVideoButton.addEventListener('click', this.handleWillSeeVideoNav);
     this.alreadyWatchedVideoButton.addEventListener('click', this.handleAlreadyWatchedVideoNav);
-    this.contentsContainer.addEventListener('click', this.handleContentsButton);
   }
-
-  removeNavButtonClicked = () => {
-    this.alreadyWatchedVideoButton.classList.remove('clicked');
-    this.willSeeVideoButton.classList.remove('clicked');
-  };
 
   handleWillSeeVideoNav = async () => {
     this.clearClassroomContentsContainer();
-
-    this.alreadyWatchedVideoButton.classList.remove('clicked');
-    this.willSeeVideoButton.classList.add('clicked');
+    this.highlightNavButtons(this.willSeeVideoButton);
 
     this.classroom.videoList = getVideoItemsFromLocalStorage();
+    this.renderContents('watchLater');
+    if (this.classroom.hasNoWillSeeVideo()) {
+      this.contentsContainer.insertAdjacentHTML('beforeend', noClassroomContentsTemplate());
+    }
+  };
+
+  handleAlreadyWatchedVideoNav = () => {
+    this.clearClassroomContentsContainer();
+    this.highlightNavButtons(this.alreadyWatchedVideoButton);
+
+    this.classroom.videoList = getVideoItemsFromLocalStorage();
+    this.renderContents('alreadyWatched');
+    if (this.classroom.hasNoAlreadyWatchVideo()) {
+      this.contentsContainer.insertAdjacentHTML('beforeend', noClassroomContentsTemplate());
+    }
+  };
+
+  renderContents(key) {
+    const later = key === 'watchLater' ? true : false;
     this.contentsContainer.insertAdjacentHTML(
       'beforeend',
       this.classroom.videoList
         .map((video) => {
-          if (video.watchLater === true) {
+          if (video.watchLater === later) {
             return makeThumbnailTemplate(video);
           }
         })
         .join(''),
     );
-
-    if (this.contentsContainer.childElementCount === 0) {
-      this.contentsContainer.insertAdjacentHTML('beforeend', noClassroomContentsTemplate());
-    }
-  };
+  }
 
   clearClassroomContentsContainer() {
     this.contentsContainer.innerHTML = ``;
@@ -55,45 +62,37 @@ export class ClassroomView {
 
   handleContentsButton = (e) => {
     if (e.target.classList.contains('already-watch-button')) {
-      this.classroom.videoList.forEach((video) => {
-        if (e.target.id === video.id) {
-          video.watchLater = false;
-          e.target.parentNode.parentNode.remove();
-        }
-      });
-      saveVideoItemToLocalStorage(this.classroom.videoList);
+      this.moveVideoCardToAlreadyWatchVideo(e);
     }
 
     if (e.target.classList.contains('discard-button')) {
-      if (window.confirm('진짜 지우실?')) {
-        this.classroom.videoList = this.classroom.videoList.filter((video) => video.id !== e.target.id);
+      this.discardVideoCard(e);
+    }
+  };
+
+  moveVideoCardToAlreadyWatchVideo(e) {
+    this.classroom.videoList.forEach((video) => {
+      if (e.target.id === video.id) {
+        video.watchLater = false;
         e.target.parentNode.parentNode.remove();
-
-        saveVideoItemToLocalStorage(this.classroom.videoList);
       }
+    });
+    saveVideoItemToLocalStorage(this.classroom.videoList);
+  }
+
+  discardVideoCard(e) {
+    if (window.confirm('진짜 지우실?')) {
+      this.classroom.videoList = this.classroom.videoList.filter((video) => video.id !== e.target.id);
+      e.target.parentNode.parentNode.remove();
+
+      saveVideoItemToLocalStorage(this.classroom.videoList);
     }
-  };
+  }
 
-  handleAlreadyWatchedVideoNav = () => {
-    this.clearClassroomContentsContainer();
-
+  highlightNavButtons() {
     this.willSeeVideoButton.classList.remove('clicked');
-    this.alreadyWatchedVideoButton.classList.add('clicked');
+    this.alreadyWatchedVideoButton.classList.remove('clicked');
 
-    this.classroom.videoList = getVideoItemsFromLocalStorage();
-    this.contentsContainer.insertAdjacentHTML(
-      'beforeend',
-      this.classroom.videoList
-        .map((video) => {
-          if (video.watchLater === false) {
-            return makeThumbnailTemplate(video);
-          }
-        })
-        .join(''),
-    );
-
-    if (this.contentsContainer.childElementCount === 0) {
-      this.contentsContainer.insertAdjacentHTML('beforeend', noClassroomContentsTemplate());
-    }
-  };
+    [...arguments].forEach((button) => button.classList.add('clicked'));
+  }
 }
--- src/js/model/Classroom.js
+++ src/js/model/Classroom.js
@@ -2,4 +2,11 @@ export class Classroom {
   constructor() {
     this.videoList;
   }
+
+  hasNoWillSeeVideo() {
+    return this.videoList.filter((video) => video.watchLater === true).length === 0;
+  }
+  hasNoAlreadyWatchVideo() {
+    return this.videoList.filter((video) => video.watchLater === false).length === 0;
+  }
 }
