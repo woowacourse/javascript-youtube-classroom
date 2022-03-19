0272e542eabb48d1d2c46b269e1d1bf187f529aa
intae92 <kangit2013@gmail.com>
2022-03-19 17:21:01 +0900

    test: 로컬스토리지 최대 저장 유무 테스트

--- src/js/__test__/webStore.test.js
+++ src/js/__test__/webStore.test.js
@@ -1,13 +1,12 @@
-// import { ERROR_MESSAGE, RULES } from "../constants";
-// import { setStorageVideoIDs, checkVideoStorageFull } from "../utils/localStorage";
+import { ERROR_MESSAGE, RULES } from "../constants";
+import { setStorageVideoIDs, checkVideoStorageFull } from "../utils/localStorage";
 
-// describe('localStorage 저장 테스트', () => {
-//     it('최대 저장 개수를 넘으면 에러를 발생시킨다.', () => {
-//         const key = 'test';
-//         const value = new Array(RULES.MAX_STORED_IDS_AMOUNT).fill(null).map((v,i) => i);
-//         console.log(value,'valuevaluevalue')
+describe('localStorage 저장 테스트', () => {
+    it('최대 저장 개수를 넘으면 에러를 발생시킨다.', () => {
+        const key = 'test';
+        const value = new Array(RULES.MAX_STORED_IDS_AMOUNT).fill(null).map((v,i) => i);
         
-//         setStorageVideoIDs({ key, value })
-//         expect(() => checkVideoStorageFull({key})).toThrowError(ERROR_MESSAGE.FULL_STORAGE);
-//     });
-// })
+        setStorageVideoIDs({ key, value })
+        expect(() => checkVideoStorageFull(key)).toThrowError(ERROR_MESSAGE.FULL_STORAGE);
+    });
+})
