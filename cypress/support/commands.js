b5119f332a565846ad5deba02caa8b6ab37b1cfb
2019015414 <2019015414@hanyang.ac.kr>
2022-03-19 16:22:36 +0900

    test: 저장된 영상 관련 테스트

---
+++ cypress/support/commands.js
@@ -0,0 +1,33 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('saveVideo', () => {
+  cy.get('#search-modal-button').click();
+  cy.get('#search-input-keyword').type('고양이');
+  cy.get('#search-button').click();
+  cy.get('.video-item__save-button').first().click();
+  cy.get('#modal-background').click({ force: true });
+});
