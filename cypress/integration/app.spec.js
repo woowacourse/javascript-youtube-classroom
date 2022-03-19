c9118027819f6704eec3e7b20b9f9c4a19e05d75
gytks4 <gytks4@naver.com>
2022-03-19 23:58:49 +0900

    test: 검색 버튼 클릭 시 카드 10개 보여주기

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -7,14 +7,22 @@ describe('유튜브 App 태스트', () => {
   beforeEach(() => {
     cy.visit('http://localhost:8080/');
   });
-  // it('검색을 하면(enter), 10개의 영상 데이터를 받아와서, 화면에 보여준다.', () => {
-  //   cy.get($('#search-input-keyword'))
-  //     .type('우테코{enter}')
-  //     .then(() => {
-  //       cy.get($('skeleton')).should('be.visible');
-  //     });
-  //   cy.get($('#video-list'));
-  // });
+
+  it('검색을 하면(enter), 10개의 영상 데이터를 받아와서, 화면에 보여준다.', () => {
+    cy.get('#search-modal-button').click();
+    cy.get('#search-input-keyword')
+      .type('우테코{enter}')
+      .then(() => {
+        cy.get('#video-list .video-card.real').should(
+          'have.length',
+          LOAD_VIDEOS_COUNT
+        );
+
+        $$('#video-list .video-item__title').forEach(el =>
+          cy.get(el).should('have.text')
+        );
+      });
+  });
 
   it('검색을 하면(enter), 10개의 skeleton UI가 보여진다.', () => {
     cy.get('#search-modal-button').click();
