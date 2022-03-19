6b3187b03617c85bdd0f5f04eb5c36d9853556db
2019015414 <2019015414@hanyang.ac.kr>
2022-03-19 18:21:57 +0900

    fix: 주소 변경

--- src/js/YoutubeAPI/constants.js
+++ src/js/YoutubeAPI/constants.js
@@ -1,1 +1,3 @@
-export const REDIRECT_SERVER_HOST = 'https://sad-mcclintock-e1eeea.netlify.app/youtube/v3/search';
+export const REDIRECT_SERVER_HOST = 'https://pensive-allen-3f5673.netlify.app/youtube/v3/search';
+
+// export const REDIRECT_SERVER_HOST = 'https://pensive-allen-3f5673.netlify.app/dummy/youtube/v3/search';
