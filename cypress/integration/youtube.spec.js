91161bac1dc7fc3c5ef13c076ee18deb41c03492
최광희 <polyckh@gmail.com>
2022-03-20 05:48:45 +0900

    test: custom commands 추가

--- cypress/integration/youtube.spec.js
+++ cypress/integration/youtube.spec.js
@@ -8,24 +8,19 @@ describe('Youtube ClassRoom Test', () => {
   });
 
   it(`입력창에 keyword를 입력하고 submit을 하면 ${RULES.MAX_VIDEO_AMOUNT_PER_REQUEST}개의 영상을 받아와 화면에 보여준다.`, () => {
-    cy.get('#search-modal-button').click();
-    cy.get('#search-input-keyword')
-      .type('후이{enter}')
-      .then(() => {
-        cy.get('.search-modal .video-item').should(
-          'have.length',
-          RULES.MAX_VIDEO_AMOUNT_PER_REQUEST,
-        );
-      });
+    const keyword = '후이{enter}';
+
+    cy.searchKeyword(keyword).then(() => {
+      cy.get('.search-modal .video-item').should('have.length', RULES.MAX_VIDEO_AMOUNT_PER_REQUEST);
+    });
   });
 
   it('빈 입력을 하면 영상을 불러오지 않는다.', () => {
-    cy.get('#search-modal-button').click();
-    cy.get('#search-input-keyword')
-      .type('{enter}')
-      .then(() => {
-        cy.get('.search-modal .video-item').should('have.length', 0);
-      });
+    const emptyKeyword = '{enter}';
+
+    cy.searchKeyword(emptyKeyword).then(() => {
+      cy.get('.search-modal .video-item').should('have.length', 0);
+    });
   });
 
   it('결과가 없는 키워드 입력에 대해 결과가 없음을 보여줘야 한다.', () => {
@@ -33,13 +28,12 @@ describe('Youtube ClassRoom Test', () => {
       '!@#!@$!#%@$^#%&$^%!@#!$!#%&(^#%$!@#!@$#$!#@!#))&%^)&%^)&@!@#!#$@#$%$@#^%&$%^&#$@$^#%&$%^$^%*$^&^@#$@#$@#%@#$^#%&^**#^#$%@#$@#$^@#$!$@#%@#$%#$^#$%^$%@#$!@#!@#%)^&)%^$%#$%#$^#%^#%^#^&%^)&#$)%)#$)%#$%!@#!@$#$!#@!#))&%^)&%^)&%)^&)%^&%^)&#$)%)#$)%#$%';
     const noResultMessage = `검색 결과가 없습니다\n  다른 키워드로 검색해보세요`;
 
-    cy.get('#search-modal-button').click();
-    cy.get('#search-input-keyword')
-      .type(`${noResultKeyword}{enter}`)
-      .then(() => {
-        cy.get('.no-result__description').should('have.text', noResultMessage);
-      });
+    cy.searchKeyword(`${noResultKeyword}{enter}`).then(() => {
+      cy.get('.no-result__description').should('have.text', noResultMessage);
+    });
   });
 
-  it('영상의 저장 버튼을 클릭하면, 저장 버튼이 사라진다.', () => {});
+  // it('영상의 저장 버튼을 클릭하면, 저장 버튼이 사라진다.', () => {
+
+  // });
 });
