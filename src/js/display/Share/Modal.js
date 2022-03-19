0d402024b9fbc964ae2aa640790f9d4adb58e966
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-03-19 19:13:41 +0900

    💄 : 모달 사용성 개선

--- src/js/display/Share/Modal.js
+++ src/js/display/Share/Modal.js
@@ -1,10 +1,11 @@
 import { $ } from '@Utils/Dom';
-import { addEventDelegate } from '@Utils/CustomEvent';
+import { addEventDelegate, addEventOnce } from '@Utils/CustomEvent';
 
 class Modal {
   $activeModal;
   $container;
   $modal = $('#modal');
+  $modalDimmer = $('.dimmer');
 
   enable(container) {
     this.$container = $(container);
@@ -28,16 +29,24 @@ class Modal {
     const beforeModal = this.$activeModal;
 
     this.$modal.classList.remove('hide');
-
     !!beforeModal && beforeModal.classList.remove('show');
+
     this.$activeModal = $(`#${modalId}`);
     this.$activeModal.classList.add('show');
+
+    document.body.classList.add('scroll-lock');
   };
 
   handleCloseModal = () => {
-    this.$modal.classList.add('hide');
-    this.$activeModal.classList.remove('show');
-    this.$activeModal = null;
+    this.$modal.classList.add('disappear');
+    document.body.classList.remove('scroll-lock');
+
+    addEventOnce('animationend', this.$modalDimmer, () => {
+      this.$modal.classList.add('hide');
+      this.$modal.classList.remove('disappear');
+      this.$activeModal.classList.remove('show');
+      this.$activeModal = null;
+    });
   };
 }
 
