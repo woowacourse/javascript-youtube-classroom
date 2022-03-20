5d908cf675bd556f9c872a820a15e430a1536d18
Jeongmin Woo <greenblues1190@gmail.com>
2022-03-20 16:13:01 +0900

    test: e2e 테스트 html 선택자 수정

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -25,23 +25,20 @@ describe('e2e test', () => {
   it('영상을 저장하면 저장한 영상이 보여야 한다.', () => {
     cy.get('.video-item__save-button').first().click();
     cy.get('#modal-background').click({ force: true });
-    cy.get('#saved-video-list').find('.video-card').should('have.length', 1);
+    cy.get('saved-list').find('.video-card').should('have.length', 1);
   });
 
   it('본 영상 버튼을 클릭하면 본 영상 필터를 클릭했을 때 영상이 보여야 한다.', () => {
-    cy.get('#saved-video-list')
-      .find('.video-item__watched-button')
-      .first()
-      .click();
-    cy.get('#saved-video-list').find('.video-card').should('have.length', 0);
+    cy.get('saved-list').find('.video-item__watched-button').first().click();
+    cy.get('saved-list').find('.video-card').should('have.length', 0);
     cy.get('#filter-watched-button').click();
-    cy.get('#saved-video-list').find('.video-card').should('have.length', 1);
+    cy.get('saved-list').find('.video-card').should('have.length', 1);
   });
 
   it('삭제 버튼을 클릭하면 사용자에게 삭제할 것인지 물어야 한다.', () => {
     const confirmStub = createConfirmStub(false);
 
-    cy.get('#saved-video-list')
+    cy.get('saved-list')
       .find('.video-item__remove-button')
       .first()
       .click()
@@ -51,15 +48,13 @@ describe('e2e test', () => {
   });
 
   it('사용자가 삭제를 confirm하면 저장된 영상에서 지워져야 한다.', () => {
-    cy.get('#saved-video-list')
+    cy.get('saved-list')
       .find('.video-item__remove-button')
       .first()
       .click()
       .then(() => {
         cy.on('window:confirm', () => true);
-        cy.get('#saved-video-list')
-          .find('.video-card')
-          .should('have.length', 0);
+        cy.get('saved-list').find('.video-card').should('have.length', 0);
       });
   });
 });
