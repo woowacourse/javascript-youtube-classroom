9266f66cf0a72ce420a41f570cea82e3b8be15f9
sanaandmomo <qajjang46@naver.com>
2022-03-21 03:14:53 +0900

    test: 모달 재오픈시, 검색어와 결과 내용이 비어있는지

--- src/test/app.spec.js
+++ src/test/app.spec.js
@@ -1,9 +1,10 @@
-import { SELECTOR, ERROR_MESSAGE } from '../js/constants/index.js';
+import { SELECTOR, ERROR_MESSAGE, YOUTUBE_API_REQUEST_COUNT } from '../js/constants/index.js';
 
 describe('구현 결과가 요구사항과 일치해야 한다.', () => {
   const baseURL = 'index.html';
 
   beforeEach(() => {
+    cy.viewport(1500, 1200);
     cy.visit(baseURL);
   });
 
@@ -16,6 +17,21 @@ describe('구현 결과가 요구사항과 일치해야 한다.', () => {
       .submit()
       .then(() => {
         expect(alertStub).to.be.calledWith(ERROR_MESSAGE.EMPTY_KEYWORD);
+      });
+  });
+
+  it('모달에서 올바르게 검색 후, 모달을 껏다가 키면 검색어와 결과 내용이 비어있어야 한다.', () => {
+    const keyword = '아놀드';
+
+    cy.get(SELECTOR.SEARCH_MODAL_BUTTON).click();
+    cy.get(SELECTOR.SEARCH_INPUT_KEYWORD).type(keyword);
+    cy.get(SELECTOR.SEARCH_FORM).submit();
+    cy.get(SELECTOR.MODAL_BACKGROUND).click({ force: true });
+    cy.get(SELECTOR.SEARCH_MODAL_BUTTON)
+      .click()
+      .then(() => {
+        cy.get(SELECTOR.SEARCH_INPUT_KEYWORD).should('have.value', '');
+        cy.get(SELECTOR.VIDEOS).children().should('have.length', YOUTUBE_API_REQUEST_COUNT);
       });
   });
 });
