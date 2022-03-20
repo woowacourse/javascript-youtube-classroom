5598e9efaad065bd88e5631ff7b45c872e5782c8
Soyi Jeon <soyi.dev@gmail.com>
2022-03-20 22:10:54 +0900

    test: Storage 관련 테스트 명세 수정
    - localStroage에 대한 기능임을 명시

--- __tests__/unit/storage.test.js
+++ __tests__/unit/storage.test.js
@@ -9,26 +9,26 @@ describe('Storage 기능 테스트', () => {
     storage = new Storage();
   });
 
-  it('영상 정보를 저장한다.', () => {
+  it('localStorage에 영상 정보를 저장한다.', () => {
     const newVideo = { id: 'testVideoID', watched: false };
     storage.saveVideo(newVideo);
 
     expect(JSON.parse(localStorage.getItem('videos')).find((video) => video.id === newVideo.id)).toBeTruthy();
   });
 
-  it('영상을 이미 저장했는지 확인할 수 있다.', () => {
+  it('localStorage에 저장된 영상인지 ID로 확인할 수 있다.', () => {
     const newVideo = { id: 'testVideoID', watched: false };
     storage.saveVideo(newVideo);
 
     expect(storage.findVideoById(newVideo.id)).toBeDefined();
   });
 
-  it('저장되지 않은 영상에 대해 업데이트를 시도하면 에러를 던진다.', () => {
+  it('localStorage에 저장되지 않은 영상에 대해 업데이트를 시도하면 에러를 던진다.', () => {
     const unsavedVideo = { id: 'undefinedVideoID' };
     expect(() => storage.updateVideo(unsavedVideo)).toThrow(ERROR_MESSAGE.CAN_NOT_UPDATE_ON_NOT_SAVED_VIDEO);
   });
 
-  it('저장된 영상 정보는 업데이트 할 수 있다.', () => {
+  it('localStorage에 저장된 영상 정보를 업데이트 할 수 있다.', () => {
     const video = { id: 'testVideoID', watched: false };
     const updatedVideo = { id: 'testVideoID', watched: true };
     storage.saveVideo(video);
@@ -36,12 +36,12 @@ describe('Storage 기능 테스트', () => {
     expect(storage.findVideoById(video.id).watched).toBeTruthy();
   });
 
-  it('저장되지 않은 영상을 삭제하려고 하면 에러를 던진다.', () => {
+  it('localStorage에 저장되지 않은 영상을 삭제하려고 하면 에러를 던진다.', () => {
     const unsavedVideo = { id: 'undefinedVideoID' };
     expect(() => storage.deleteVideoById(unsavedVideo.id)).toThrow(ERROR_MESSAGE.CAN_NOT_DELETE_ON_NOT_SAVED_VIDEO);
   });
 
-  it('저장된 영상 정보는 삭제할 수 있다.', () => {
+  it('localStorage에 저장된 영상 정보를 삭제할 수 있다.', () => {
     const video = { id: 'testVideoID', watched: false };
     storage.saveVideo(video);
     storage.deleteVideoById(video.id);
