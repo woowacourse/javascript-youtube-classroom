736fb91a6b742c240012188386cdff048b10e84d
YUNHO KANG <ansghkdbsgh@gmail.com>
2022-03-20 14:43:55 +0900

    merge: resolve conflict

--- src/js/components/Classroom.js
+++
@@ -1,7 +0,0 @@
-export class Classroom {
-  constructor(props) {
-    this.props = props;
-    this.openModalButton = document.getElementById('search-modal-button');
-    this.openModalButton.addEventListener('click', this.props.openModal);
-  }
-}
