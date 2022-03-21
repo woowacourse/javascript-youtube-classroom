f164b1890b5fc338514d82f6de90f52d7db71cb5
jhy979 <jhy979@gmail.com>
2022-03-22 00:08:42 +0900

    fix: mockdata parseData 네이밍 수정

--- src/js/__test__/videoStorage.test.js
+++ src/js/__test__/videoStorage.test.js
@@ -1,17 +1,17 @@
-import { parseData } from '../utils/mockData';
+import { PARSE_DATA } from '../utils/mockData';
 import videoStorage from '../videoStorage.js';
 
 describe('비디오 저장소에서 ', () => {
-  videoStorage.storeVideoData(parseData[0]);
-  videoStorage.storeVideoData(parseData[1]);
-  videoStorage.storeVideoData(parseData[2]);
+  videoStorage.storeVideoData(PARSE_DATA[0]);
+  videoStorage.storeVideoData(PARSE_DATA[1]);
+  videoStorage.storeVideoData(PARSE_DATA[2]);
 
   test('[썸네일 이미지 url, 제목, 채널 이름, 업로드 날짜, 비디오 고유 id]가 객체로 잘 저장되는지 확인합니다.', () => {
-    expect(videoStorage.hasVideoData(parseData[0].videoId)).toBe(true);
+    expect(videoStorage.hasVideoData(PARSE_DATA[0].videoId)).toBe(true);
   });
 
   test('특정 비디오 데이터를 삭제할 수 있다.', () => {
-    videoStorage.deleteVideoData(parseData[1].videoId);
-    expect(videoStorage.hasVideoData(parseData[1].videoId)).toBe(false);
+    videoStorage.deleteVideoData(PARSE_DATA[1].videoId);
+    expect(videoStorage.hasVideoData(PARSE_DATA[1].videoId)).toBe(false);
   });
 });
