fdfad4c2f594c3930ea2632bd4bb8669f309a179
jhy979 <jhy979@gmail.com>
2022-03-21 22:10:51 +0900

    refactor: mockData 상수 네이밍 적용

--- src/js/__test__/API.test.js
+++ src/js/__test__/API.test.js
@@ -1,18 +1,18 @@
 import videoAPI from '../videoAPI.js';
-import { fetchData, errorData, parseData } from '../utils/mockData.js';
+import { FETCH_DATA, ERROR_DATA, PARSE_DATA } from '../utils/mockData.js';
 
 describe('API 요청이 끝나고', () => {
   test('응답 데이터에 에러가 있는지 확인할 수 있다.', () => {
     expect(() => {
-      videoAPI.checkResponseError(errorData);
+      videoAPI.checkResponseError(ERROR_DATA);
     }).toThrowError();
   });
 
   test('응답 데이터가 정상인지 확인할 수 있다.', () => {
-    expect(videoAPI.checkResponseError(fetchData)).toBe(false);
+    expect(videoAPI.checkResponseError(FETCH_DATA)).toBe(false);
   });
 
   test('response 데이터를 썸네일 이미지 url, 제목, 작성자, 작성요일, id로 분리할 수 있다.', () => {
-    expect(videoAPI.parsingVideoData(fetchData)).toEqual(parseData);
+    expect(videoAPI.parsingVideoData(FETCH_DATA)).toEqual(PARSE_DATA);
   });
 });
