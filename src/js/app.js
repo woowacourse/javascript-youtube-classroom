2ec00eada61d0a947f9e76d1c3ae949be98335ad
uk960214 <uk960214@gmail.com>
2022-03-22 23:00:34 +0900

    refactor: 상위 뷰를 앱으로 대체

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,18 @@
-import YoutubeClassRoomView from './view/YoutubeClassRoomView';
+import ManageVideoStorage from './domain/ManageVideoStorage';
+import SavedVideosView from './view/SavedVideosView/SavedVideosView';
+import SearchModalView from './view/SearchModal/SearchModalView';
+
+class YoutubeClassRoomApp {
+  constructor() {
+    this.manageVideoStorage = new ManageVideoStorage();
+    this.savedVideosView = new SavedVideosView(this.manageVideoStorage);
+    this.searchModalView = new SearchModalView(this.renderOnModalClose, this.manageVideoStorage);
+  }
+
+  renderOnModalClose = () => {
+    this.savedVideosView.renderVideoListUpdate();
+  };
+}
 
 // eslint-disable-next-line
-new YoutubeClassRoomView();
+new YoutubeClassRoomApp();
